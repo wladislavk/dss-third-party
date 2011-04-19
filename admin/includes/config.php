7b5cdcf72d341c4f634f9dc5ab452b7ab8ed4f5c
Adam Bert <abert@dss.xforty.com>
2011-04-19 11:47:03 -0400

    Fixed connection

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,7 +1,7 @@
 <?
 	session_start();
-	$con = mysql_connect("localhost","dentalsl_site","cr3at1vItYsite") or die('connection failure');
-	$db = mysql_select_db("dentalsl_site") or die('DB connection failure');
+	$con = mysql_connect("localhost","root","t3mp123") or die('connection failure');
+	$db = mysql_select_db("dentalsl_site_dev") or die('DB connection failure');
 	
 	$base_path = 'http://'.$_SERVER['HTTP_HOST'].'/dental/';
 	include("general.htm");

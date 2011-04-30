43f76d804b7c5c51d1e3ba7c29ec2c0910f2971b
Andrew Rosborough <arosborough@acgnw.com>
2011-04-29 22:04:49 -0400

    Template information

---
+++ sql_updates/2011-04-29-11-51_letter-template-information.sql
@@ -0,0 +1,4 @@
+INSERT INTO dental_letter_templates VALUES ('18', 'Progress Note Non Compliance', '/manage/dss_to_pt_non_compliant.php');
+INSERT INTO dental_letter_templates VALUES ('21', 'Annual Follow Up', '/manage/dss_to_pt_yearly_follow_up.php');
+INSERT INTO dental_letter_templates VALUES ('23', 'Thirty Month Follow Up', '/manage/dss_to_pt_30month_follow_up.php');
+

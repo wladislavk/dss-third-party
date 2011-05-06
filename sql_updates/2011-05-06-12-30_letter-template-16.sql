609383eb39e7183b52cfa8c29ca634f573d4602a
Andrew Rosborough <arosborough@acgnw.com>
2011-05-06 17:34:51 -0400

    Updated letter template names, and added 2 templates

---
+++ sql_updates/2011-05-06-12-30_letter-template-16.sql
@@ -0,0 +1,2 @@
+INSERT INTO dental_letter_templates VALUES ('16', 'Progress Note to MD and Pt', '/manage/dss_to_md_pt_progress_note.php');
+INSERT INTO dental_letter_templates VALUES ('20', 'Ltr To Pt Treatment Complete', 'dss_to_pt_treatment_complete.php');

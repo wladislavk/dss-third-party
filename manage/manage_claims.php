343a6a22ae0b807c2b7c80404f620bd10b44cc3e
Adam Bert <abert@xforty.com>
2011-07-06 13:12:35 -0400

    Ticket #6916. Changed claim number to be patients with pending transactions

--- manage/manage_claims.php
+++ manage/manage_claims.php
@@ -42,6 +42,99 @@ $my=mysql_query($sql) or die(mysql_error());
 	<b><? echo $_GET['msg'];?></b>
 </div>
 
+<table width="98%" cellpadding="5" cellspacing="1" bgcolor="#FFFFFF" align="center" >
+        <tr class="tr_bg_h">
+                <td valign="top" class="col_head" width="20%">
+                        Patient
+                </td>
+                <td valign="top" class="col_head" width="20%">
+                        Action
+                </td>
+        </tr>
+<?php
+$sqlp = "SELECT DISTINCT p.patientid, p.firstname, p.lastname FROM dental_ledger ledger
+                         JOIN dental_transaction_code trxn_code ON trxn_code.transaction_code = ledger.transaction_code
+                         JOIN dental_users user ON user.userid = ledger.docid
+                         JOIN dental_patients p ON p.patientid = ledger.patientid
+                  WHERE 
+                         ledger.status = " . DSS_TRXN_PENDING ." 
+                         AND ledger.docid = " . $_SESSION['docid'] . "
+                         AND trxn_code.docid = " . $_SESSION['docid'] . "
+                         AND trxn_code.type = " . DSS_TRXN_TYPE_MED;
+
+
+
+$myp=mysql_query($sqlp) or die(mysql_error());
+
+while($myarrayp = mysql_fetch_array($myp))
+                {
+                        if($myarray["status"] == 1)
+                        {
+                                $tr_class = "tr_active";
+                        }
+                        else
+                        {
+                                $tr_class = "tr_inactive";
+                        }
+                        $tr_class = "tr_active";
+                ?>
+                        <tr class="<?=$tr_class;?>">
+                                <td valign="top">
+                                        <?= $myarrayp['firstname'].' '.$myarrayp['lastname']; ?>
+                                </td>
+                                <td valign="top">
+        <button onclick="Javascript: window.location = 'insurance.php?pid=<?=$myarrayp['patientid'];?>';" class="addButton">
+                  Add New Claim
+            </button>
+
+                                </td>
+                        </tr>
+        <?      }
+?>
+</table>
+
+<span class="admin_head">Claims</span>
+<label style="margin-left:20px;">Filter by status</label> 
+<select onchange="updateClaims(this.value)">
+<option value="all">All</option>
+<option value="<?= DSS_CLAIM_PENDING; ?>"><?= $dss_claim_status_labels[DSS_CLAIM_PENDING]; ?></option>
+<option value="<?= DSS_CLAIM_PAID_INSURANCE; ?>"><?= $dss_claim_status_labels[DSS_CLAIM_PAID_INSURANCE]; ?></option>
+<option value="<?= DSS_CLAIM_SENT; ?>"><?= $dss_claim_status_labels[DSS_CLAIM_SENT]; ?></option>
+<option value="<?= DSS_CLAIM_DISPUTE; ?>"><?= $dss_claim_status_labels[DSS_CLAIM_DISPUTE]; ?></option>
+<option value="<?= DSS_CLAIM_REJECTED; ?>"><?= $dss_claim_status_labels[DSS_CLAIM_REJECTED]; ?></option>
+</select>
+
+<script type="text/javascript">
+
+function updateClaims(v){
+
+if(v == 'all'){
+  $('.claim').show();
+}else if(v == '<?= DSS_CLAIM_PENDING; ?>'){
+  $('.claim').hide();
+  $('.status_<?= DSS_CLAIM_PENDING; ?>').show();
+  $('.status_<?= DSS_CLAIM_SEC_PENDING; ?>').show();
+}else if(v == '<?= DSS_CLAIM_PAID_INSURANCE; ?>'){
+  $('.claim').hide();
+  $('.status_<?= DSS_CLAIM_PAID_INSURANCE; ?>').show();
+  $('.status_<?= DSS_CLAIM_PAID_SEC_INSURANCE; ?>').show();
+  $('.status_<?= DSS_CLAIM_PAID_PATIENT; ?>').show();
+}else if(v == '<?= DSS_CLAIM_SENT; ?>'){
+  $('.claim').hide();
+  $('.status_<?= DSS_CLAIM_SENT; ?>').show();
+  $('.status_<?= DSS_CLAIM_SEC_SENT; ?>').show();
+}else if(v == '<?= DSS_CLAIM_DISPUTE; ?>'){
+  $('.claim').hide();
+  $('.status_<?= DSS_CLAIM_DISPUTE; ?>').show();
+  $('.status_<?= DSS_CLAIM_SEC_DISPUTE; ?>').show();
+}else if(v == '<?= DSS_CLAIM_REJECTED; ?>'){
+  $('.claim').hide();
+  $('.status_<?= DSS_CLAIM_REJECTED; ?>').show();
+}
+
+}
+</script>
+
 <insurance name="sortfrm" action="<?=$_SERVER['PHP_SELF']?>" method="post">
 <table width="98%" cellpadding="5" cellspacing="1" bgcolor="#FFFFFF" align="center" >
 	<? if($total_rec > $rec_disp) {?>
@@ -91,7 +184,7 @@ $my=mysql_query($sql) or die(mysql_error());
 			}
 			$tr_class = "tr_active";
 		?>
-			<tr class="<?=$tr_class;?>">
+			<tr class="<?=$tr_class;?> status_<?= $myarray['status']; ?> claim">
 				<td valign="top">
                 	<?=date('m-d-Y H:i',strtotime(st($myarray["adddate"])));?>
 				</td>

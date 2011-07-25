eb4c115b5b2b4a07cc6c2b51967933410f0063e6
Andrew Rosborough <arosborough@acgnw.com>
2011-07-24 21:33:44 -0400

    Fields for letterhead images

---
+++ sql_updates/2011-07-24-19-54_dental_users-alter-letterhead.sql
@@ -0,0 +1,4 @@
+ALTER TABLE dental_users ADD email_header varchar(255);
+ALTER TABLE dental_users ADD email_footer varchar(255);
+ALTER TABLE dental_users ADD fax_header varchar(255);
+ALTER TABLE dental_users ADD fax_footer varchar(255);

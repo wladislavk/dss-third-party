e2a913da1244c3560bec27fd4e909228e49addf8
Adam Bert <abert@xforty.com>
2011-08-04 13:07:33 -0400

    Added encryption for admin

--- manage/admin/index.php
+++ manage/admin/index.php
@@ -1,10 +1,16 @@
 <?
 session_start();
 include('includes/config.php');
-
+include_once('includes/password.php');
 if($_POST["loginsub"] == 1)
 {
-	$check_sql = "SELECT * FROM admin where username='".$_POST['username']."' and password='".$_POST['password']."'";
+        $salt_sql = "SELECT salt FROM admin WHERE username='".mysql_real_escape_string($_POST['username'])."'";
+        $salt_q = mysql_query($salt_sql);
+        $salt_row = mysql_fetch_assoc($salt_q);
+      
+        $pass = gen_password($_POST['password'], $salt_row['salt']);
+
+	$check_sql = "SELECT * FROM admin where username='".mysql_real_escape_string($_POST['username'])."' and password='".$pass."'";
 	$check_my = mysql_query($check_sql) or die(mysql_error().' | '.$check_sql);
 	
 	if(mysql_num_rows($check_my) == 1) 
---
+++ sql_updates/2011-08-04-12-48_admin_encryption_salt.sql
@@ -0,0 +1,3 @@
+ALTER TABLE admin ADD COLUMN salt varchar(100);
+ALTER TABLE admin ADD COLUMN recover_hash varchar(100);
+ALTER TABLE admin ADD COLUMN recover_time datetime;

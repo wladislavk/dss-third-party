2c8684a306548e18a8fe18b6fe73273332f7051b
Andrew Rosborough <arosborough@acgnw.com>
2011-08-10 16:10:23 -0400

    Converted letter 18 from send letter o create memo

---
+++ manage/letter18.php
@@ -0,0 +1,18 @@
+<?php
+
+include('admin/includes/config.php');
+
+$letterid = '18';
+$patientid = $_GET['pid'];
+//$md_list = get_mdcontactids($patientid);
+//$md_referral_list = get_mdreferralids($patientid);
+$letter = create_letter($letterid, $patientid, '', '', '', '');
+if (!is_numeric($letter)) {
+  print $letter . "<br />";
+}
+
+?>
+
+<script type="text/javascript">
+  window.location = '/manage/index.php';
+</script>
--- manage/letter_cronjob.php
+++ manage/letter_cronjob.php
@@ -5,7 +5,7 @@ include('admin/includes/config.php');
 if ($_REQUEST['key'] != $cron_key) die();
 
 // Trigger Letter 12
-$lateconsult_query = "SELECT dental_flow_pg2_info.patientid, dental_patients.docid, dental_patients.salutation, dental_patients.firstname, dental_patients.lastname FROM dental_flow_pg2_info JOIN dental_patients ON dental_flow_pg2_info.patientid=dental_patients.patientid WHERE dental_flow_pg2_info.date_scheduled <= DATE_SUB(NOW(), INTERVAL 30 day) AND dental_flow_pg2_info.segmentid = '2' GROUP BY dental_flow_pg2_info.patientid;";
+$lateconsult_query = "SELECT dental_flow_pg2_info.patientid, dental_patients.docid, dental_patients.salutation, dental_patients.firstname, dental_patients.lastname FROM dental_flow_pg2_info JOIN dental_patients ON dental_flow_pg2_info.patientid=dental_patients.patientid WHERE dental_flow_pg2_info.date_scheduled <= DATE_SUB(NOW(), INTERVAL 30 day) AND dental_flow_pg2_info.date_completed = '0000-00-00' AND dental_flow_pg2_info.segmentid = '2' GROUP BY dental_flow_pg2_info.patientid;";
 $lateconsult_result = mysql_query($lateconsult_query);
 $patients = array();
 while ($row = mysql_fetch_assoc($lateconsult_result)) {
@@ -33,7 +33,7 @@ $pastdue_patients = array();
 foreach ($patients as $patient) {
   $steps = explode(",", $patient['steparray']);
   $current_step = count($steps);
-  $ddpastdue = "SELECT patientid, segmentid, date_completed FROM dental_flow_pg2_info WHERE patientid IN('".$patient['patientid']."') AND date_completed <= DATE_SUB(NOW(), INTERVAL 30 DAY) AND segmentid = '7' AND stepid = '".$current_step."';";
+  $ddpastdue = "SELECT dental_flow_pg2_info.patientid, dental_patients.docid, dental_patients.salutation, dental_patients.firstname, dental_patients.lastname, dental_flow_pg2_info.segmentid, dental_flow_pg2_info.date_completed FROM dental_flow_pg2_info JOIN dental_patients ON dental_flow_pg2_info.patientid=dental_patients.patientid WHERE patientid IN('".$patient['patientid']."') AND date_completed <= DATE_SUB(NOW(), INTERVAL 30 DAY) AND segmentid = '7' AND stepid = '".$current_step."';";
 //print $ddpastdue. "<br />";
   $ddpastdue_result = mysql_query($ddpastdue);
   while ($row = mysql_fetch_assoc($ddpastdue_result)) {
@@ -62,7 +62,7 @@ foreach ($casetwo_patients as $patient) {
   }
   foreach ($current_step as $step) {
     // SELECT previous device delivery segments completed more than 30 days ago
-    $ddquery = "SELECT patientid, segmentid, date_completed FROM dental_flow_pg2_info WHERE patientid IN('".$step['patientid']."') AND date_completed <= DATE_SUB(NOW(), INTERVAL 30 DAY) AND segmentid = '7';";
+    $ddquery = "SELECT dental_flow_pg2_info.patientid, dental_patients.docid, dental_patients.salutation, dental_patients.firstname, dental_patients.lastname, dental_flow_pg2_info.segmentid, dental_flow_pg2_info.date_completed FROM dental_flow_pg2_info JOIN dental_patients ON dental_flow_pg2_info.patientid=dental_patients.patientid WHERE patientid IN('".$step['patientid']."') AND date_completed <= DATE_SUB(NOW(), INTERVAL 30 DAY) AND segmentid = '7';";
     $ddresult = mysql_query($ddquery);
     while ($row = mysql_fetch_assoc($ddresult)) {
       $second_case_patients[] = $row;
@@ -95,8 +95,16 @@ foreach ($pastdue_patients as $patient) {
 }
 //print_r($segment_count);
 //print_r($unique_pids);
-$sentto = array();
+//$sentto = array();
 foreach ($letter18_patientlist as $patient) {
+  $user_id = $patient['docid'];
+  $memo = "Patient, " . $patient['salutation'] . " " . $patient['firstname'] . " " . $patient['lastname'] . ", has not been in for a device check appt within 30 days of delivery.  Click <a href=\"/manage/letter18.php?pid=" . $patient['patientid'] . "\">Yes</a> to send them a letter, or click <a href=\"/manage/manage_flowsheet3.php?pid=" . $patient['patientid'] . "&page=page2\">No</a> to view the patient's Flow Sheet.";
+  $memo = mysql_real_escape_string($memo); 
+  $memo_query = "INSERT INTO memo VALUES ('$user_id', '$memo', DATE_ADD(NOW(), INTERVAL 1 DAY));";
+  $memo_result = mysql_query($memo_query);
+	
+
+/*
   $letterid = '18';
   $topatient = '1';
   if (array_search($patient['patientid'], $sentto) === false) {
@@ -111,7 +119,7 @@ foreach ($letter18_patientlist as $patient) {
       }
       $sentto[] = $patient['patientid'];
     }
-  }
+  }*/
 }
 
 $letter21_query = "SELECT dental_flow_pg2_info.patientid, dental_flow_pg2_info.stepid, dental_flow_pg2_info.date_completed, dental_letters.letterid FROM dental_flow_pg2_info LEFT JOIN dental_letters ON (dental_flow_pg2_info.patientid=dental_letters.patientid AND dental_flow_pg2_info.stepid=dental_letters.stepid) WHERE dental_flow_pg2_info.segmentid = '7' AND date_completed <= DATE_SUB(NOW(), INTERVAL 350 DAY) AND dental_letters.letterid IS NULL;";

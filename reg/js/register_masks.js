a83d10c55d5d2667b6a2705f894e876fa2a735cb
Adam Bert <abert@xforty.com>
2012-03-15 13:40:45 -0400

    Input masks

--- reg/js/register_masks.js
+++ reg/js/register_masks.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
-	$('.phonemask').mask('(999) 999-9999? x99999');
+	$('.workphonemask').mask('(999) 999-9999? x99999');
+	$.mask.definitions['~']='[x1-9]';
+        $('.phonemask').mask('(999) 999-9999? ~99999');
 	$('.ssnmask').mask('999-99-9999');
 });
--- reg/register.php
+++ reg/register.php
@@ -75,10 +75,10 @@ include 'includes/header.php';
                         <label class="lbl_a"><strong>5.</strong> Home Phone:</label><input class="inpt_a phonemask" type="text" id="home_phone" name="home_phone" value="<?= $p['home_phone']; ?>" />
                 </div>
                 <div class="sepH_b third">
-                        <label class="lbl_a"><strong>6.</strong> Work Phone:</label><input class="inpt_a" type="text" id="work_phone" name="work_phone" value="<?= $p['work_phone']; ?>" />
+                        <label class="lbl_a"><strong>6.</strong> Work Phone:</label><input class="inpt_a workphonemask" type="text" id="work_phone" name="work_phone" value="<?= $p['work_phone']; ?>" />
                 </div>
                 <div class="sepH_b third">
-                        <label class="lbl_a"><strong>7.</strong> Cell Phone:</label><input class="inpt_a validate" type="text" id="cell_phone" name="cell_phone" value="<?= $p['cell_phone']; ?>" />
+                        <label class="lbl_a"><strong>7.</strong> Cell Phone:</label><input class="inpt_a validate phonemask" type="text" id="cell_phone" name="cell_phone" value="<?= $p['cell_phone']; ?>" />
                 </div>
                 <div class="sepH_b clear">
                         <label class="lbl_a"><strong>8.</strong> Address 1:</label><input class="inpt_a validate" type="text" name="add1" value="<?= $p['add1']; ?>" />

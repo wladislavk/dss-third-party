4a2373ebf2c9f23043bab60fc3f682e5a019f620
Adam Bert <abert@xforty.com>
2012-03-19 12:58:36 -0400

    PP activate bug fix

--- reg/activate.php
+++ reg/activate.php
@@ -1,7 +1,7 @@
 <?php include '../manage/admin/includes/config.php'; ?>
 <?php require_once("twilio/twilio.config.php");
 
-$s = "SELECT * FROM dental_patients dp JOIN dental_users du on du.userid=dp.docid 
+$s = "SELECT dp.email, dp.cell_phone FROM dental_patients dp JOIN dental_users du on du.userid=dp.docid 
 	WHERE dp.patientid='".mysql_real_escape_string($_GET['id'])."' AND
 		dp.recover_hash='".mysql_real_escape_string($_GET['hash'])."' AND
 		dp.use_patient_portal='1' AND

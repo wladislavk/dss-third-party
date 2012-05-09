f1908157ee4eecd12409eaee0cdb13cf59d8dd06
Adam Bert <abert@xforty.com>
2012-05-09 16:32:26 -0400

    Ticket #8396

--- manage/js/task.js
+++ manage/js/task.js
@@ -12,6 +12,7 @@ $('.task_status').click(function(){
                                                         x.find('td.status_col').remove();
 							$('.task_'+t).remove();
                                                         x.appendTo('#completed_tasks');
+							$('#task_count').html($('#task_count').html()-1);
                                                 }
                                         },
                                         failure: function(data){
@@ -29,3 +30,27 @@ $('.task_status').click(function(){
         var v = $(this).find('.task_extra').hide();
   });
 
+
+function delete_task(id){
+
+  if(confirm('Are you sure you want to delete this task?')){
+
+                                  $.ajax({
+                                        url: "includes/delete_task.php",
+                                        type: "post",
+                                        data: {id: id},
+                                        success: function(data){
+                                                var r = $.parseJSON(data);
+                                                if(r.error){
+                                                }else{
+                                                        $('.task_'+id).remove();
+							$('#task_count').html($('#task_count').html()-1);
+                                                }
+                                        },
+                                        failure: function(data){
+                                                //alert('fail');
+                                        }
+                                  });
+
+  }
+}

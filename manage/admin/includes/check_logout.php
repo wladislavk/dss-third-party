2c9500d8fbbf345bc4e4c1dbc8aa5b3156975829
Adam Bert <abert@xforty.com>
2012-09-06 09:46:46 -0400

    Ticket 9178. Added logout check to admin section

---
+++ manage/admin/includes/check_logout.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require_once './config.php';
+$id = $_SESSION['adminuserid'];
+$logout_time = 3*60*60; // 3 hours in seconds
+$s = "SELECT last_accessed_date FROM admin
+	WHERE adminid='".mysql_real_escape_string($id)."'";
+$q = mysql_query($s);
+$r = mysql_fetch_assoc($q);
+$lat = strtotime($r['last_accessed_date']);
+$now = time();
+if($lat > $now - $logout_time){
+  $rt = ($logout_time - ($now - $lat))*1000;	
+  echo '{"reset_time":'.$rt.'}';
+}else{
+  echo '{"logout":true}';
+}
+?>

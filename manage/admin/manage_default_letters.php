256daeda2df55858ebd2f0b7add7f5f6e3a0e2b0
Adam Bert <abert@xforty.com>
2012-11-16 17:09:25 -0500

    Ticket #9504. letter templates

--- manage/admin/manage_default_letters.php
+++ manage/admin/manage_default_letters.php
@@ -24,12 +24,24 @@ die();
 	<option value="<?= $r['id']; ?>"><?= $r['id']." - ".$r['name']; ?></option>
   <?php } ?>
 </select>
-
+<br /><br />
+<textarea id="letter"></textarea>
 <br /><br />	
 <? include "includes/bottom.htm";?>
 
 
 <script type="text/javascript">
+
+                tinyMCE.init({
+                        mode : "textareas",
+                        theme : "advanced",
+                        theme_advanced_buttons1 : "bold,italic,underline, separator, bullist ,numlist, separator,justifyleft, justifycenter,justifyright,  justifyfull, separator,help",
+                        theme_advanced_buttons2 : "",
+                        theme_advanced_buttons3 : "",
+                        gecko_spellcheck : true,
+                        theme_advanced_toolbar_location : "top",
+                        theme_advanced_toolbar_align : "left"
+                });
 
 $('#letterid').change(function(){
   id = $(this).val();
@@ -41,7 +53,7 @@ $('#letterid').change(function(){
       var r = $.parseJSON(data);
       if(r.error){
       }else{
-	alert(r.body);
+	$('#tinymce').html(r.body);
       }
     },
     failure: function(data){

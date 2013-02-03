be4284d8191935f2d2e5a42a0816fea608800dd8
Adam Bert <abert@xforty.com>
2013-02-03 16:58:34 -0500

    Stripe mask on amount

--- manage/admin/percase_bill.php
+++ manage/admin/percase_bill.php
@@ -48,4 +48,27 @@ Amount to charge credit card for <?= $r['name']; ?> $<input type="text" id="amou
     a = $('#amount').val();
     return confirm("Credit card for <?= $r['name']; ?> will be charged $"+a+". Proceed?");
   }
+
+  $(document).ready(function() {
+    $("#amount").keydown(function(event) {
+        // Allow: backspace, delete, tab, escape, and enter
+        if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 || 
+             // Allow: Ctrl+A
+            (event.keyCode == 65 && event.ctrlKey === true) ||
+
+	    (event.keyCode == 190 ||  event.keyCode == 110) || 
+             // Allow: home, end, left, right
+            (event.keyCode >= 35 && event.keyCode <= 39)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        else {
+            // Ensure that it is a number and stop the keypress
+            if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                event.preventDefault(); 
+            }   
+        }
+    });
+});
+
 </script>

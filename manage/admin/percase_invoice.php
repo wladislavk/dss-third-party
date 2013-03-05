fa8b4723b7cf736091e65546f341f8036474d08a
Adam Bert <abert@xforty.com>
2013-03-05 13:27:19 -0500

    percase remove fix

--- manage/admin/percase_invoice.php
+++ manage/admin/percase_invoice.php
@@ -152,7 +152,7 @@ row += '<input type="text" name="extra_name_'+row_count+'" value="" />';
 row += '</td><td valign="top">';
 row += '<input type="text" name="extra_service_date_'+row_count+'" value="<?=date('m/d/Y');?>" />';
 row += '</td><td valign="top">';
-row += '<a href="#" onclick="$(\'#extra_row_'+row_count+'\').remove()">Remove</a>';
+row += '<a href="#" onclick="$(\'#extra_row_'+row_count+'\').remove(); calcTotal();">Remove</a>';
 row += '</td><td valign="top">';
 row += '$<input type="text" class="amount" name="extra_amount_'+row_count+'" value="195.00" />';
 row += '</td></tr>';

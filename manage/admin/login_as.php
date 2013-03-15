76f13140a260581b259b61a885e248045d686280
Adam Bert <abert@xforty.com>
2013-03-15 15:23:58 -0400

    Fix to login as

--- manage/admin/login_as.php
+++ manage/admin/login_as.php
@@ -2,7 +2,10 @@
 	session_start();
         require_once('includes/config.php');
 	require_once('includes/sescheck.php');
-        $check_sql = "SELECT userid, username, name, user_access, docid FROM dental_users where username='".$_POST['username']."'";
+        //$check_sql = "SELECT userid, username, name, user_access, docid FROM dental_users where username='".$_POST['username']."'";
+        $check_sql = "SELECT dental_users.userid, username, name, user_access, docid, user_type, uc.companyid FROM dental_users 
+                        LEFT JOIN dental_user_company uc ON dental_users.userid=uc.userid
+                        where username='".mysql_real_escape_string($_POST['username'])."'";
         $check_my = mysql_query($check_sql);
         if(mysql_num_rows($check_my) == 1)
         {
@@ -17,11 +20,16 @@
                 session_register("name");
                 session_register("user_access");
                 session_register("docid");
+                session_register("companyid");
+                session_register("user_type");
+
 
                 $_SESSION['userid']=$check_myarray['userid'];
                 $_SESSION['username']=$check_myarray['username'];
                 $_SESSION['name']=$check_myarray['name'];
                 $_SESSION['user_access']=$check_myarray['user_access'];
+                $_SESSION['companyid']=$check_myarray['companyid'];
+                $_SESSION['user_type']=$check_myarray['user_type'];
                 if($check_myarray['docid'] != 0)
                 {
                         $_SESSION['docid']=$check_myarray['docid'];

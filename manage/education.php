84b78b012cd449b37c55d8cb643ec9fd5e55592f
Adam Bert <abert@xforty.com>
2013-05-23 13:06:06 -0400

    education

---
+++ manage/education.php
@@ -0,0 +1,83 @@
+<? 
+include "includes/top.htm";
+include_once "includes/constants.inc";
+?>
+
+<div style="padding:0 30px;">
+<h1>Education</h1>
+&nbsp;
+
+<h2>Fundamentals of Dental Sleep Medicine</h2>
+When we treat patients suffering from sleep apnea we cross over into the realm of medicine, and we need to learn the language in order to communicate with our physician colleagues.  Learn the basics here, from sleep to testing to treatment.
+<em>Time: 35 Minutes</em><br />
+<iframe src="https://player.vimeo.com/video/65686921" width="500" height="281" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+
+&nbsp;
+
+<h2>Screening for Sleep Disordered Breathing</h2>
+Patients have been walking through our doors for years NOT knowing that they are suffering from a deadly disease.  Learn effective ways to screen patients for sleep and breathing disorders, things you can say to get the patient to acknowledge that he may have a problem, and how to lead them down the path toward testing and treatment.
+<em>Time:  50 minutes</em><br />
+<iframe src="https://player.vimeo.com/video/65784954" height="281" width="500" allowfullscreen="" frameborder="0"></iframe>
+
+&nbsp;
+
+<h2>Consultation</h2>
+We've been consulting with patients for years about how and why to fix teeth; treating sleep apnea is different and requires that you do some homework.  Learn a process to efficiently evaluate patients for candidacy for dental device therapy and how to steer patients toward appropriate treatment.
+<em>Time 58 minutes</em><br />
+<iframe src="https://player.vimeo.com/video/65683866" height="281" width="500" allowfullscreen="" frameborder="0"></iframe>
+
+&nbsp;
+
+<h2>Treatment Options for Sleep Disordered Breathing</h2>
+What are my options for treating sleep apnea, Doctor?  Learn the options, the ins and outs, the ups and downs, the advantages and disadvantages of a wide scope of treatment options.
+<em>Time: 37 minutes</em><br />
+<iframe src="https://player.vimeo.com/video/65688339" height="281" width="500" allowfullscreen="" frameborder="0"></iframe>
+
+&nbsp;
+
+<h2>Comprehensive Exam and Impressions</h2>
+Learn about "the other things" that you should look for, evaluate, and document, with regards to upper airway and dental device treatment, as well as tips and tricks for getting accurate impressions for your devices.
+<em>Time: 37 minutes</em><br />
+<iframe src="https://player.vimeo.com/video/65686917" height="281" width="500" allowfullscreen="" frameborder="0"></iframe>
+
+&nbsp;
+
+<h2>Device Delivery</h2>
+Though each dental device requires certain things, most devices share many commonalities.  Learn proper protocols and procedures to produce consistent results; know what to say BEFORE the patient goes home with your device to help dramatically increase your treatment success.
+<em>Time: 20 minutes</em><br />
+<iframe src="http://player.vimeo.com/video/65686916" height="281" width="500" allowfullscreen="" frameborder="0"></iframe>
+
+&nbsp;
+
+<h2>Managing Side Effects</h2>
+Dental device therapy will cause side effects.  Get over it!  Learn specific tricks on how to deal with specific problems.  You should NOT practice dental sleep medicine without knowing this material inside and out.
+<em>Time: 30 minutes</em><br />
+<iframe src="https://player.vimeo.com/video/65688341" height="281" width="500" allowfullscreen="" frameborder="0"></iframe>
+
+&nbsp;
+
+<h2>Follow Up and Home Sleep Testing</h2>
+Dental device treatment requires continual adjustment and feedback.  Deliver the device.  Now what?  Learn what to document, how to evaluate effectiveness, and when to proceed to the next step of home sleep testing.
+<em>Time: 35 minutes</em><br />
+<iframe src="https://player.vimeo.com/video/65686919" height="281" width="500" allowfullscreen="" frameborder="0"></iframe>
+
+&nbsp;
+
+<h2>Treatment Complete and Annual Recall</h2>
+Learn how to decide when you have titrated your device far enough, when you have reached "maximum medical improvement", what constitutes successful treatment,  and what to review at each annual recall appointment.
+<em>Time: 7 minutes</em><br />
+<iframe src="https://player.vimeo.com/video/65784955" height="281" width="500" allowfullscreen="" frameborder="0"></iframe>
+
+
+
+</div>
+
+<a href="course.php" target="_blank" style="margin-left: 30px;"  class="addButton">Course</a>
+
+
+<?php
+
+include "includes/bottom.htm";
+
+?>
+

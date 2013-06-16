65f067ef84a9f7b11ee4d04d4c7c9409535bd9bc
Darin Avery <davery@dss.xforty.com>
2013-06-16 19:54:08 -0400

    added appt types

---
+++ sql_updates/2013-06-16-19-53_appt-types.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 5.1.67, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: dentalsl_main_dev
+-- ------------------------------------------------------
+-- Server version	5.1.67-0ubuntu0.10.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `dental_appt_types`
+--
+
+DROP TABLE IF EXISTS `dental_appt_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dental_appt_types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `color` varchar(255) DEFAULT NULL,
+  `classname` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `classname` (`classname`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dental_appt_types`
+--
+
+LOCK TABLES `dental_appt_types` WRITE;
+/*!40000 ALTER TABLE `dental_appt_types` DISABLE KEYS */;
+INSERT INTO `dental_appt_types` VALUES (1,'General','FFF9CF','general'),(2,'Follow-up','D6CFFF','follow-up'),(3,'Sleep Test','CFF5FF','sleep_test'),(4,'Impressions','DFFFCF','impressions'),(5,'New Pt','FFCFCF','new_pt'),(6,'Deliver Device','FBA16C','deliver_device');
+/*!40000 ALTER TABLE `dental_appt_types` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-06-16 19:53:06

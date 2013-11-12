7a43fdf604af14f0adeefb0c49a41fcc93552a57
Adam Bert <abert@xforty.com>
2013-11-12 11:46:18 -0500

    Ticket 11147. Eligible API updated list to use web service instead of DSS database.

--- manage/list_ins_payers.php
+++ manage/list_ins_payers.php
@@ -13,23 +13,29 @@ if (isset($_POST['partial_name'])) {
 }
 
 
-$sql = "SELECT p.id, p.name, p.payer_id "
+/*$sql = "SELECT p.id, p.name, p.payer_id "
   .			" FROM dental_ins_payer p"
   .			" WHERE name LIKE '%" . $partial . "%' "
         .               " ORDER BY name ASC";
 $result = mysql_query($sql);
+*/
 
+$data = json_decode(file_get_contents("https://eligibleapi.com/resources/claims-payer.json"));
+
+$payers = $data->claims_payer_list;
 $patients = array();
-$i = 0;
-while ($row = mysql_fetch_assoc($result)) {
-  $patients[$i]['id'] = $row['payer_id']."-".$row['name'];
-  $patients[$i]['name'] = $row['payer_id'] ." - ".$row['name'];
+$i = 0; 
+foreach ($payers as $payer) {
+        if (strstr(strtolower($payer->payer_name), strtolower($partial))) {
+
+  $patients[$i]['id'] = $payer->payer_id."-".$payer->payer_name;
+  $patients[$i]['name'] = $payer->payer_id ." - ".$payer->payer_name;
   //$patients[$i]['source'] = $row['referral_type'];
   $i++;
 }
-
+}
 if (!$result) {
-	$patients = array("error" => $sql."Error: Could not select patients from database");
+	//$patients = array("error" => $sql."Error: Could not select patients from database");
 }
 
 echo json_encode($patients);

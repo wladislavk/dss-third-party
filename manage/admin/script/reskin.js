a52ec8b9963908e1c64e24715a810411fd47cbfa
AstDerek <ast.derek@gmail.com>
2014-02-07 10:25:13 -0600

    Header, footer and menu are now bootstrap compatible

--- manage/admin/script/reskin.js
+++ manage/admin/script/reskin.js
@@ -23,67 +23,7 @@ $(function(){
     $('.btn:icontains(edit)').removeClass('btn-success').addClass('btn-primary').append('<span>&nbsp;</span><span class="glyphicon glyphicon-pencil"/>');
     $('.btn:icontains(create), .btn:icontains("add ")').removeClass('btn-default').addClass('btn-success').append('<span>&nbsp;</span><span class="glyphicon glyphicon-plus"/>');
     $('.btn:icontains(delete), .btn:icontains(remove)').removeClass('btn-default').addClass('btn-danger').append('<span>&nbsp;</span><span class="glyphicon glyphicon-remove"/>');
-    
-    /**
-     * Main table/content
-     */
-    $('table:first').each(function(){
-        var $this = $(this),
-        tr = $this.find('>tbody>tr')
-        header = tr.eq(0),
-        menu = tr.eq(1),
-        content = tr.eq(2),
-        footer = tr.eq(3),
-        navbar = $('<div class="navbar navbar-default navbar-fixed-top"><div class="container"/></div>'),
-        container = $('<div class="container"/>');
-        
-        $this.removeAttr('align cellspacing cellpadding class').wrap('<div class="container"/>');
-        $this.attr({
-            border: 0,
-            width: '100%'
-        });
-        
-        header.find('td').removeClass('header_bg')
-        .contents().wrapAll('<div class="well"/>')
-        .eq(0).wrap('<h1/>');
-        
-        header.find('a').addClass('btn btn-default');
-        
-        footer.find('td').removeClass('bottom_bg')
-        .contents().wrapAll('<div class="well text-center"/>')
-    });
     $('table:not(:first)').addClass('table table-bordered');
-    
-    /**
-     * Dropdown
-     */
-    $('ul.dropdown.dropdown-horizontal').each(function(){
-        var $this = $(this);
-        
-        $this.find('ul, li').andSelf().removeAttr('class').css({
-            display: '',
-            visibility: ''
-        });
-        $this.addClass('nav nav-tabs');
-        
-        $this.find('>li').has('ul').addClass('dropdown');
-        $this.find('li:not(.dropdown)').has('ul').addClass('dropdown-submenu');
-        $this.find('ul').addClass('dropdown-menu');
-        
-        $this.find('li').not($this.find('li a').closest('li')).each(function(){
-            var extras = '', current = $(this);
-            
-            if (current.parent().is($this)) {
-                extras = 'data-toggle="dropdown" class="dropdown-toggle"';
-            }
-            
-            current.contents().eq(0).wrap('<a href="#" ' + extras + '/>');
-            
-            if (current.parent().is($this)) {
-                current.find('a:first').append('<span>&nbsp;</span><span class="caret"/>');
-            }
-        });
-    });
     
     /**
      * Append dropdown to change skin

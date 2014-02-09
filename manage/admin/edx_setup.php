3ef94508fced98f637c55670f94b4f442399f4cc
Adam Bert <abert@xforty.com>
2014-02-09 13:03:52 -0500

    EdX updates. Removed --data-urlencode from shell scripts

--- manage/admin/edx_setup.php
+++ manage/admin/edx_setup.php
@@ -1,12 +1,12 @@
 <?php
-  include 'admin/includes/main_include.php';
-  include 'includes/edx_functions.php';
+  include 'includes/main_include.php';
+  include '../includes/edx_functions.php';
 
   $u_sql = "SELECT * FROM dental_users";
   $u_q = mysql_query($u_sql);
   while($u = mysql_fetch_assoc($u_q)){ 
     echo $u['username']."|".$u['edx_id']."|"."<br />";
-    edx_user_update($u['userid'], $edx_con);
+    //edx_user_update($u['userid']);
 
 
 

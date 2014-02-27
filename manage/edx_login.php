07b5d269a5b2e70e876dd09012a49128ef4db3c0
Adam Bert <abert@xforty.com>
2014-02-27 16:50:41 -0500

    fixed edx_login redirects

--- manage/edx_login.php
+++ manage/edx_login.php
@@ -8,7 +8,7 @@ $u = mysql_fetch_assoc($u_q);
 $userid = $u['edx_id'];
 $ses = shell_exec('sh edx_scripts/edxScript.sh '.urlencode($userid));
 $expire=time()+60*60*2;
-if($_SERVER['HTTP_HOST']=='dentalsleepsolutions.com'){
+if($_SERVER['HTTP_HOST']=='www.dentalsleepsolutions.com' || $_SERVER['HTTP_HOST']=='dentalsleepsolutions.com'){
   setcookie("edxloggin", "true", $expire, "/", "dentalsleepsolutions.com", false);
   setcookie("sessionid", $ses, $expire, "/", "dentalsleepsolutions.com", false);
 }else{
@@ -18,11 +18,13 @@ if($_SERVER['HTTP_HOST']=='dentalsleepsolutions.com'){
 ?>
 <script type="text/javascript">
 <?php
-                if($_SERVER['HTTP_HOST']=='www.dentalsleepsolutions.com'){
+                if($_SERVER['HTTP_HOST']=='www.dentalsleepsolutions.com' || $_SERVER['HTTP_HOST']=='dentalsleepsolutions.com'){
 ?>  window.location='http://education.dentalsleepsolutions.com/dashboard'; <?php
-                }else{
+                }elseif($_SERVER['HTTP_HOST']=='preprod.dss.xforty.com' || $_SERVER['HTTP_HOST']=='staging.dss-rh.xforty.com'){
 ?>  window.location='http://preprod.edx.dss.xforty.com/dashboard'; <?php
-                }
+                }else{
+?>  window.location='http://staging1.edx.dss.xforty.com/dashboard'; <?php
+		}
 ?>
 </script>
 

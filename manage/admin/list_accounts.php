43767b83e13ad4574ef7b8d9d27956952464c52e
Adam Bert <abert@xforty.com>
2014-10-03 14:52:00 -0400

    added missing search files

---
+++ manage/admin/list_accounts.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once('includes/main_include.php');
+include("includes/sescheck.php");
+require_once("includes/general.htm");
+require_once('../includes/constants.inc');
+require_once('../includes/formatters.php');
+
+if (isset($_POST['partial_name'])) {
+	$partial = $_POST['partial_name'];
+	$partial = ereg_replace("[^ A-Za-z'\-]", "", $partial);
+	$partial = s_for($partial);
+}
+$names = explode(" ", $partial);
+
+$sql = "SELECT u.userid, u.last_name, u.first_name"
+  .			" FROM dental_users u"
+  .			" WHERE (((last_name LIKE '" . $names[0] . "%' OR first_name LIKE '" . $names[0] . "%')"
+        .               " AND (last_name LIKE '" . $names[1] . "%' OR first_name LIKE '" . $names[1] . "%'))"
+        .               " OR (first_name LIKE '" . $names[0] ."%' AND last_name LIKE '" . $names[1] . "%'))"
+	.		" AND u.docid=0"
+	.		" ORDER BY last_name ASC";
+$result = mysql_query($sql);
+
+$patients = array();
+$i = 0;
+while ($row = mysql_fetch_assoc($result)) {
+  $patients[$i]['id'] = $row['userid'];
+  $patients[$i]['name'] = $row['last_name'].", ".$row['first_name'];
+  $i++;
+}
+
+if (!$result) {
+	$patients = array("error" => $sql."Error: Could not select users from database");
+}
+
+echo json_encode($patients);
+
+?>

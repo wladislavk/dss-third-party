2fc3d8b45eda85ee10a13e36188c08358b485a65
Daryl Boyer <dboyer@xforty.com>
2015-04-07 09:48:43 -0400

    fixed eligible check submit button enable/disable states

--- manage/admin/js/eligible_check.js
+++ manage/admin/js/eligible_check.js
@@ -1,15 +1,26 @@
 function disable_submit (){
+    console.log("disabled");
     $('#submit-button').css('background-color', '#999999');
-    $('#submit-button').disabled = true;
-    $('#submit-button-inner').replaceWith('<img src="/manage/images/DSS-ajax-animated_loading-gif.gif"></img>');
+    $('#submit-button').prop("disabled",true);
+    $('#submit-button-inner').replaceWith('<img id="submit-button-inner" src="/manage/images/DSS-ajax-animated_loading-gif.gif"></img>');
+
+}
+
+function enable_submit (){
+    console.log("enabled");
+    $('#submit-button').css('background-color', '#428bca');
+    $('#submit-button').prop("disabled",false);
+    $('#submit-button-inner').replaceWith('<div id="submit-button-inner">Submit</div>');
 }
 
 $(document).ready(function(){
   var api_key = <?php echo "'".$api_key."'" ?>;
   setup_autocomplete_local('payer_name', 'ins_payer_hints', 'payer_id', '', 'https://gds.eligibleapi.com/v1.5/payers.json?api_key='+api_key, 'ins_payer', '', true, false);
+  $('#submit-button').on("click",null ,null , disable_submit);
 });
 
 function view_coverage(response){
+    enable_submit();
 	var coverage = new Coverage(response);
 	
 	if (coverage.hasError()) {

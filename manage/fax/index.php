e5493e52dc95bd02585f58b1043483c905c1d451
Grigori Tishkin <tishkin_gr@elance-odesk.com>
2015-05-12 22:16:27 +0400

    dentalsleepsolutions/ds3-private02#77 Remove namespaces  from SFax

--- manage/fax/index.php
+++ manage/fax/index.php
@@ -1,4 +1,4 @@
-<?php namespace Ds3\Libraries\Legacy; ?><?php 
+<?php
 
 include 'class.fax.php';
                 $faxNumber = "18883935968";                                           //<--- IMPORTANT: Enter a valid fax number
--- manage/fax/padCrypt.php
+++ manage/fax/padCrypt.php
@@ -1,4 +1,4 @@
-<?php namespace Ds3\Libraries\Legacy; ?>// Common to All
+// Common to All
 <?php
 /**
  * padCrypt.php

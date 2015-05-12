f54591767662df07200179c03186b1c4b66de0be
Grigori Tishkin <tishkin_gr@elance-odesk.com>
2015-05-12 22:05:14 +0400

    dentalsleepsolutions/ds3-private02#77 Remove namespaces  from thomasjbradley

--- manage/3rdParty/thomasjbradley-signature-to-image/examples/signature-from-text/index.php
+++ manage/3rdParty/thomasjbradley-signature-to-image/examples/signature-from-text/index.php
@@ -1,4 +1,4 @@
-<?php namespace Ds3\Libraries\Legacy; ?><?php
+<?php
 
 header('Content-type: image/png');
 
--- manage/3rdParty/thomasjbradley-signature-to-image/signature-to-image.php
+++ manage/3rdParty/thomasjbradley-signature-to-image/signature-to-image.php
@@ -1,4 +1,4 @@
-<?php namespace Ds3\Libraries\Legacy; ?><?php
+<?php
 /**
  *  Signature to Image: A supplemental script for Signature Pad that
  *  generates an image of the signature’s JSON output server-side using PHP.

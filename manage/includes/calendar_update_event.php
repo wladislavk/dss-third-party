68aa4a1cd6b519ad7a31714c52a3d75ee0cb06cb
AstDerek <ast.derek@gmail.com>
2015-07-15 14:23:31 -0500

    dentalsleepsolutions/ds3-private02#231 NewRelic Errors, update calendar and sign letters

--- manage/includes/calendar_update_event.php
+++ manage/includes/calendar_update_event.php
@@ -1,40 +1,40 @@
 <?php namespace Ds3\Libraries\Legacy; ?><?php
-	$docid = $_SESSION['docid'];
-	$sd = $_POST['start_date'];
-	$ed = $_POST['end_date'];
-	$de = $_POST['description'];
-	$cat = $_POST['category'];
-	$pi = $_POST['producer'];
-	$pid = $_POST['patient'];
-	$e_id = $_POST['e_id'];
-	$t_id = $_POST['t_id'];
-	$res = $_POST['resource'];
-	$r_type = ($_POST['rec_type']!='null')?$_POST['rec_type']:''; //to fix bug with string 'null' 
-	$r_pattern = ($_POST['rec_pattern']!='null')?$_POST['rec_pattern']:'';
-	$e_length = empty($_POST['elength']) ? "''" : $_POST['elength'];
-	$e_pid = empty($_POST['epid']) ? "''" : $_POST['epid'];
 
-	include_once '../admin/includes/main_include.php';
-	include_once 'checkemail.php';
+include_once '../admin/includes/main_include.php';
+include_once 'checkemail.php';
 
-	$s = "UPDATE dental_calendar SET
-		start_date='".$sd."',
-		end_date='".$ed."',
-		event_id='".$e_id."',
-		description='".mysqli_real_escape_string($con,$de)."',
-		category='".$cat."',
-		producer_id=".$pi.",
-		res_id=".$res.",
-		rec_type='".$r_type."',
-	  rec_pattern='".$r_pattern."',
-		event_length=".$e_length.",
-		event_pid=".$e_pid.",
-		patientid=".$pid."
-		WHERE event_id='".$e_id."'";
+$docid = mysqli_real_escape_string($con, $_SESSION['docid']);
+$sd = mysqli_real_escape_string($con, $_POST['start_date']);
+$ed = mysqli_real_escape_string($con, $_POST['end_date']);
+$de = mysqli_real_escape_string($con, $_POST['description']);
+$cat = mysqli_real_escape_string($con, $_POST['category']);
+$pi = mysqli_real_escape_string($con, $_POST['producer']);
+$pid = mysqli_real_escape_string($con, $_POST['patient']);
+$e_id = mysqli_real_escape_string($con, $_POST['e_id']);
+$t_id = mysqli_real_escape_string($con, $_POST['t_id']);
+$res = mysqli_real_escape_string($con, $_POST['resource']);
+$r_type = mysqli_real_escape_string($con, ($_POST['rec_type'] != 'null') ? $_POST['rec_type'] : ''); //to fix bug with string 'null'
+$r_pattern = mysqli_real_escape_string($con, ($_POST['rec_pattern'] != 'null') ? $_POST['rec_pattern'] : '');
+$e_length = mysqli_real_escape_string($con, empty($_POST['elength']) ? '' : $_POST['elength']);
+$e_pid = mysqli_real_escape_string($con, empty($_POST['epid']) ? '' : $_POST['epid']);
 
-	if($db->query($s)) {
-	  echo '{"success":true, "eventid":"' . $e_id .'"}';
-	} else {
-	  echo '{"error":true}';
-	}
-?>
+$s = "UPDATE dental_calendar SET
+        start_date = '$sd',
+        end_date = '$ed',
+        event_id = '$e_id',
+        description = '$de',
+        category = '$cat',
+        producer_id = '$pi',
+        res_id = '$res',
+        rec_type = '$r_type',
+        rec_pattern = '$r_pattern',
+        event_length = '$e_length',
+        event_pid = '$e_pid',
+        patientid = '$pid'
+    WHERE event_id = '$e_id'";
+
+if ($db->query($s)) {
+    echo '{"success":true, "eventid":"' . $e_id .'"}';
+} else {
+    echo '{"error":true}';
+}

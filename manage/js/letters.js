a77c93aa7dbe3d2aa6a77a8abbf93bd05cd48bf0
Sergey <gerashchenko@ya.ru>
2015-07-15 11:59:55 +0300

    dentalsleepsolutions/ds3-private02#204 Bugs are fixed

--- manage/js/letters.js
+++ manage/js/letters.js
@@ -18,23 +18,24 @@ function delete_pending_letter(lid, type, rid, par){
     }
 }
 
-
-$('.mailed_chk').click( function(){
-lid = $(this).val();
-c = $(this).is(':checked');
-    $.ajax({
-        url: "includes/letter_mail.php",
-        type: "post",
-        data: {lid: lid, mailed: c},
-        success: function(data){
-                var r = $.parseJSON(data);
-                if(r.error){
-                }else{
-                        //window.location.reload();
-                }
-        },
-        failure: function(data){
-                //alert('fail');
-        }
+$(document).ready(function(){
+    $('.mailed_chk').click( function(){
+    lid = $(this).val();
+    c = $(this).is(':checked');
+        $.ajax({
+            url: "includes/letter_mail.php",
+            type: "post",
+            data: {lid: lid, mailed: c},
+            success: function(data){
+                    var r = $.parseJSON(data);
+                    if(r.error){
+                    }else{
+                            //window.location.reload();
+                    }
+            },
+            failure: function(data){
+                    //alert('fail');
+            }
+        });
     });
 });

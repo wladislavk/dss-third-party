0993880d95515c2b4bae4d82911a7e3cad79b693
AstDerek <ast.derek@gmail.com>
2015-07-17 14:08:23 -0500

    dentalsleepsolutions/ds3-private02#223 Progres Notes, fix to popup from several places

--- manage/select_custom_all.php
+++ manage/select_custom_all.php
@@ -1,22 +1,38 @@
 <?php namespace Ds3\Libraries\Legacy; ?><?php 
 	include "admin/includes/main_include.php";
 
-	if(!empty($_POST['selsub']) && $_POST['selsub'] == 1) {
-?>
+	if (!empty($_POST['selsub']) && $_POST['selsub'] == 1) { ?>
 		<script type="text/javascript">
-			parent.document.<?php echo $_GET['fr'];?>.<?php echo $_GET['tx'];?>.value = '<?php echo addslashes(preg_replace('/\r\n|\n\r/',' ', $_POST['description']));?>';
+			parent.document[<?= json_encode($_GET['fr']) ?>][<?= json_encode($_GET['tx']) ?>].value =
+                <?= json_encode($_POST['description']) ?>;
 			parent.disablePopupRefClean();
 		</script>
-<?php
+        <?php
 		trigger_error("Die called", E_USER_ERROR);
 	}
 
-	$sql = "select * from dental_custom where docid='".$_SESSION['docid']."' order by Title";
-	
-	$my = $db->getResults($sql);
-	$total_rec = count($my);
-?>
+$sql = "select * from dental_custom where docid='".$_SESSION['docid']."' order by Title";
+$my = $db->getResults($sql);
 
+$customNotes = [];
+$currentNote = null;
+$notes = '';
+
+if ($my) {
+    foreach ($my as $myarray) {
+        $customNotes []= [
+            'title' => trim(utf8_encode($myarray['title'])),
+            'description' => trim(utf8_encode($myarray['description']))
+        ];
+    }
+
+    if (isset($_GET['title']) && isset($customNotes[$_GET['title']])) {
+        $currentNote = $_GET['title'];
+        $notes = $customNotes[$currentNote]['description'];
+    }
+}
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
@@ -26,6 +42,10 @@
 		<link href="css/admin.css" rel="stylesheet" type="text/css" />
 		<script type="text/javascript" src="/manage/admin/script/jquery-1.6.2.min.js"></script>
 		<script language="javascript" type="text/javascript" src="script/validation.js"></script>
+        <script type="text/javascript">
+            var customNotes = <?= json_encode($customNotes) ?>;
+        </script>
+        <script type="text/javascript" src="/manage/js/select_custom.js?v=<?= time() ?>"></script>
 	</head>
 
 	<body>
@@ -37,51 +57,34 @@
 						Select Custom Text
 					</span>
 					<br />&nbsp;
-
-					<?php
-						$title_arr = array();
-						$desc_arr = array();
-
-						if ($my) foreach ($my as $myarray) {
-							$title_arr[] = st($myarray['title']);
-							$desc_arr[] = st($myarray['description']);
-						}
-
-						$title_str = json_encode($title_arr);
-						$desc_str = json_encode($desc_arr);
-					?>
-
-					<script type="text/javascript">
-						var title_arr = <?php echo $title_str; ?>;
-						var desc_arr = <?php echo $desc_str; ?>;
-					</script>
-
-					<script type="text/javascript" src="/manage/js/select_custom.js"></script>
-
-					<form name="selfrm" action="<?php echo $_SERVER['PHP_SELF']?>?fr=<?php echo (!empty($_GET['fr']) ? $_GET['fr'] : '');?>&tx=<?php echo (!empty($_GET['tx']) ? $_GET['tx'] : '');?>" method="post" onSubmit="return selabc(this)">
-						<table width="98%" cellpadding="5" cellspacing="1" bgcolor="#FFFFFF" align="center" >
-							<tr class="tr_bg_h">
-								<td valign="top" class="col_head" >
-									Title: 
-						            <select name="title" class="tbox" onChange="change_desc(this.value)">
-						            	<option value="">Select</option>
-						                <?php 
-											$j = 0;
-											if ($my) foreach ($my as $myarray) {
-										?>
-												<option value="<?php echo $j;?>">
-							                    	<?php echo st($myarray['title']);?>
-							                    </option>
-										<?php 
-												$j++;
-											}
-										?>
-						            </select>
-								</td>
-							</tr>
+                    <form name="selfrm" action="?">
+                        <input type="hidden" name="fr" value="<?= htmlspecialchars($_GET['fr']) ?>" />
+                        <input type="hidden" name="tx" value="<?= htmlspecialchars($_GET['tx']) ?>" />
+                        <table width="98%" cellpadding="5" cellspacing="1" bgcolor="#FFFFFF" align="center" >
+                            <tr class="tr_bg_h">
+                                <td valign="top" class="col_head" >
+                                    Title:
+                                    <select name="title" class="tbox">
+                                        <option value="">Select</option>
+                                        <?php foreach ($customNotes as $index=>$note) { ?>
+                                            <option value="<?= $index ?>" <?= !is_null($currentNote) && $index == $currentNote ? 'selected="selected"' : '' ?> <?= $note['title'] === '' ? 'style="font-style: italic"' : '' ?>>
+                                                <?= htmlspecialchars($note['title'] ?: 'no title') ?>
+                                            </option>
+                                        <?php } ?>
+                                    </select>
+                                    <span title="Click here if the text template does not load automatically.&#013;Your changes will be lost" style="cursor: pointer;">
+                                        <input type="submit" class="button" value="Load">
+                                        ?
+                                    </span>
+                                </td>
+                            </tr>
+                        </table>
+                    </form>
+                    <form name="selfrm" action="<?php echo $_SERVER['PHP_SELF']?>?fr=<?php echo (!empty($_GET['fr']) ? $_GET['fr'] : '');?>&tx=<?php echo (!empty($_GET['tx']) ? $_GET['tx'] : '');?>" method="post" onSubmit="return selabc(this)">
+                        <table width="98%" cellpadding="5" cellspacing="1" bgcolor="#FFFFFF" align="center" >
 						    <tr class="tr_bg_h">
 								<td valign="top" class="col_data" >
-						        	<textarea name="description" id="description" class="tbox" style="width:98%; height:150px;"></textarea>
+						        	<textarea name="description" id="description" class="tbox" style="width:98%; height:150px;"><?php echo $notes;?></textarea>
 								</td>
 							</tr>
 						    <tr class="tr_bg_h">

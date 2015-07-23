011b1e36455c8999adeb92319a6088397c97ec22
Brendan Rehman <brendan@ignitedcoder.com>
2015-07-23 14:18:51 -0700

    Added Api Controllers: Admin Auth ApiBaseController.

---
+++ app/Http/Controllers/Api/ApiAdminMemoController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brendan
+ * Date: 7/23/2015
+ * Time: 10:27 AM
+ */
+
+namespace DentalSleepSolutions\Http\Controllers\Api;
+
+
+class ApiAdminMemoController extends ApiBaseController
+{
+    public function store()
+    {
+        return response()->json(['status' => true],200);
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+}

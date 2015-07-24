6b774b9c1d63deb20b8aeb90335b428d6428569b
Brendan Rehman <brendan@ignitedcoder.com>
2015-07-23 17:28:32 -0700

    ApiServiceProvider to register bindings for contracts/interfaces.

---
+++ app/Providers/ApiServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace DentalSleepSolutions\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        $this->app->bind('DentalSleepSolutions\\Interfaces\\MemoAdminInterface', 'DentalSleepSolutions\\Repositories\\MemoAdminRepository');
+    }
+}

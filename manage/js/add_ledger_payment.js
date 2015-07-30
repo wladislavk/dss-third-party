5cf66fa53850b9c686e992528f9fd6de9f9f74de
AstDerek <ast.derek@gmail.com>
2015-07-30 03:40:34 -0500

    dentalsleepsolutions/ds3-private02#244 Ledger Payments, change invalid DOM node ids

--- manage/js/add_ledger_payment.js
+++ manage/js/add_ledger_payment.js
@@ -40,13 +40,6 @@ function eraseCookie(name)
   createCookie(name,"",-1);
 }
 
-function validate(tempforledgerentry)
-{
-  if(document.getElementById("form[" + tempforledgerentry + "][service_date]")  == ''){
-    alert("DIE");
-  }
-}
-
 function appendElement() {
   if (readCookie('tempforledgerentry') == null || readCookie('tempforledgerentry') == 0) {
     createCookie('tempforledgerentry',1,'');
@@ -65,11 +58,11 @@ function appendElement() {
     var divIdName = 'innerdiv'+num;
     var tempforledgerentry = readCookie('tempforledgerentry');
     newdiv.setAttribute('id',divIdName);  
-    newdiv.innerHTML = '<div><input type="text" name="form['+tempforledgerentry+'][service_date]" id="form['+tempforledgerentry+'][service_date]" class="calendar" value="'+todaysdate+'" style="margin: 0pt 10px 0pt 0pt; float: left; width:75px;" ><input type="text" name="form['+tempforledgerentry+'][entry_date]" style="width:75px;margin: 0pt 10px 0pt 0pt; float: left;" value="'+month+'/'+day+'/'+year+'" readonly="readonly">' +
-                       '<select id="form['+tempforledgerentry+'][payer]" name="form['+tempforledgerentry+'][payer]" style="width:120px;margin: 0pt 10px 0pt 0pt; float: left;" ><option value="' + DSS_TRXN_PAYER_PRIMARY + '">' + dss_trxn_payer_labels_primary + '</option><option value="' + DSS_TRXN_PAYER_SECONDARY + '">' + dss_trxn_payer_labels_secondary + '<option value="' + DSS_TRXN_PAYER_PATIENT + '">' + dss_trxn_payer_labels_patient +
+    newdiv.innerHTML = '<div><input type="text" name="form['+tempforledgerentry+'][service_date]" id="service_date_field_'+tempforledgerentry+'" class="calendar" value="'+todaysdate+'" style="margin: 0pt 10px 0pt 0pt; float: left; width:75px;" ><input type="text" name="form['+tempforledgerentry+'][entry_date]" style="width:75px;margin: 0pt 10px 0pt 0pt; float: left;" value="'+month+'/'+day+'/'+year+'" readonly="readonly">' +
+                       '<select id="payer_field_'+tempforledgerentry+'" name="form['+tempforledgerentry+'][payer]" style="width:120px;margin: 0pt 10px 0pt 0pt; float: left;" ><option value="' + DSS_TRXN_PAYER_PRIMARY + '">' + dss_trxn_payer_labels_primary + '</option><option value="' + DSS_TRXN_PAYER_SECONDARY + '">' + dss_trxn_payer_labels_secondary + '<option value="' + DSS_TRXN_PAYER_PATIENT + '">' + dss_trxn_payer_labels_patient +
                        '<option value="' + DSS_TRXN_PAYER_WRITEOFF + '">' + dss_trxn_payer_labels_writeoff + '<option value="' + DSS_TRXN_PAYER_DISCOUNT + '">' + dss_trxn_payer_labels_discount + '</select><select id="form['+tempforledgerentry+'][payment_type]" name="form['+tempforledgerentry+'][payment_type]" style="width:120px;margin: 0pt 10px 0pt 0pt; float: left;" ><option value="' + DSS_TRXN_PYMT_CREDIT +
                        '">' + dss_trxn_pymt_type_labels_credit + '</option><option value="' + DSS_TRXN_PYMT_DEBIT + '">' + dss_trxn_pymt_type_labels_debit + '</option><option value="' + DSS_TRXN_PYMT_CHECK + '">' + dss_trxn_pymt_type_labels_check + '</option><option value="' + DSS_TRXN_PYMT_CASH + '">' + dss_trxn_pymt_type_labels_cash + '</option><option value="' + DSS_TRXN_PYMT_WRITEOFF +
-                       '">' + dss_trxn_pymt_type_labels_writeoff + '</option></select><div style="float:right;color:#FFF; font-weight:bold;font-size:18px;"><input type="text" id="form['+tempforledgerentry+'][amount]" class="dollar_input" name="form['+tempforledgerentry+'][amount]" style="margin: 0; float: left; width:75px;margin-right:10px;"></div><div style="clear: both; height: 10px;"></div>';
+                       '">' + dss_trxn_pymt_type_labels_writeoff + '</option></select><div style="float:right;color:#FFF; font-weight:bold;font-size:18px;"><input type="text" id="amount_field_'+tempforledgerentry+'" class="dollar_input" name="form['+tempforledgerentry+'][amount]" style="margin: 0; float: left; width:75px;margin-right:10px;"></div><div style="clear: both; height: 10px;"></div>';
     ni.appendChild(newdiv);
     setupCal(tempforledgerentry);
   } else if(readCookie('tempforledgerentry') > 0) {
@@ -90,11 +83,11 @@ function appendElement() {
     var day = currentTime.getDate()
     var year = currentTime.getFullYear()
     var todaysdate = month + "/" + day + "/" + year;  
-    newdiv.innerHTML = '<div><input type="text" name="form['+tempforledgerentry+'][service_date]" id="form['+tempforledgerentry+'][service_date]" class="calendar" value="'+todaysdate+'" style="margin: 0pt 10px 0pt 0pt; float: left; width:75px;" ><input type="text" name="form['+tempforledgerentry+'][entry_date]" style="width:75px;margin: 0pt 10px 0pt 0pt; float: left;" value="'+month+'/'+day+'/'+year+'" readonly="readonly">' +
-                       '<select id="form['+tempforledgerentry+'][payer]" name="form['+tempforledgerentry+'][payer]" style="width:120px;margin: 0pt 10px 0pt 0pt; float: left;" ><option value="' + DSS_TRXN_PAYER_PRIMARY + '">' + dss_trxn_payer_labels_primary + '</option><option value="' + DSS_TRXN_PAYER_SECONDARY + '">' + dss_trxn_payer_labels_secondary + '<option value="' + DSS_TRXN_PAYER_PATIENT + '">' + dss_trxn_payer_labels_patient +
+    newdiv.innerHTML = '<div><input type="text" name="form['+tempforledgerentry+'][service_date]" id="service_date_field_'+tempforledgerentry+'" class="calendar" value="'+todaysdate+'" style="margin: 0pt 10px 0pt 0pt; float: left; width:75px;" ><input type="text" name="form['+tempforledgerentry+'][entry_date]" style="width:75px;margin: 0pt 10px 0pt 0pt; float: left;" value="'+month+'/'+day+'/'+year+'" readonly="readonly">' +
+                       '<select id="payer_field_'+tempforledgerentry+'" name="form['+tempforledgerentry+'][payer]" style="width:120px;margin: 0pt 10px 0pt 0pt; float: left;" ><option value="' + DSS_TRXN_PAYER_PRIMARY + '">' + dss_trxn_payer_labels_primary + '</option><option value="' + DSS_TRXN_PAYER_SECONDARY + '">' + dss_trxn_payer_labels_secondary + '<option value="' + DSS_TRXN_PAYER_PATIENT + '">' + dss_trxn_payer_labels_patient +
                        '<option value="' + DSS_TRXN_PAYER_WRITEOFF + '">' + dss_trxn_payer_labels_writeoff + '<option value="' + DSS_TRXN_PAYER_DISCOUNT + '">' + dss_trxn_payer_labels_discount + '</select><select id="form['+tempforledgerentry+'][payment_type]" name="form['+tempforledgerentry+'][payment_type]" style="width:120px;margin: 0pt 10px 0pt 0pt; float: left;" ><option value="' + DSS_TRXN_PYMT_CREDIT +
                        '">' + dss_trxn_pymt_type_labels_credit + '</option><option value="' + DSS_TRXN_PYMT_DEBIT + '">' + dss_trxn_pymt_type_labels_debit + '</option><option value="' + DSS_TRXN_PYMT_CHECK + '">' + dss_trxn_pymt_type_labels_check + '</option><option value="' + DSS_TRXN_PYMT_CASH + '">' + dss_trxn_pymt_type_labels_cash + '</option><option value="' + DSS_TRXN_PYMT_WRITEOFF +
-                       '">' + dss_trxn_pymt_type_labels_writeoff + '</option></select><div style="float:right;color:#FFF; font-weight:bold;font-size:18px;"><input type="text" id="form['+tempforledgerentry+'][amount]" class="dollar_input" name="form['+tempforledgerentry+'][amount]" style="margin: 0; float: left; width:75px;margin-right:10px;"></div><div style="clear: both; height: 10px;"></div>';
+                       '">' + dss_trxn_pymt_type_labels_writeoff + '</option></select><div style="float:right;color:#FFF; font-weight:bold;font-size:18px;"><input type="text" id="amount_field_'+tempforledgerentry+'" class="dollar_input" name="form['+tempforledgerentry+'][amount]" style="margin: 0; float: left; width:75px;margin-right:10px;"></div><div style="clear: both; height: 10px;"></div>';
     ni.appendChild(newdiv);    
     setupCal(tempforledgerentry);
   }
@@ -130,7 +123,8 @@ function showsubmitbutton()
 
 function setupCal(tempforledgerentry)
 {
-  var cid = 'form[' + tempforledgerentry + '][service_date]'
+  var cid = 'service_date_field_' + tempforledgerentry;
+
   window["cal"+tempforledgerentry] = Calendar.setup({
       inputField : cid,
       trigger    : cid,

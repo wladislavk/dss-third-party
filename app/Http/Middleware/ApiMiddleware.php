bc6c977b2f252b5b8dca61d9ea846521e9371d10
Brendan Rehman <brendan@ignitedcoder.com>
2015-08-07 11:02:30 -0700

    Remove routes we don't need or use. - Added ApiMiddleware.php class so we can handle CORS.

---
+++ app/Http/Middleware/ApiMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace DentalSleepSolutions\Http\Middleware;
+
+use Closure;
+
+class ApiMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request)->header('Access-Control-Allow-Origin' , '*')
+               ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+               ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
+    }
+}

2e7e3076c8109069ab8ed74b0c8ec0e7e49f1771
Brendan Rehman <brendan@ignitedcoder.com>
2015-08-07 11:17:31 -0700

    Make it a bit more secure with CORS. Access-Control-Allow-Origin...

--- app/Http/Middleware/ApiMiddleware.php
+++ app/Http/Middleware/ApiMiddleware.php
@@ -15,7 +15,7 @@ class ApiMiddleware
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)->header('Access-Control-Allow-Origin' , '*')
+        return $next($request)->header('Access-Control-Allow-Origin' , '*.ds3soft.net')
                ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
                ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
     }

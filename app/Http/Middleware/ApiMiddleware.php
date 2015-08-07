85c1629d847a03c3efe1fe392de9b04b51825ff7
Brendan Rehman <brendan@ignitedcoder.com>
2015-08-07 11:27:58 -0700

    Remove routes we don't need or use. - Added ApiMiddleware.php class so we can handle CORS.

--- app/Http/Middleware/ApiMiddleware.php
+++ app/Http/Middleware/ApiMiddleware.php
@@ -15,7 +15,14 @@ class ApiMiddleware
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)->header('Access-Control-Allow-Origin' , '*.ds3soft.net')
+        $http_origin = $_SERVER['HTTP_ORIGIN'];
+        $allow_origins = null;
+
+        if (strpos($http_origin,'ds3soft.net') !== false || strpos($http_origin,'ds3soft.local') !== false) {
+            $allow_origin = $http_origin;
+        }
+
+        return $next($request)->header('Access-Control-Allow-Origin' , $http_origin)
                ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
                ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
     }

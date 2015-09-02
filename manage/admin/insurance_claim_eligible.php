c3ebe301cdf2e504bafe1e189e804655c0453a9a
Sergey <gerashchenko@ya.ru>
2015-09-02 17:53:55 +0300

    DSS-92 `Box-5 State`, `Box-7 State` (insurance_eligible) and `Box-32 ZIP`, `Box-33 ZIP`, `Box-21 ICD Ind.`, `Box-5 ZIP`, `Box-7 ZIP`, `Box-20 CHARGES` (insurance_claim_eligible) were fixed.

--- manage/admin/insurance_claim_eligible.php
+++ manage/admin/insurance_claim_eligible.php
@@ -13,3 +13,7 @@ $admin_path = "";
 $called_from = "manage_claims.php";
 $v2_form = "insurance_claim_v2.php?insid=".(!empty($_GET['insid']) ? $_GET['insid'] : '')."&pid=".(!empty($_GET['pid']) ? $_GET['pid'] : '')."&instype=".(!empty($_GET['instype']) ? $_GET['instype'] : '');
 include_once("../includes/claim_form_eligible.inc");
+?>
+
+    <script type="text/javascript" src="/manage/3rdParty/input_mask/jquery.maskedinput-1.3.min.js"></script>
+    <script type="text/javascript" src="/manage/js/masks.js?v=<?= time() ?>"></script>

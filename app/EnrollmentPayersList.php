017bcd2b39440ba43aa0ccd3842a95afa0b08157
Brendan Rehman <brendan@ignitedcoder.com>
2015-09-04 06:49:57 -0700

    Enrollment Payers Model

---
+++ app/EnrollmentPayersList.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace DentalSleepSolutions;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EnrollmentPayersList extends Model
+{
+
+    /**
+     * Mass assignable attributes
+     *
+     * @var array
+     */
+    protected $fillable = ['payer_id', 'names', 'supported_endpoints'];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'enrollment_payers_list';
+
+
+}

41e88eeb3a726da9cf87fb906ec8b121cee11b23
Brendan Rehman <brendan@ignitedcoder.com>
2015-09-07 12:28:20 -0700

    Enrollment routes.

--- app/Http/apiroutes.php
+++ app/Http/apiroutes.php
@@ -9,8 +9,11 @@
 Route::group(['prefix' => 'api/v1', 'before' => '', 'after' => 'allowOrigin'], function ()
 {
     Route::resource('memo','Api\ApiAdminMemoController');
-    Route::resource('enrollment','Api\ApiEnrollmentsController');
     Route::group(['prefix' => 'enrollment'], function(){
         Route::get('payers',['as' => 'payers.list', 'uses' => 'Api\ApiEnrollmentsController@payersList']);
+        Route::post('create',['as' => 'create.enrollment', 'uses' => 'Api\ApiEnrollmentsController@createEnrollment']);
+        Route::put('update',['as' => 'update.enrollment', 'uses' => 'Api\ApiEnrollmentsController@updateEnrollment']);
+        Route::get('show',['as' => 'show.enrollment', 'uses' => 'Api\ApiEnrollmentsController@showEnrollment']);
+        Route::delete('delete',['as' => 'delete.enrollment', 'uses' => 'Api\ApiEnrollmentsController@destroyEnrollment']);
     });
 });

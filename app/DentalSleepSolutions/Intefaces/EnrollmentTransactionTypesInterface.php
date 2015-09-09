fd2c2228556ce5e53483718e3662d29b0f9698a2
Brendan Rehman <brendan@ignitedcoder.com>
2015-09-09 15:42:49 -0700

    Enrollment Transaction Types Interface.

---
+++ app/DentalSleepSolutions/Intefaces/EnrollmentTransactionTypesInterface.php
@@ -0,0 +1,13 @@
+<?php namespace DentalSleepSolutions\Interfaces;
+
+interface EnrollmentTransactionTypesInterface
+{
+
+    /**
+     *
+     *
+     * @return mixed
+     */
+    public function getTransactionTypesList();
+
+}

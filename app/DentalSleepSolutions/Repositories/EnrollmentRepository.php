f1699653aafc84c24b93677419b6d422064fac40
Brendan Rehman <brendan@ignitedcoder.com>
2015-09-16 12:13:26 -0700

    Enrollment  repository.

--- app/DentalSleepSolutions/Repositories/EnrollmentRepository.php
+++ app/DentalSleepSolutions/Repositories/EnrollmentRepository.php
@@ -2,16 +2,18 @@
 
 use DentalSleepSolutions\Interfaces\EnrollmentInterface;
 use DentalSleepSolutions\Interfaces\EnrollmentPayersInterface;
+use Carbon\Carbon;
 
 class EnrollmentRepository extends BaseRepository implements EnrollmentInterface
 {
+
     /**
      *
      * @var string
      *
      * Main model name for the Enrollment  Payers Model
      */
-    protected $modelName = '';
+    protected $modelName = 'DentalSleepSolutions\Enrollment';
 
     /**
      * @var null
@@ -38,78 +40,262 @@ class EnrollmentRepository extends BaseRepository implements EnrollmentInterface
      */
     protected $payers;
 
-    public function __construct(EnrollmentPayersInterface $enrollmentPayersInterface)
-    {
-
-        $this->elligibleConfigurtion = config('elligibleapi');
-
-        $this->providerUri = $this->elligibleConfigurtion['base_uri'];
-        $this->enrollmentsRoute = $this->elligibleConfigurtion['request_uri']['enrollments'];
-        $this->payers = $enrollmentPayersInterface;
-
-    }
-
+    /**
+     * @var array
+     */
+    protected $elligibleParams = [];
 
     /**
      *
      *
-     * @param $payerId
+     *
+     * @param EnrollmentPayersInterface $enrollmentPayersInterface
+     */
+    public function __construct(EnrollmentPayersInterface $enrollmentPayersInterface)
+    {
+        $this->elligibleConfigurtion = config('elligibleapi');
+        $this->providerUri = $this->elligibleConfigurtion['base_uri'];
+        $this->enrollmentsRoute = $this->elligibleConfigurtion['request_uri']['enrollments'];
+        $this->payers = $enrollmentPayersInterface;
+    }
+
+    /**
+     *
+     *
+     * @param integer $payerId
      * @return mixed
      */
     public function getRequiredFieldsForEnrollment($payerId)
     {
         $mandatoryEnrollmentFields = [];
         $endPoints = $this->payers->getPayerSupportedEndpoints($payerId);
-        foreach ($endPoints as $endPoint) {
-            if($endPoint->endpoint=='coverage') {
-                foreach ($endPoint->enrollment_mandatory_fields as $mandatoryEnrollmentField) {
+
+        foreach ($endPoints as $endPoint)
+        {
+            if ($endPoint->endpoint == 'coverage')
+            {
+                foreach ($endPoint->enrollment_mandatory_fields as $mandatoryEnrollmentField)
+                {
                     $mandatoryEnrollmentFields[] = $mandatoryEnrollmentField;
                 }
-
             }
         }
 
-        dd($mandatoryEnrollmentFields);
+        return ($mandatoryEnrollmentFields);
+    }
 
+    /**
+     *
+     *
+     * @param array  $enrollmentParams
+     * @param string $apiKey
+     * @return mixed
+     */
+    public function createEnrollment(array $enrollmentParams, $apiKey = '')
+    {
+        $enrollmentParams['endpoint'] = 'coverage';
+
+        $this->elligibleParams['enrollment_npi'] = $enrollmentParams;
+        $requestUri = $this->providerUri.$this->enrollmentsRoute;
+        $this->checkAndSetProviderApiKey($apiKey);
+
+        $data_string = $this->convertEnrollmentParamsToJson();
+
+        $headers = $this->setupApiRequestHeaders($data_string);
+        $response = \Requests::post($requestUri, $headers, $data_string);
+        $enrollmentResponse = json_decode($response->body);
+
+        if (isset($enrollmentResponse->error))
+        {
+            return $enrollmentResponse->error;
+        }
+
+        $this->setupEnrollmentResponseForCreateUpdate($enrollmentParams, $enrollmentResponse, $response);
+
+        return $enrollmentResponse;
+    }
+
+    /**
+     *
+     *
+     * @param int $enrollmentId
+     * @param array $enrollmentParams
+     * @param string $apiKey
+     * @return mixed
+     */
+    public function updateEnrollment($enrollmentId = 0,array $enrollmentParams, $apiKey = '')
+    {
+        $enrollmentParams['endpoint'] = 'coverage';
+
+        $this->elligibleParams['enrollment_npi'] = $enrollmentParams;
+        $requestUri = $this->providerUri.$this->enrollmentsRoute.'/'.$enrollmentId;
+        $this->checkAndSetProviderApiKey($apiKey);
+        $data_string = $this->convertEnrollmentParamsToJson();
+        $headers = $this->setupApiRequestHeaders($data_string);
+        $response = \Requests::put($requestUri, $headers, $data_string);
+        $enrollmentResponse = json_decode($response->body);
+
+        if (isset($enrollmentResponse->error))
+        {
+            return $enrollmentResponse->error;
+        }
+
+        $this->setupEnrollmentResponseForCreateUpdate($enrollmentParams, $enrollmentResponse, $response);
+
+        return $enrollmentResponse;
+
+    }
+
+    /**
+     *
+     *
+     * @param integer $enrollmentId
+     * @param array   $enrollmentParams
+     * @param string  $apiKey
+     * @return string
+     */
+    public function retrieveEnrollment($enrollmentId = 0, array $enrollmentParams = [], $apiKey = '')
+    {
+        if ($enrollmentId == 0)
+        {
+            return 'enrollment id parameter missing';
+        }
+
+        $this->elligibleParams = $enrollmentParams;
+        $this->checkAndSetProviderApiKey($apiKey);
+        $data_string = $this->convertEnrollmentParamsToJson();
+
+        $requestUri = $this->setupRetrieveEnrollmentUrl($enrollmentId);
+
+        $headers = $this->setupApiRequestHeaders($data_string);
+        $response = \Requests::get($requestUri, $headers);
+
+        return $response->body;
+    }
+
+    /**
+     *
+     *
+     * @param integer $page
+     * @param string  $apiKey
+     * @return string
+     */
+    public function listEnrollments($page = 1, $apiKey = '')
+    {
+        $this->checkAndSetProviderApiKey($apiKey);
+        $data_string = $this->convertEnrollmentParamsToJson();
+        $requestUri = $this->setupListEnrollmentsUrl($page);
+        $headers = $this->setupApiRequestHeaders($data_string);
+        $response = \Requests::get($requestUri, $headers);
+
+        return $response->body;
     }
 
 
     /**
      *
      *
-     * @param array $enrollmentParams
+     * @param array $data
+     * @return void
+     */
+    public function saveEnrollmentDetailsToDatabase($data = [])
+    {
+        $this->store($data);
+    }
+
+    /**
+     *
+     *
+     * @return void
+     */
+    protected function checkApiTestMode()
+    {
+        if ($this->elligibleConfigurtion['test'] == true)
+        {
+            $this->elligibleParams['test'] = "test";
+        }
+    }
+
+    /**
+     *
+     *
+     * @param string $apiKey
+     * @return void
+     */
+    protected function checkAndSetProviderApiKey($apiKey)
+    {
+        $this->elligibleParams['api_key'] = $apiKey != ''
+            ? $apiKey
+            : $this->elligibleConfigurtion['default_api_key'];
+    }
+
+    /**
+     *
+     *
      * @return string
      */
-    public function createEnrollment(array $enrollmentParams)
+    protected function convertEnrollmentParamsToJson()
     {
+        $this->checkApiTestMode();
+        $data_string = json_encode($this->elligibleParams);
+        return $data_string;
+    }
 
-        $postUrl = $this->providerUri.$this->enrollmentsRoute;
-        $data['api_key'] = $this->elligibleConfigurtion['default_api_key'];
-
-        if($this->elligibleConfigurtion['test']===TRUE)
-        {
-            $data['test'] = "true";
-        }
-
-
-        $data['enrollment_npi'] = $enrollmentParams;
-        $data_string = json_encode($data);
-
+    /**
+     *
+     *
+     * @param string $data_string
+     * @return array
+     */
+    public function setupApiRequestHeaders($data_string)
+    {
         $headers = ['Content-Type' => 'application/json',
-                    'Content-Length: ' . strlen($data_string)];
-
-        $response = \Requests::post($postUrl,$headers,$data_string);
-        return $response->body;
-
+            'Content-Length: ' . strlen($data_string)];
+        return $headers;
     }
 
-    public function retrieveEnrollment()
+    /**
+     *
+     *
+     * @param string $enrollmentId
+     * @return string
+     */
+    protected function setupRetrieveEnrollmentUrl($enrollmentId)
     {
-
+        $urlParams = $enrollmentId . '?api_key=' . $this->elligibleParams['api_key'];
+        $urlParams .= array_key_exists('test', $this->elligibleParams) ? '&test=true' : '';
+        $requestUri = $this->providerUri . $this->enrollmentsRoute . '/' . $urlParams;
+        return $requestUri;
     }
 
-    public function listEnrollments()
+    /**
+     *
+     *
+     * @param integer $page
+     * @return string
+     */
+    protected function setupListEnrollmentsUrl($page = 1)
     {
-
+        $urlParams = '?api_key=' . $this->elligibleParams['api_key'];
+        $urlParams .= array_key_exists('test', $this->elligibleParams) ? '&test=true' : '';
+        $urlParams .= '&page=' . $page;
+        $requestUri = $this->providerUri . $this->enrollmentsRoute . $urlParams;
+        return $requestUri;
     }
+
+    /**
+     *
+     *
+     * @param array $enrollmentParams
+     * @param $enrollmentResponse
+     * @param $response
+     * @return void
+     */
+    protected function setupEnrollmentResponseForCreateUpdate(array $enrollmentParams, $enrollmentResponse, $response)
+    {
+        $enrollmentResponse->ip_address = $enrollmentParams['ip_address'];
+        $enrollmentResponse->adddate = Carbon::now();
+        $enrollmentResponse->response = $response->body;
+    }
+
 }

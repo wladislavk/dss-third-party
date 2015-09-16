5f03c82d1d9ca63f123e550e8b00dfe0ee6021fa
Brendan Rehman <brendan@ignitedcoder.com>
2015-09-16 12:12:56 -0700

    Enrollment Service provider class.

--- app/Providers/ApiServiceProvider.php
+++ app/Providers/ApiServiceProvider.php
@@ -23,9 +23,16 @@ class ApiServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
-        $this->app->bind('DentalSleepSolutions\\Interfaces\\MemoAdminInterface', 'DentalSleepSolutions\\Repositories\\MemoAdminRepository');
-        $this->app->bind('DentalSleepSolutions\\Interfaces\\EnrollmentPayersInterface', 'DentalSleepSolutions\\Repositories\\EnrollmentPayersApiRepository');
-        $this->app->bind('DentalSleepSolutions\\Interfaces\\EnrollmentInterface', 'DentalSleepSolutions\\Repositories\\EnrollmentRepository');
+        $this->app->bind('DentalSleepSolutions\\Interfaces\\MemoAdminInterface',
+                        'DentalSleepSolutions\\Repositories\\MemoAdminRepository');
+
+        $this->app->bind('DentalSleepSolutions\\Interfaces\\EnrollmentPayersInterface',
+                        'DentalSleepSolutions\\Repositories\\EnrollmentPayersApiRepository');
+
+        $this->app->bind('DentalSleepSolutions\\Interfaces\\EnrollmentInterface',
+                        'DentalSleepSolutions\\Repositories\\EnrollmentRepository');
+
+        $this->app->bind('DentalSleepSolutions\\Interfaces\\UserSignaturesInterface',
+                        'DentalSleepSolutions\\Repositories\\UserSignaturesRepository');
     }
 }

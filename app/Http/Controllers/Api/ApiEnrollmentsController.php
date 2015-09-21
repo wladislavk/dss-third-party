9181450319c37de4a80e93993bec3aeb3e6891b5
Brendan Rehman <brendan@ignitedcoder.com>
2015-09-21 13:18:40 -0700

    Enrollment controller.

--- app/Http/Controllers/Api/ApiEnrollmentsController.php
+++ app/Http/Controllers/Api/ApiEnrollmentsController.php
@@ -1,16 +1,17 @@
 <?php namespace DentalSleepSolutions\Http\Controllers\Api;
 
 use DentalSleepSolutions\Interfaces\EnrollmentInterface;
-use DentalSleepSolutions\Http\Requests\ApiElligibleEnrollmentRequest;
+use DentalSleepSolutions\Http\Requests\ApiEligibleEnrollmentRequest;
 use DentalSleepSolutions\Interfaces\EnrollmentPayersInterface;
 use DentalSleepSolutions\Interfaces\UserSignaturesInterface;
 use Mockery\CountValidator\Exception;
 
+/**
+ * @SWG\Info(title="Dental Sleep Solutions Enrollment Api", version="0.1")
+ */
+
 class ApiEnrollmentsController extends ApiBaseController
 {
-    /**
-     * @SWG\Info(title="Dental Sleep Solutions Enrollment Api", version="0.1")
-     */
 
     /**
      * @var EnrollmentInterface $enrollments
@@ -28,32 +29,48 @@ class ApiEnrollmentsController extends ApiBaseController
     protected $signatures;
 
     /**
-     * @param EnrollmentInterface $enrollments
-     * @param EnrollmentPayersInterface $payers
-     * @param UserSignaturesInterface $signatures
+     * @var array $enrollmentValues
      */
-    public function __construct(EnrollmentInterface $enrollments,
-                                EnrollmentPayersInterface $payers,
-                                UserSignaturesInterface $signatures)
+    protected $enrollmentValues = [];
+
+    /**
+     * @param EnrollmentInterface       $enrollments
+     * @param EnrollmentPayersInterface $payers
+     * @param UserSignaturesInterface   $signatures
+     */
+    public function __construct(
+        EnrollmentInterface $enrollments,
+        EnrollmentPayersInterface $payers,
+        UserSignaturesInterface $signatures
+    )
     {
         $this->enrollments = $enrollments;
         $this->payers      = $payers;
         $this->signatures  = $signatures;
     }
 
-    public function listEnrollments($page=1)
+    /**
+     *
+     *
+     * @param integer $page
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function listEnrollments($page = 1)
     {
-        try {
+        try
+        {
             $results = $this->enrollments->listEnrollments($page);
             $response = ['data' => $results, 'status' => true, 'message' => ''];
-            return response()->json($response,200);
-        } catch(Exception $ex) {
+            return response()->json($response, 200);
+        }
+        catch (Exception $ex)
+        {
             $this->createErrorResponse('Could not retrieve list of Enrollments from Provider', 404);
         }
-
     }
 
     /**
+     *
      * @SWG\Post(
      *     path="/api/v1/enrollments.json",
      *     @SWG\Parameter(name="endpoint",
@@ -89,45 +106,43 @@ class ApiEnrollmentsController extends ApiBaseController
      *
      *    @SWG\Response(response="200", description="Action completed successfully.")
      * )
-     */
-
-    /**
      *
-     *
-     * @param ApiElligibleEnrollmentRequest $apiElligibleEnrollmentRequest
+     * @param ApiEligibleEnrollmentRequest $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function createEnrollment(ApiElligibleEnrollmentRequest $request)
+    public function createEnrollment(ApiEligibleEnrollmentRequest $request)
     {
         $response = [];
-        $enrollmentValues = [];
 
         try
         {
+            if(null !== $request->get('payer_id') || !empty($request->get('payer_id')))
+            {
+                //What fields are required for this enrollment
+                $enrollmentRequiredFields = $this->checkEnrollmentFields($request->get('payer_id'));
+            }
 
-            $enrollmentRequiredFields = $this->checkEnrollmentFields();
-
-            dd($enrollmentRequiredFields);
-
+            //setup the eligible enrollment array
             $enrollmentParams = $this->setupEnrollmentArrayFromFormInput($request);
+            //create a new eligible enrollment
             $enrollment = $this->enrollments->createEnrollment($enrollmentParams);
-
-            $enrollmentValues = $this->setCreatedEnrollmentsValuesForSavingToDb($enrollment, $enrollmentValues);
-            $enrollmentValues = $this->setEnrollmentFormPostValuesForSavingToDb($request, $enrollmentValues);
-
-            $this->enrollments->saveEnrollmentDetailsToDatabase($enrollmentValues);
+            //dd($enrollment);
+            //grab the request values submited from the form so we can persist it to the DB
+            //setup the returned values from Eligible to be saved to db.
+            $this->setEnrollmentValuesForSavingToDb($request, $enrollment);
+            $this->enrollments->saveEnrollmentDetailsToDatabase($this->enrollmentValues);
 
             return response()->json($enrollment, 200);
-
         }
-        catch(Exception $ex)
+        catch (Exception $ex)
         {
             $this->createErrorResponse('An error occured creating the Enrollment.', 404);
         }
     }
 
-
+    //Todo - add the json signature to the swagegr items
     /**
+     *
      * @SWG\Put(
      *     path="/api/v1/enrollments.json",
      *     @SWG\Parameter(name="endpoint",
@@ -163,64 +178,60 @@ class ApiEnrollmentsController extends ApiBaseController
      *
      *    @SWG\Response(response="200", description="Action completed successfully.")
      * )
-     */
+     *
 
-    /**
-     *
-     *
-     * @param int $enrollmentId
-     * @param ApiElligibleEnrollmentRequest $request
+     * @param ApiEligibleEnrollmentRequest $request
+     * @param integer                      $enrollmentId
      * @return \Illuminate\Http\JsonResponse
      */
-    public function updateEnrollment($enrollmentId=0, ApiElligibleEnrollmentRequest $request)
+    public function updateEnrollment(ApiEligibleEnrollmentRequest $request, $enrollmentId = 0)
     {
         $response = [];
-        try{
-            $enrollmentParams = $this->enrollmentFormInput($request);
-            $enrollmentParams['ip_address'] = $request->ip();
-            $this->enrollment = $this->enrollments->updateEnrollment($enrollmentId,$enrollmentParams);
-            return response()->json($this->enrollment, 200);
-        } catch(Exception $ex) {
+        try
+        {
+            $enrollmentParams = $this->setupEnrollmentArrayFromFormInput($request);
+            $enrollment = $this->enrollments->updateEnrollment($enrollmentId, $enrollmentParams);
+            return response()->json($enrollment, 200);
+        }
+        catch (Exception $ex)
+        {
             $this->createErrorResponse('An error occured updating the Enrollment.', 404);
         }
     }
 
 
+    //Todo - Add relevant params to the Swagger Items.
 
     /**
+     *
+     *
      * @SWG\Get(
      *     path="/api/v1/enrollments.json",
      * )
-     */
-
-    /**
      *
-     *
-     * @param int $enrollmentId
+     * @param integer $enrollmentId
      * @return \Illuminate\Http\JsonResponse
      */
-    public function retrieveEnrollment($enrollmentId=0)
+    public function retrieveEnrollment($enrollmentId = 0)
     {
-        try {
+        try
+        {
             $response = $this->enrollments->retrieveEnrollment($enrollmentId);
             return response()->json($response, 200);
-        } catch(Exception $ex) {
+        }
+        catch (Exception $ex)
+        {
             $this->createErrorResponse('Could not retrieve list of Enrollments from Provider', 404);
         }
     }
 
-    public function destroyEnrollment()
-    {
-
-    }
-
-
     /**
      *
      *
+     * @param ApiEligibleEnrollmentRequest $request
      * @return mixed
      */
-    private function setupEnrollmentArrayFromFormInput($request)
+    private function setupEnrollmentArrayFromFormInput(ApiEligibleEnrollmentRequest $request)
     {
         $elligibleEnrollment['payer_id']            = $request->get('payer_id');
         $elligibleEnrollment['transaction_type']    = $request->get('transaction_type');
@@ -247,65 +258,69 @@ class ApiEnrollmentsController extends ApiBaseController
         return $elligibleEnrollment;
     }
 
+
     /**
      *
      *
-     * @param ApiElligibleEnrollmentRequest $request
-     * @param $enrollmentValues
-     * @return mixed
+     * @param ApiEligibleEnrollmentRequest  $request
+     * @param                               $enrollment
+     * @return void
      */
-    private function setEnrollmentFormPostValuesForSavingToDb(ApiElligibleEnrollmentRequest $request, $enrollmentValues)
+    private function setEnrollmentValuesForSavingToDb(
+        ApiEligibleEnrollmentRequest $request,
+        $enrollment
+    )
     {
-        $enrollmentValues['user_id']                = $request->get('user_id');
-        $enrollmentValues['payer_id']               = $request->get('payer_id');
-        $enrollmentValues['transaction_type_id']    = $request->get('transaction_type');
-        $enrollmentValues['facility_name']          = $request->get('facility_name');
-        $enrollmentValues['provider_name']          = $request->get('provider_name');
-        $enrollmentValues['tax_id']                 = $request->get('tax_id');
-        $enrollmentValues['address']                = $request->get('address');
-        $enrollmentValues['city']                   = $request->get('city');
-        $enrollmentValues['state']                  = $request->get('state');
-        $enrollmentValues['zip']                    = $request->get('zip');
-        $enrollmentValues['first_name']             = $request->get('first_name');
-        $enrollmentValues['last_name']              = $request->get('last_name');
-        $enrollmentValues['contact_number']         = $request->get('contact_number');
-        $enrollmentValues['email']                  = $request->get('email');
+        $this->enrollmentValues['user_id']                = $request->get('user_id');
+        $this->enrollmentValues['payer_id']               = $request->get('payer_id');
+        $this->enrollmentValues['transaction_type_id']    = $request->get('transaction_type');
+        $this->enrollmentValues['facility_name']          = $request->get('facility_name');
+        $this->enrollmentValues['provider_name']          = $request->get('provider_name');
+        $this->enrollmentValues['tax_id']                 = $request->get('tax_id');
+        $this->enrollmentValues['address']                = $request->get('address');
+        $this->enrollmentValues['city']                   = $request->get('city');
+        $this->enrollmentValues['state']                  = $request->get('state');
+        $this->enrollmentValues['zip']                    = $request->get('zip');
+        $this->enrollmentValues['first_name']             = $request->get('first_name');
+        $this->enrollmentValues['last_name']              = $request->get('last_name');
+        $this->enrollmentValues['contact_number']         = $request->get('contact_number');
+        $this->enrollmentValues['email']                  = $request->get('email');
 
-        return $enrollmentValues;
+        $this->enrollmentValues['payer_name'] = rtrim(
+            implode(
+                ',',
+                $enrollment->enrollment_npi->payer->names
+            ),
+            ','
+        );
+
+        $this->enrollmentValues['npi'] = $enrollment->enrollment_npi->npi;
+        $this->enrollmentValues['reference_id'] = $enrollment->enrollment_npi->id;
+        $this->enrollmentValues['response'] = $enrollment->response;
+        $this->enrollmentValues['adddate'] = $enrollment->adddate;
+        $this->enrollmentValues['ip_address'] = $enrollment->ip_address;
+        $this->enrollmentValues['status'] = 0;
     }
 
     /**
      *
      *
+     * @param integer $payerId
      * @return mixed
      */
-    protected function checkEnrollmentFields() {
-        return $this->enrollments->getRequiredFieldsForEnrollment('00901');
+    protected function checkEnrollmentFields($payerId)
+    {
+        return $this->enrollments->getRequiredFieldsForEnrollment($payerId);
     }
 
-    protected function getDentalUserSignature($user_id) {
+    /**
+     *
+     *
+     * @param integer $user_id
+     * @return mixed
+     */
+    protected function getDentalUserSignature($user_id)
+    {
         return $this->signatures->findBy('user_id', $user_id);
     }
-
-    /**
-     *
-     *
-     * @param $enrollment
-     * @param $enrollmentValues
-     * @return mixed
-     */
-    private function setCreatedEnrollmentsValuesForSavingToDb($enrollment, $enrollmentValues)
-    {
-        $enrollmentValues['payer_name'] = rtrim(implode(',',
-            $enrollment->enrollment_npi->payer->names), ',');
-        $enrollmentValues['npi'] = $enrollment->enrollment_npi->npi;
-        $enrollmentValues['reference_id'] = $enrollment->enrollment_npi->id;
-        $enrollmentValues['response'] = $enrollment->response;
-        $enrollmentValues['adddate'] = $enrollment->adddate;
-        $enrollmentValues['ip_address'] = $enrollment->ip_address;
-
-        $enrollmentValues['status'] = 0;
-        return $enrollmentValues; //Find out why were not using the elligible status here...
-    }
-
 }

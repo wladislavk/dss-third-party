de729b26de14f12398b716039a9a3e74beeba132
Grigori Tishkin <tishkin_gr@elance-odesk.com>
2015-11-09 23:03:54 +0400

    DSS-144 Payment Report. User parameter

--- manage/payment_reports_list.php
+++ manage/payment_reports_list.php
@@ -1,10 +1,10 @@
-<?php namespace Ds3\Libraries\Legacy; ?>
-<?php include 'includes/top.htm';?>
 <?php
+namespace Ds3\Libraries\Legacy;
+include 'includes/top.htm';
 
 $back_office = false;
 $sql_join_section = 'INNER JOIN dental_users AS u on u.userid = i.docid';
-$sql_user_filter = 'i.docid = ' . $_SESSION['docid'];;
+$sql_user_filter = 'i.docid = ' . $_SESSION['docid'];
 $header_class = 'admin_head';
 $table_style = 'width="98%" cellpadding="5" cellspacing="1" bgcolor="#FFFFFF" align="center"';
 

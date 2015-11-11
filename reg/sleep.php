5577ace667a8eb45747253ef1fbc4c4d186ec8dd
AstDerek <ast.derek@gmail.com>
2015-11-11 13:06:01 -0600

    DSS-181 Change logic in all sections

--- reg/sleep.php
+++ reg/sleep.php
@@ -131,10 +131,9 @@ if($_POST['q_sleepsub'] == 1)
 }
 
 
-        $exist_sql = "SELECT sleep_status, symptoms_status FROM dental_patients WHERE patientid='".mysqli_real_escape_string($con, $_SESSION['pid'])."'";
-        $exist_q = mysqli_query($con, $exist_sql);
-        $exist_row = mysqli_fetch_assoc($exist_q);
-        if($exist_row['sleep_status'] == 0 && ($exist_row['symptoms_status'] == 0 || $exist_row['symptoms_status']==1))
+$comp = questionnaireCompletedSections($_SESSION['pid']);
+
+        if($comp['epworth'] == 0)
         {
 
 $sql = "select * from dental_thorton where patientid='".$_SESSION['pid']."'";

bbacded003269074da344157905aec07557a7315
AstDerek <ast.derek@gmail.com>
2015-11-17 11:08:58 -0600

    DSS-196 Prevent accidental letter deletion

--- manage/includes/delete_appt.php
+++ manage/includes/delete_appt.php
@@ -1,15 +1,35 @@
-<?php namespace Ds3\Libraries\Legacy; ?><?php
-	include_once '../admin/includes/main_include.php';
+<?php
+namespace Ds3\Libraries\Legacy;
 
-	$id = (!empty($_REQUEST['id']) ? $_REQUEST['id'] : '');
-	$pid = (!empty($_REQUEST['pid']) ? $_REQUEST['pid'] : '');
-	$s = "DELETE from dental_flow_pg2_info WHERE id = '".mysqli_real_escape_string($con,$id)."' AND patientid = '".mysqli_real_escape_string($con,$pid)."'";
-	
-	$q = $db->query($s);
-	if(!empty($q)){
-        $db->query("DELETE FROM dental_letters where info_id = '".mysqli_real_escape_string($con,$id)."'");
-		echo '{"success":true}';
-	} else {
-	  	echo '{"error":true}';
-	}
-?>
+include_once __DIR__ . '/../admin/includes/main_include.php';
+
+$id = $db->escape($_REQUEST['id']);
+$pid = $db->escape($_REQUEST['pid']);
+
+$response = ['error' => true];
+
+if (empty($_SESSION['userid'])) {
+    throw new \RuntimeException('Unregistered users cannot delete appt.');
+}
+
+/**
+ * This section was deleting dental_letters rows by mistake. We now NEVER delete them, we just deactivate them.
+ *
+ * @see DSS-196
+ * @see DSS-198
+ */
+if ($id && $pid) {
+    $infoData = $db->query("SELECT id from dental_flow_pg2_info WHERE id = '$id' AND patientid = '$pid'");
+
+    if ($infoData) {
+        $userId = $db->escape($_SESSION['userid']);
+
+        $db->query("DELETE from dental_flow_pg2_info WHERE id = '$id' AND patientid = '$pid'");
+        $db->query("UPDATE dental_letters SET deleted = 1, deleted_by = '$userId', deleted_on = NOW()
+            WHERE info_id != '' AND info_id = '$id'");
+
+        $response = ['success' => true];
+    }
+}
+
+echo json_encode($response);

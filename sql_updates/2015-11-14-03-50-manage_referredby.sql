1fc6310871cc071d2c9942d668884c4b8b26ce1f
AstDerek <ast.derek@gmail.com>
2015-11-18 16:00:29 -0600

    DSS-186 Named indexes

--- sql_updates/2015-11-14-03-50-manage_referredby.sql
+++ sql_updates/2015-11-14-03-50-manage_referredby.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `dental_patients` ADD INDEX(`referred_by`);
-ALTER TABLE `dental_patients` ADD INDEX(`referred_source`);
-ALTER TABLE `dental_patients` ADD INDEX(`copyreqdate`);
+ALTER TABLE `dental_patients` ADD INDEX `referred_by` (`referred_by`);
+ALTER TABLE `dental_patients` ADD INDEX `referred_source` (`referred_source`);
+ALTER TABLE `dental_patients` ADD INDEX `copyreqdate` (`copyreqdate`);

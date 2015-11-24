a9d4466ecfe9f20a7ce6b4dabbaec7d397a90ab3
jaroslawf <jaroslaw.f@devpark.pl>
2015-11-24 14:38:48 +0100

    request errors

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -3,8 +3,29 @@
 namespace DentalSleepSolutions\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use DentalSleepSolutions\Helpers\ApiResponse;
 
 abstract class Request extends FormRequest
 {
-    //
+
+    /**
+     * Get the proper failed validation response for the request.
+     *
+     * @param  array  $errors
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function response(array $errors)
+    {
+        if ($this->ajax() || $this->wantsJson()) {
+            $data = [
+                'errors' => $errors
+            ];
+
+            return ApiResponse::responseError("Error data", 422, $data);
+        }
+
+        return $this->redirector->to($this->getRedirectUrl())
+            ->withInput($this->except($this->dontFlash))
+            ->withErrors($errors, $this->errorBag);
+    }
 }

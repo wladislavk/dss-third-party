1f139471f81760d9ea0d2f9e7276fbee8c46cd16
jaroslawf <jaroslaw.f@devpark.pl>
2016-01-20 16:33:11 +0100

    update signature

--- app/Http/Controllers/Api/ApiEnrollmentsController.php
+++ app/Http/Controllers/Api/ApiEnrollmentsController.php
@@ -60,8 +60,16 @@ class ApiEnrollmentsController extends ApiBaseController
 
         $payer_id = explode('-', $request->input('payer_id'));
 
-        $user_signature = UserSignature::formUser($user_id);
-        $signature = $user_signature ? $user_signature->signature_json : $request->input('signature', '');
+        $signature = $request->input('signature', '');
+        if ($signature == '') {
+            $user_signature = UserSignature::formUser($user_id);
+            $signature = $user_signature->signature_json;
+        }
+
+//        $user_signature->signature_json;
+//
+//        $user_signature = UserSignature::formUser($user_id);
+//        $signature = $user_signature ? $user_signature->signature_json : $request->input('signature', '');
 
         $transaction_type = TransactionType::where('id', $request->input('transaction_type_id'))
             ->where('status', 1)->first();
@@ -109,13 +117,18 @@ class ApiEnrollmentsController extends ApiBaseController
             Invoice::addEnrollment('1', $user_id, $enrollment_id);
 
             if ($request->input('signature', '') != '') {
-                $signature_id = UserSignature::add($user_id, $signature, $ip);
+                $signature_id = UserSignature::addUpdate($user_id, $signature, $ip);
 
                 $signature = new SignatureToImage();
                 $img = $signature->sigJsonToImage($request->input('signature', ''));
 
                 $file = "signature_" . $user_id . "_" . $signature_id . ".png";
-                $path = env('LEGACY_PATH').'/../../shared/q_file/'.$file;
+                $path = env('LEGACY_PATH', '').'/../../shared/q_file/'.$file;
+
+                if (file_exists($path)) {
+                    unlink($path);
+                }
+
                 imagepng($img, $path);
                 imagedestroy($img);
             }

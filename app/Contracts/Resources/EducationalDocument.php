0edae7262ec49a1fd8f5bce604dab3ffd00ff547
Sergey <gerra.lampdev@gmail.com>
2016-01-28 14:40:21 +0200

    DSS-287 * Added base structure of the resource Educational Documents

---
+++ app/Contracts/Resources/EducationalDocument.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace DentalSleepSolutions\Contracts\Resources;
+
+interface EducationalDocument extends Resource
+{
+
+}
---
+++ app/Eloquent/Dental/EducationalDocument.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace DentalSleepSolutions\Eloquent\Dental;
+
+use Illuminate\Database\Eloquent\Model;
+use DentalSleepSolutions\Eloquent\WithoutUpdatedTimestamp;
+use DentalSleepSolutions\Contracts\Resources\EducationalDocument as Resource;
+use DentalSleepSolutions\Contracts\Repositories\EducationalDocuments as Repository;
+
+class EducationalDocument extends Model implements Resource, Repository
+{
+    use WithoutUpdatedTimestamp;
+
+    /**
+     * Mass assignable attributes
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'description', 'video_file',
+        'doc_file', 'sortby', 'status',
+        'adddate', 'ip_address', 'docid'
+    ];
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'dental_doc_educational';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'doc_educationalid';
+
+    /**
+     * The name of the "created at" column.
+     *
+     * @var string
+     */
+    const CREATED_AT = 'adddate';
+}

50d355b7e98e64ec9dca16db0b72a916c4af4776
Sergey <gerra.lampdev@gmail.com>
2016-02-04 14:43:08 +0200

    DSS-296 * Updated the model, store and update requests, factory, unit test

--- app/Eloquent/Dental/TmjClinicalExam.php
+++ app/Eloquent/Dental/TmjClinicalExam.php
@@ -12,25 +12,11 @@ class TmjClinicalExam extends Model implements Resource, Repository
     use WithoutUpdatedTimestamp;
 
     /**
-     * Mass assignable attributes
+     * Guarded attributes
      *
      * @var array
      */
-    protected $fillable = [
-        'formid', 'patientid', 'palpationid', 'palpationRid',
-        'additional_paragraph_pal', 'joint_exam', 'jointid',
-        'i_opening_from', 'i_opening_to', 'i_opening_equal',
-        'protrusion_from', 'protrusion_to', 'protrusion_equal',
-        'l_lateral_from', 'l_lateral_to', 'l_lateral_equal',
-        'r_lateral_from', 'r_lateral_to', 'r_lateral_equal',
-        'deviation_from', 'deviation_to', 'deviation_equal',
-        'deflection_from', 'deflection_to', 'deflection_equal',
-        'range_normal', 'normal', 'other_range_motion',
-        'additional_paragraph_rm', 'screening_aware',
-        'screening_normal', 'userid', 'docid', 'status',
-        'adddate', 'ip_address', 'deviation_r_l', 'deflection_r_l',
-        'dentaldevice', 'dentaldevice_date'
-    ];
+    protected $guarded = ['ex_page5id'];
 
     /**
      * The database table used by the model.
--- tests/TmjClinicalExamsApiTest.php
+++ tests/TmjClinicalExamsApiTest.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Carbon\Carbon;
 
 use DentalSleepSolutions\Eloquent\Dental\TmjClinicalExam;
 
@@ -17,49 +16,12 @@ class TmjClinicalExamsApiTest extends TestCase
      */
     public function testAddTmjClinicalExam()
     {
-        $data = [
-            'formid'                   => 5,
-            'patientid'                => 5,
-            'palpationid'              => '1|1~3|4~5|1~',
-            'palpationRid'             => '1|1~2|4~5|1~',
-            'additional_paragraph_pal' => 'Add additional paragraph pal',
-            'joint_exam'               => '~2~3~',
-            'jointid'                  => '1|LMMM~2|L~3|R~4|B~5|B~',
-            'i_opening_from'           => '22',
-            'i_opening_to'             => '22',
-            'i_opening_equal'          => '22',
-            'protrusion_from'          => '22',
-            'protrusion_to'            => '22',
-            'protrusion_equal'         => '22',
-            'l_lateral_from'           => '22',
-            'l_lateral_to'             => '22',
-            'l_lateral_equal'          => '22',
-            'r_lateral_from'           => '22',
-            'r_lateral_to'             => '22',
-            'r_lateral_equal'          => '22',
-            'deviation_from'           => '22',
-            'deviation_to'             => '22',
-            'deviation_equal'          => '22',
-            'deflection_from'          => '22',
-            'deflection_to'            => '22',
-            'deflection_equal'         => '22',
-            'range_normal'             => '22',
-            'normal'                   => '22',
-            'other_range_motion'       => 'Add other range motion',
-            'additional_paragraph_rm'  => 'Add additional paragraph rm',
-            'screening_aware'          => '3',
-            'screening_normal'         => '3',
-            'userid'                   => 7,
-            'docid'                    => 7,
-            'status'                   => 7,
-            'deviation_r_l'            => 'Right',
-            'deflection_r_l'           => 'Left',
-            'dentaldevice'             => 10,
-            'dentaldevice_date'        => Carbon::now()
-        ];
+        $data = factory(TmjClinicalExam::class)->make()->toArray();
+
+        $data['formid'] = 100;
 
         $this->post('/api/v1/tmj-clinical-exams', $data)
-            ->seeInDatabase('dental_ex_page5', ['other_range_motion' => 'Add other range motion'])
+            ->seeInDatabase('dental_ex_page5', ['formid' => 100])
             ->assertResponseOk();
     }
 
@@ -73,12 +35,12 @@ class TmjClinicalExamsApiTest extends TestCase
         $tmjClinicalExamTestRecord = factory(TmjClinicalExam::class)->create();
 
         $data = [
-            'other_range_motion' => 'Update other range motion',
-            'dentaldevice'       => '9',
+            'patientid'          => 100,
+            'other_range_motion' => 'test'
         ];
 
         $this->put('/api/v1/tmj-clinical-exams/' . $tmjClinicalExamTestRecord->ex_page5id, $data)
-            ->seeInDatabase('dental_ex_page5', ['other_range_motion' => 'Update other range motion'])
+            ->seeInDatabase('dental_ex_page5', ['patientid' => 100])
             ->assertResponseOk();
     }
 

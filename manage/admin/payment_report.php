4387e17f7627fe58f2dc296a3c9d3593f749578c
AstDerek <ast.derek@gmail.com>
2016-02-05 14:59:58 -0600

    CS-76 Check proper permissions to view the payment report

--- manage/admin/payment_report.php
+++ manage/admin/payment_report.php
@@ -1,8 +1,13 @@
 <?php
 namespace Ds3\Libraries\Legacy;
 
+$is_front_office = false;
+$is_back_office = true;
+
 $mark_as_viewed = false;
 $header_class = 'page-header';
-$table_style = 'class="table table-bordered table-hover"';
+$table_style = 'class="table -table-bordered table-hover"';
 
-include_once('../includes/payment_report.inc');
+require_once __DIR__ . '/includes/top.htm';
+require_once __DIR__ . '/../includes/payment_report.inc';
+require_once __DIR__ . '/includes/bottom.htm';
--- manage/payment_report.php
+++ manage/payment_report.php
@@ -1,8 +1,13 @@
 <?php
 namespace Ds3\Libraries\Legacy;
 
+$is_front_office = true;
+$is_back_office = false;
+
 $mark_as_viewed = true;
 $header_class = 'admin_head';
 $table_style = 'width="98%" cellpadding="5" cellspacing="1" bgcolor="#FFFFFF" align="center"';
 
-include_once('includes/payment_report.inc');
+require_once __DIR__ . '/includes/top.htm';
+require_once __DIR__ . '/includes/payment_report.inc';
+require_once __DIR__ . '/includes/bottom.htm';

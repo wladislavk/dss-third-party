55bce66c4d7d4bf07bcdb0b267aab5e8059af18b
Sergey <gerra.lampdev@gmail.com>
2016-02-11 14:36:46 +0200

    DSS-308 * Added base structure of the resource Insurances

--- app/Eloquent/Dental/Insurance.php
+++ app/Eloquent/Dental/Insurance.php
@@ -3,9 +3,49 @@
 namespace DentalSleepSolutions\Eloquent\Dental;
 
 use Illuminate\Database\Eloquent\Model;
+use DentalSleepSolutions\Eloquent\WithoutUpdatedTimestamp;
+use DentalSleepSolutions\Contracts\Resources\Insurance as Resource;
+use DentalSleepSolutions\Contracts\Repositories\Insurances as Repository;
 
-class Insurance extends Model
+class Insurance extends Model implements Resource, Repository
 {
+    use WithoutUpdatedTimestamp;
+
+    /**
+     * Guarded attributes
+     *
+     * @var array
+     */
+    protected $guarded = ['insuranceid'];
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
     protected $table = 'dental_insurance';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
     protected $primaryKey = 'insuranceid';
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'patient_dob', 'insured_dob', 'patient_signed_date',
+        'service_date1_to', 'physician_signed_date', 'mailed_date', 'sec_mailed_date', 'percase_date'
+    ];
+
+    /**
+     * The name of the "created at" column.
+     *
+     * @var string
+     */
+    const CREATED_AT = 'adddate';
 }

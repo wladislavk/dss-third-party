2767665d855924bb40462bcfe401246112ed915c
Sergey <gerra.lampdev@gmail.com>
2016-03-07 13:01:12 +0200

    DSS-350 * Updated the factory * Added a unit test

--- app/Eloquent/Dental/PlaceService.php
+++ app/Eloquent/Dental/PlaceService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DentalSleepSolutions\Eloquent;
+namespace DentalSleepSolutions\Eloquent\Dental;
 
 use Illuminate\Database\Eloquent\Model;
 use DentalSleepSolutions\Eloquent\WithoutUpdatedTimestamp;

dd36cd5e91a34161dc6f4dfeda8fa874a3cab314
AstDerek <ast.derek@gmail.com>
2016-03-13 23:38:22 -0600

    DSS-323 Display comprehensive claim history, format raw view, show snapshots in Paper/E-File

--- manage/claim_history_versions_view.php
+++ manage/claim_history_versions_view.php
@@ -1,12 +1,23 @@
-<?php namespace Ds3\Libraries\Legacy; ?><?php 
-	include 'includes/top.htm';
+<?php
+namespace Ds3\Libraries\Legacy;
 
-	$s = "SELECT * FROM dental_insurance_history WHERE id='".mysqli_real_escape_string($con, !empty($_GET['id']) ? $_GET['id'] : '')."'";
-	
-	$r = $db->getRow($s);
-?>
-	<div class="fullwidth">
-		<?php print_r($r); ?>
-	</div>
+require_once __DIR__ . '/includes/constants.inc';
+require_once __DIR__ . '/admin/includes/main_include.php';
+require_once __DIR__ . '/includes/sescheck.php';
+require_once __DIR__ . '/includes/claim_create.php';
+require_once __DIR__ . '/includes/claim_functions.php';
 
-<?php include 'includes/bottom.htm'; ?>
+$is_front_office = true;
+$manage_path = "";
+$admin_path = "admin/";
+$called_from = "manage_insurance.php";
+$electronic_form = "insurance_eligible.php?insid=".(!empty($_GET['insid']) ? $_GET['insid'] : '')."&pid=".(!empty($_GET['pid']) ? $_GET['pid'] : '');
+
+$isEClaimView = $_GET['view'] === 'efile';
+$isHistoricView = true;
+
+if ($isEClaimView) {
+    require_once __DIR__ . '/includes/claim_form_eligible.inc';
+} else {
+    require_once __DIR__ . '/includes/claim_form_v2.inc';
+}

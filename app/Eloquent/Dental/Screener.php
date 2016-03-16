d4065f1ac1fc777c4abc3204dd3a485219f61704
Sergey <gerra.lampdev@gmail.com>
2016-03-16 15:58:29 +0200

    DSS-367 * Added base structure of the resource Screeners

---
+++ app/Eloquent/Dental/Screener.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DentalSleepSolutions\Eloquent\Dental;
+
+use Illuminate\Database\Eloquent\Model;
+use DentalSleepSolutions\Eloquent\WithoutUpdatedTimestamp;
+use DentalSleepSolutions\Contracts\Resources\Screener as Resource;
+use DentalSleepSolutions\Contracts\Repositories\Screeners as Repository;
+
+class Screener extends Model implements Resource, Repository
+{
+    use WithoutUpdatedTimestamp;
+
+    /**
+     * Guarded attributes
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'dental_screener';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The name of the "created at" column.
+     *
+     * @var string
+     */
+    const CREATED_AT = 'adddate';
+}
---
+++ app/Http/Requests/ScreenerUpdate.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace DentalSleepSolutions\Http\Requests;
+
+class ScreenerUpdate extends Request
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'docid'                 => 'sometimes|required|integer',
+            'userid'                => 'sometimes|required|integer',
+            'first_name'            => 'sometimes|required|string',
+            'last_name'             => 'sometimes|required|string',
+            'email'                 => 'email',
+            'epworth_reading'       => 'integer',
+            'epworth_public'        => 'integer',
+            'epworth_passenger'     => 'integer',
+            'epworth_lying'         => 'integer',
+            'epworth_talking'       => 'integer',
+            'epworth_lunch'         => 'integer',
+            'epworth_traffic'       => 'integer',
+            'snore_1'               => 'integer',
+            'snore_2'               => 'integer',
+            'snore_3'               => 'integer',
+            'snore_4'               => 'integer',
+            'snore_5'               => 'integer',
+            'breathing'             => 'integer',
+            'driving'               => 'integer',
+            'gasping'               => 'integer',
+            'sleepy'                => 'integer',
+            'snore'                 => 'integer',
+            'weight_gain'           => 'integer',
+            'blood_pressure'        => 'integer',
+            'jerk'                  => 'integer',
+            'burning'               => 'integer',
+            'headaches'             => 'integer',
+            'falling_asleep'        => 'integer',
+            'staying_asleep'        => 'integer',
+            'rx_blood_pressure'     => 'integer',
+            'rx_hypertension'       => 'integer',
+            'rx_heart_disease'      => 'integer',
+            'rx_stroke'             => 'integer',
+            'rx_apnea'              => 'integer',
+            'rx_diabetes'           => 'integer',
+            'rx_lung_disease'       => 'integer',
+            'rx_insomnia'           => 'integer',
+            'rx_depression'         => 'integer',
+            'rx_narcolepsy'         => 'integer',
+            'rx_medication'         => 'integer',
+            'rx_restless_leg'       => 'integer',
+            'rx_headaches'          => 'integer',
+            'rx_heartburn'          => 'integer',
+            'rx_cpap'               => 'integer',
+            'phone'                 => 'sometimes|required|regex:/^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$/',
+            'contacted'             => 'integer',
+            'patient_id'            => 'integer',
+            'rx_metabolic_syndrome' => 'integer',
+            'rx_obesity'            => 'integer',
+            'rx_afib'               => 'integer'
+        ];
+    }
+}

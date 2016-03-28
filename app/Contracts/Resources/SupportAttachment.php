b00345370a005536682f429d511b9844ed94df30
Sergey <gerra.lampdev@gmail.com>
2016-03-28 19:02:18 +0300

    DSS-380 * Added base structure of the resource Support Attachments

---
+++ app/Contracts/Resources/SupportAttachment.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace DentalSleepSolutions\Contracts\Resources;
+
+interface SupportAttachment extends Resource
+{
+
+}
---
+++ app/Eloquent/Dental/SupportAttachment.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace DentalSleepSolutions\Eloquent\Dental;
+
+use Illuminate\Database\Eloquent\Model;
+use DentalSleepSolutions\Eloquent\WithoutUpdatedTimestamp;
+use DentalSleepSolutions\Contracts\Resources\SupportAttachment as Resource;
+use DentalSleepSolutions\Contracts\Repositories\SupportAttachments as Repository;
+
+class SupportAttachment extends Model implements Resource, Repository
+{
+    use WithoutUpdatedTimestamp;
+
+    /**
+     * Mass assignable attributes
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'ticket_id', 'response_id', 'filename',
+        'adddate', 'ip_address'
+    ];
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'dental_support_attachment';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The name of the "created at" column.
+     *
+     * @var string
+     */
+    const CREATED_AT = 'adddate';
+}

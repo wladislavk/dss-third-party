f97c0da587feb9cf3d928e9943c1e0f1adc5d168
AstDerek <ast.derek@gmail.com>
2016-03-28 21:31:25 -0600

    DSS-381 Remove alert from calendar functions

--- manage/calendar1.js
+++ manage/calendar1.js
@@ -155,6 +155,9 @@ function cal_prs_time1 (str_time, dt_date) {
 }
 
 function cal_error (str_message) {
-	alert (str_message);
+	if (typeof console === 'object' && console.hasOwnProperty('warn')) {
+		console.warn('calendar1.js:', str_message);
+	}
+
 	return null;
 }

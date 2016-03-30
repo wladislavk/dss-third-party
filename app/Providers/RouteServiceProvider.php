fc9791307b10553067b1b08e650c2b1309de3e3f
Sergey <gerra.lampdev@gmail.com>
2016-03-30 15:36:24 +0300

    DSS-384 * Added base structure of the resource Support Tickets

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,6 +24,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var array
      */
     protected $resourceBindings = [
+        'support-tickets' => \DentalSleepSolutions\Eloquent\Dental\SupportTicket::class,
         'guide-settings' => \DentalSleepSolutions\Eloquent\Dental\GuideSetting::class,
         'guide-devices' => \DentalSleepSolutions\Eloquent\Dental\GuideDevice::class,
         'diagnostics' => \DentalSleepSolutions\Eloquent\Dental\Diagnostic::class,

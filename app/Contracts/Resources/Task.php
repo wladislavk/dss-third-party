b05a25aa8113948c2d3efb5a86c9807c70ae58dd
Sergey <gerra.lampdev@gmail.com>
2016-03-30 18:20:36 +0300

    DSS-387 * Added base structure of the resource Tasks

---
+++ app/Contracts/Resources/Task.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace DentalSleepSolutions\Contracts\Resources;
+
+interface Task extends Resource
+{
+
+}
---
+++ app/Eloquent/Dental/Task.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DentalSleepSolutions\Eloquent\Dental;
+
+use Illuminate\Database\Eloquent\Model;
+use DentalSleepSolutions\Eloquent\WithoutUpdatedTimestamp;
+use DentalSleepSolutions\Contracts\Resources\Task as Resource;
+use DentalSleepSolutions\Contracts\Repositories\Tasks as Repository;
+
+class Task extends Model implements Resource, Repository
+{
+    use WithoutUpdatedTimestamp;
+
+    /**
+     * Guarded attributes
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'dental_task';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The name of the "created at" column.
+     *
+     * @var string
+     */
+    const CREATED_AT = 'adddate';
+}

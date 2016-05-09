b2f843c9a2a5c86c9d55ec91c361700cc9f70ea5
Sergey <gerra.lampdev@gmail.com>
2016-05-09 14:15:20 +0300

    DSS-429 * Updated the migration of view combine users

--- database/migrations/2015_12_22_203443_views_combine_users.php
+++ database/migrations/2015_12_22_203443_views_combine_users.php
@@ -15,14 +15,49 @@ class ViewsCombineUsers extends Migration
     {
         DB::unprepared('drop view if exists v_users');
 
-        DB::unprepared(
-            "create view v_users as ".
+        DB::unprepared("create view v_users as "
             // dental_users table
-            "select concat('u_', userid) COLLATE utf8_unicode_ci as id, email, if(name is null, concat(first_name,' ', last_name), name) as name, first_name, last_name, username, password, salt, status, ip_address, user_access as access, adddate from dental_users ".
-            "union ".
+            . "select
+                concat('u_', u.userid) COLLATE utf8_unicode_ci as id,
+                email,
+                if(name is null, concat(first_name,' ', last_name), name) as name,
+                first_name,
+                last_name,
+                username,
+                password,
+                salt,
+                status,
+                u.ip_address,
+                user_access as access,
+                uc.companyid as companyid,
+                u.adddate
+            from dental_users u
+            left join dental_user_company uc
+                ON uc.userid = (
+                    case docid
+                        when 0 then u.userid
+                        else docid
+                end)"
+            . " union "
             // admin table
-            "select concat('a_', adminid) COLLATE utf8_unicode_ci as id, email, if(name is null, concat(first_name,' ', last_name), name) as name, first_name, last_name, username, password, salt, status, ip_address, admin_access access, adddate from admin"
-        );
+            . "select
+                concat('a_', a.adminid) COLLATE utf8_unicode_ci as id,
+                email,
+                if(name is null, concat(first_name,' ', last_name), name) as name,
+                first_name,
+                last_name,
+                username,
+                password,
+                salt,
+                status,
+                a.ip_address,
+                admin_access access,
+                    ac.companyid as companyid,
+                    a.adddate
+            from admin a
+            left join admin_company ac
+                ON a.adminid = ac.adminid
+        ");
     }
 
     /**

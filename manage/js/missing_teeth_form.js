7db7dd2c8b69317611d47603891127a694fb773c
AstDerek <ast.derek@gmail.com>
2016-05-23 19:56:00 -0500

    DSS-447 Update class/id references, and update js logic for perio chart

--- manage/js/missing_teeth_form.js
+++ manage/js/missing_teeth_form.js
@@ -3,11 +3,14 @@ function updateTeeth(teeth){
        s = $(this).attr('src');
        $(this).attr('src', s.replace('_red', ''));
     });
-    teeth = teeth.replace(' ', '');
+    teeth = teeth.replace(/\s+/g, '');
     ts = teeth.split(',');
+    console.info(teeth);
+    console.info(ts);
     for(var tn in ts){
         t = ts[tn];
-        $('#missing_'+t).attr('src', 'missing_teeth/'+t+'_red.png');
+        console.info('[id="missing_' + t + '"]');
+        $('[id="missing_' + t + '"]').attr('src', 'missing_teeth/'+t+'_red.png');
     }
 }
 

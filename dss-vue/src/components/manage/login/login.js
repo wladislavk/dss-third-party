ad11e88148a81a93860d6be599572335cca2a2a6
Sergey <gerra.lampdev@gmail.com>
2016-08-02 15:44:43 +0300

    DSS-476 * Error with duplicate script for Godaddy verification after redirect (testing)

--- dss-vue/src/components/manage/login/login.js
+++ dss-vue/src/components/manage/login/login.js
@@ -22,12 +22,14 @@ module.exports = {
 
             this.$route.router.go('/manage/index');
         }
-    },
-    ready: function() {
+        /*
         this.loadScriptFrom(
             'https://seal.godaddy.com/getSeal?sealID=3b7qIyHRrOjVQ3mCq2GohOZtQjzgc1JF4ccCXdR6VzEhui2863QRhf',
-            '#siteseal'
+            '#siteseal',
+            'verifySeal'
         );
+        */
+        alert('compiled');
     },
     methods: {
         submitForm: function() {

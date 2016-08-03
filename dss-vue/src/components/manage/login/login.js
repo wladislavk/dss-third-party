1f140664799679f693be3ac13431b4511703f03f
Sergey <gerra.lampdev@gmail.com>
2016-08-03 13:16:57 +0300

    DSS-476 * Finished implementation of displaying of Godaddy verification

--- dss-vue/src/components/manage/login/login.js
+++ dss-vue/src/components/manage/login/login.js
@@ -22,14 +22,14 @@ module.exports = {
 
             this.$route.router.go('/manage/index');
         }
-        /*
+    },
+    ready: function() {
         this.loadScriptFrom(
             'https://seal.godaddy.com/getSeal?sealID=3b7qIyHRrOjVQ3mCq2GohOZtQjzgc1JF4ccCXdR6VzEhui2863QRhf',
             '#siteseal',
-            'verifySeal'
+            window.verifySeal,
+            window.seal_installSeal
         );
-        */
-        alert('compiled');
     },
     methods: {
         submitForm: function() {
--- dss-vue/src/modules/loader/ScriptLoaderMixin.js
+++ dss-vue/src/modules/loader/ScriptLoaderMixin.js
@@ -1,14 +1,20 @@
 module.exports = {
     methods: {
-        loadScriptFrom: function(path, toElement) {
-            var scriptElement   = document.createElement('script');
-            scriptElement.type  = 'text/javascript';
-            scriptElement.src   = path;
-            scriptElement.async = true;
+        loadScriptFrom: function(path, toElement, requiredFunction, externalFunction) {
+            if (externalFunction === undefined) {
+                externalFunction = () => {};
+            }
 
-            console.log($(this.$el).find(toElement));
+            if (!requiredFunction) {
+                var scriptElement   = document.createElement('script');
+                scriptElement.type  = 'text/javascript';
+                scriptElement.src   = path;
+                scriptElement.async = true;
 
-            $(this.$el).find(toElement).append(scriptElement);
+                $(this.$el).find(toElement).append(scriptElement);
+            } else {
+                externalFunction();
+            }
         }
     }
 }

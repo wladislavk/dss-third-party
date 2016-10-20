6320bf97811d7bf4ed5ebef2e293bf7a26c5363d
Sergey <gerra.lampdev@gmail.com>
2016-10-20 17:11:29 +0300

    DSS-500 * Updated View Contact component * Updated Quick View link in Contacts component * Updated Modal component for displaying View Contact component

--- dss-vue/src/components/manage/contacts/ViewContact.js
+++ dss-vue/src/components/manage/contacts/ViewContact.js
@@ -3,35 +3,39 @@ var handlerMixin = require('../../../modules/handler/HandlerMixin.js');
 module.exports = {
     data: function() {
         return {
-           physicianTypes : '',
-           contact        : {}
+           contact         : {},
+           componentParams : {}
         }
     },
     mixins: [handlerMixin],
-    ready: function() {
-        this.getPhysicianContactTypes()
-            .then(function(response) {
-                var data = response.data.data;
+    computed: {
+        'contact.phone1': function() {
+            var phone = this.contact.phone1;
+            phone = phone.replace(/[^0-9]/g, '').replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
 
-                if (data) {
-                    this.$set('physicianTypes', data.physician_types);
-                }
-            }, function(response) {
-                this.handleErrors('getPhysicianContactTypes', response);
-            });
+            this.$set('contact.phone1', phone);
+        }
+    },
+    events: {
+        'setting-component-params': function(parameters) {
+            this.componentParams = parameters;
 
-        this.getContactById()
-            .then(function(response) {
-                var data = response.data.data;
-
-                if (data) {
-                    this.$set('contact', data);
-                }
-            }, function(response) {
-                this.handleErrors('getContactById', response);
-            });
+            this.setCurrentContact(this.componentParams.contactId);
+        }
     },
     methods: {
+        setCurrentContact: function(contactId) {
+            this.getContactById(contactId)
+                .then(function(response) {
+                    var data = response.data.data;
+
+                    if (data) {
+                        this.$set('contact', data);
+                    }
+                }, function(response) {
+                    this.handleErrors('getContactById', response);
+                });
+        },
         getPhysicianContactTypes: function() {
             return this.$http.post(window.config.API_PATH + 'contact-types/physician');
         },

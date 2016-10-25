5211757508093108bb4716b6f09e798a3a82f17d
Sergey <gerra.lampdev@gmail.com>
2016-10-25 12:31:48 +0300

    DSS-501 * Added methods for working with the API

--- dss-vue/src/components/manage/contacts/EditContact.js
+++ dss-vue/src/components/manage/contacts/EditContact.js
@@ -14,7 +14,37 @@ module.exports = {
     },
     methods: {
         updateContact: function(contact) {
-            return this.$http.
+            return this.$http.put(window.config.API_PATH + 'contacts/' + contact.contactid, contact);
+        },
+        insertContact: function(contact) {
+            return this.$http.post(window.config.API_PATH + 'contacts', contact);
+        },
+        getLetterInfoByDocId: function() {
+            return this.$http.post(window.config.API_PATH + 'users/letter-info');
+        },
+        getContactType: function(contactTypeId) {
+            return this.$http.get(window.config.API_PATH + 'contact-types/' + contactTypeId);
+        },
+        getContactTypeWithFilter: function() {
+            var data = {
+                where: { physician: 1 }
+            };
+
+            return this.$http.post(window.config.API_PATH + 'contact-types/with-filter', data);
+        },
+        getContact: function(contactId) {
+            return this.$http.get(window.config.API_PATH + 'contacts/' + contactId);
+        },
+        getActiveNonCorporateContactTypes: function() {
+            return this.$http.post(window.config.API_PATH + 'contact-types/active-non-corporate');
+        },
+        getActiveQualifiers: function() {
+            return this.$http.post(window.config.API_PATH + 'qualifiers/active');
+        },
+        getPendingVOBsByContactId: function(contactId) {
+            var data = { contact_id: contactid };
+
+            return this.$http.post(window.config.API_PATH + 'insurance-preauth/pending-VOB', data);
         }
     }
 }

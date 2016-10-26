1b20f35ee78866b179506cded6ff9d612a96a936
Sergey <gerra.lampdev@gmail.com>
2016-10-26 15:04:17 +0300

    DSS-501 * Updated Edit Contact component

--- dss-vue/src/components/manage/contacts/EditContact.js
+++ dss-vue/src/components/manage/contacts/EditContact.js
@@ -3,14 +3,80 @@ var handlerMixin = require('../../../modules/handler/HandlerMixin.js');
 module.exports = {
     data: function() {
         return {
-            componentParams : {}
+            componentParams                : {},
+            contactTypesOfPhysician        : '',
+            contact                        : {},
+            activeNonCorporateContactTypes : [],
+            activeQualifiers               : [],
+            pendingVOB                     : {}
         }
     },
     mixins: [handlerMixin],
+    computed: {
+        'contact.name': function() {
+            return this.contact.firstname + ' ' + this.contact.middlename + ' ' + this.contact.lastname;
+        }
+    },
     events: {
         'setting-component-params': function(parameters) {
             this.componentParams = parameters;
+
+            this.getContact(this.componentParams.contactId)
+                .then(function(response) {
+                    var data = response.data.data;
+
+                    if (data) {
+                        this.$set('contact', data);
+                    }
+                }, function(response) {
+                    this.handleErrors('getContactTypesOfPhysician', response);
+                });
+
+            this.getPendingVOBsByContactId()
+                .then(function(response) {
+                    var data = response.data.data;
+
+                    if (data) {
+                        this.$set('pendingVOB', data);
+                    }
+                }, function(response) {
+                    this.handleErrors('getPendingVOBsByContactId', response);
+                });
         }
+    },
+    ready: function() {
+        this.getContactTypesOfPhysician()
+            .then(function(response) {
+                var data = response.data.data;
+
+                if (data) {
+                    this.$set('contactTypesOfPhysician', data);
+                }
+            }, function(response) {
+                this.handleErrors('getContactTypesOfPhysician', response);
+            });
+
+        this.getActiveNonCorporateContactTypes()
+            .then(function(response) {
+                var data = response.data.data;
+
+                if (data.length) {
+                    this.$set('activeNonCorporateContactTypes', data);
+                }
+            }, function(response) {
+                this.handleErrors('getActiveNonCorporateContactTypes', response);
+            });
+
+        this.getActiveQualifiers()
+            .then(function(response) {
+                var data = response.data.data;
+
+                if (data.length) {
+                    this.$set('activeQualifiers', data);
+                }
+            }, function(response) {
+                this.handleErrors('getActiveQualifiers', response);
+            });
     },
     methods: {
         updateContact: function(contact) {
@@ -25,12 +91,8 @@ module.exports = {
         getContactType: function(contactTypeId) {
             return this.$http.get(window.config.API_PATH + 'contact-types/' + contactTypeId);
         },
-        getContactTypeWithFilter: function() {
-            var data = {
-                where: { physician: 1 }
-            };
-
-            return this.$http.post(window.config.API_PATH + 'contact-types/with-filter', data);
+        getContactTypesOfPhysician: function() {
+            return this.$http.post(window.config.API_PATH + 'contact-types/physician');
         },
         getContact: function(contactId) {
             return this.$http.get(window.config.API_PATH + 'contacts/' + contactId);
@@ -42,7 +104,7 @@ module.exports = {
             return this.$http.post(window.config.API_PATH + 'qualifiers/active');
         },
         getPendingVOBsByContactId: function(contactId) {
-            var data = { contact_id: contactid };
+            var data = { contact_id: contactId };
 
             return this.$http.post(window.config.API_PATH + 'insurance-preauth/pending-VOB', data);
         }

edc2be10952231ee32ac6e3220ae74c8f9fbe84e
Sergey <gerra.lampdev@gmail.com>
2016-11-08 16:05:22 +0200

    DSS-501 * Added filtering phone/fax fields during updating/storing a contact

--- app/Http/Requests/ContactStore.php
+++ app/Http/Requests/ContactStore.php
@@ -2,6 +2,8 @@
 
 namespace DentalSleepSolutions\Http\Requests;
 
+use Illuminate\Support\Arr;
+
 class ContactStore extends Request
 {
     /**
@@ -43,5 +45,19 @@ class ContactStore extends Request
             'corporate'            => 'integer',
             'dea_number'           => 'string'
         ];
+    }
+
+    public function all()
+    {
+        $data = parent::all();
+
+        $phoneFields = ['phone1', 'phone2', 'fax'];
+        foreach ($phoneFields as $field) {
+            if (Arr::has($data, $field)) {
+                $data[$field] = preg_replace('/[^0-9]/', '', $data[$field]);
+            }
+        }
+
+        return $data;
     }
 }

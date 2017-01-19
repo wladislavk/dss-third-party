d03538099b823dea4978024a856284c261df83e1
AstDerek <ast.derek@gmail.com>
2017-01-19 15:51:27 -0600

    DSS-516 Match conditionals on export md and manage contact, use fputcsv

--- manage/export_md.php
+++ manage/export_md.php
@@ -1,70 +1,94 @@
-<?php namespace Ds3\Libraries\Legacy; ?><?php
-include_once 'admin/includes/main_include.php';
-include_once 'includes/constants.inc';
+<?php
+namespace Ds3\Libraries\Legacy;
+
+include_once __DIR__ . '/admin/includes/main_include.php';
+include_once __DIR__ . '/includes/constants.inc';
+
+$docId = intval($_SESSION['docid']);
+
 header("Content-type: text/csv");
 header("Content-Disposition: attachment; filename=dss_md_export.csv");
 header("Pragma: no-cache");
 header("Expires: 0");
 
-$csv  = 'Salutation,';
-$csv .= 'First Name,';
-$csv .= 'Last Name,';
-$csv .= 'Middle Init,';
-$csv .= 'Company,';
-$csv .= 'Address 1,';
-$csv .= 'Address 2,';
-$csv .= 'City,';
-$csv .= 'State,';
-$csv .= 'Zip,';
-$csv .= 'Phone 1,';
-$csv .= 'Phone 2,';
-$csv .= 'Fax,';
-$csv .= 'Email,';
-$csv .= 'National Provider ID,';
-$csv .= 'Qualifier,';
-$csv .= 'ID,';
-$csv .= 'Notes,';
-$csv .= 'Preferred Contact Method,';
-$csv .= 'Referrer,';
-$csv .= 'Status';
-$csv .= "\n";
+$out = fopen('php://output', 'w');
 
-$sql = "select dc.*, dq.qualifier as qualifier_name, 
-		(SELECT count(p.patientid) FROM dental_patients p WHERE dc.contactid=p.referred_by 
-                        AND p.referred_source=".DSS_REFERRED_PHYSICIAN.") as num_ref
-		from dental_contact dc
-		JOIN dental_contacttype dct ON dc.contacttypeid = dct.contacttypeid
-		LEFT JOIN dental_qualifier dq ON dq.qualifierid = dc.qualifier
-		where dc.docid='".$_SESSION['docid']."' 
-			AND dct.physician=1
-		order by dc.lastname";
-$q = $db->getResults($sql);
+$csv = [
+    'Salutation',
+    'First Name',
+    'Last Name',
+    'Middle Init',
+    'Company',
+    'Address 1',
+    'Address 2',
+    'City',
+    'State',
+    'Zip',
+    'Phone 1',
+    'Phone 2',
+    'Fax',
+    'Email',
+    'National Provider ID',
+    'Qualifier',
+    'ID',
+    'Notes',
+    'Preferred Contact Method',
+    'Referrer',
+    'Status'
+];
 
-foreach ($q as $r) {
-	$csv .= '"'.$r['salutation'].'",';
-	$csv .= '"'.$r['firstname'].'",';
-	$csv .= '"'.$r['lastname'].'",';
-	$csv .= '"'.$r['middlename'].'",';
-	$csv .= '"'.$r['company'].'",';
-	$csv .= '"'.$r['add1'].'",';
-	$csv .= '"'.$r['add2'].'",';
-	$csv .= '"'.$r['city'].'",';
-	$csv .= '"'.$r['state'].'",';
-	$csv .= '"'.$r['zip'].'",';
-	$csv .= '"'.$r['phone1'].'",';
-	$csv .= '"'.$r['phone2'].'",';
-	$csv .= '"'.$r['fax'].'",';
-	$csv .= '"'.$r['email'].'",';
-	$csv .= '"'.$r['national_provider_id'].'",';
-	$csv .= '"'.$r['qualifier_name'].'",';
-	$csv .= '"'.$r['qualifierid'].'",';
-	$csv .= '"'.$r['notes'].'",';
-	$csv .= '"'.$r['preferredcontact'].'",';
-	$csv .= '"'.(($r['num_ref']>0)?'Yes':'No').'",';
-	$csv .= '"'.(($r['status']==1)?'Active':'Inactive').'"';
-	$csv .= "\n";
+fputcsv($out, $csv);
+
+$sql = "SELECT
+        dc.*,
+        dq.qualifier AS qualifier_name, 
+        (
+            SELECT COUNT(p.patientid)
+            FROM dental_patients p
+            WHERE dc.contactid = p.referred_by
+                AND p.referred_source = " . DSS_REFERRED_PHYSICIAN . "
+        ) AS num_ref
+    FROM dental_contact dc
+        JOIN dental_contacttype dct ON dc.contacttypeid = dct.contacttypeid
+        LEFT JOIN dental_qualifier dq ON dq.qualifierid = dc.qualifier
+    WHERE dc.docid = '$docId' 
+        AND dct.physician = 1
+        AND dc.merge_id IS NULL
+        AND dc.status = 1
+    ORDER BY IF (dc.lastname = '' OR dc.lastname IS NULL, 1, 0) ASC,
+        dc.lastname ASC,
+        dc.firstname ASC,
+        dc.company ASC,
+        dct.contacttype ASC
+    ";
+$contacts = $db->getResults($sql);
+
+foreach ($contacts as $each) {
+    $csv = [
+        $each['salutation'],
+        $each['firstname'],
+        $each['lastname'],
+        $each['middlename'],
+        $each['company'],
+        $each['add1'],
+        $each['add2'],
+        $each['city'],
+        $each['state'],
+        $each['zip'],
+        $each['phone1'],
+        $each['phone2'],
+        $each['fax'],
+        $each['email'],
+        $each['national_provider_id'],
+        $each['qualifier_name'],
+        $each['qualifierid'],
+        $each['notes'],
+        $each['preferredcontact'],
+        $each['num_ref'] > 0 ? 'Yes' : 'No',
+        $each['status'] == 1 ? 'Active' : 'Inactive',
+    ];
+    
+    fputcsv($out, $csv);
 }
 
-
-echo $csv;
-?>
+fclose($out);

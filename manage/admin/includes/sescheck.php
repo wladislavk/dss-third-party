ff5659a54b93cbc97eb78489a23bf9512eeb7a95
AstDerek <ast.derek@gmail.com>
2017-03-14 11:24:04 -0600

    DSS-534 Check BO session when accessing a Letter PDF

--- manage/admin/includes/sescheck.php
+++ manage/admin/includes/sescheck.php
@@ -1,16 +1,15 @@
-<?php namespace Ds3\Libraries\Legacy; ?><?php
-	include_once 'main_include.php';
+<?php
+namespace Ds3\Libraries\Legacy;
 
-	if(empty($_SESSION["adminuserid"]))
-	{
-		?>
-		<script type="text/javascript">
-			window.location = "index.php";
-		</script>
-		<?php 
-		trigger_error("Die called", E_USER_ERROR);
-	}else{
-		$query = "UPDATE admin SET last_accessed_date = NOW() WHERE adminid='".mysqli_real_escape_string($con,$_SESSION['adminuserid'])."'";
-		mysqli_query($con, $query);
-	}
-?>
+include_once __DIR__ . '/main_include.php';
+
+if (empty($_SESSION["adminuserid"])) { ?>
+    <script type="text/javascript">
+        window.location = "index.php";
+    </script>
+    <?php
+    trigger_error("Die called", E_USER_ERROR);
+} else {
+    $query = "UPDATE admin SET last_accessed_date = NOW() WHERE adminid='".mysqli_real_escape_string($con,$_SESSION['adminuserid'])."'";
+    mysqli_query($con, $query);
+}

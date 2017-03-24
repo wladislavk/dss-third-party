5cea9535a6acd5f941929de5ea4e7e28ba5fd9cd
Sergey <gerra.lampdev@gmail.com>
2017-03-24 15:02:17 +0200

    DSS-538 * Update `SleepLab` component

--- vue-webpack/src/components/manage/sleeplabs/sleeplabs.js
+++ vue-webpack/src/components/manage/sleeplabs/sleeplabs.js
@@ -7,13 +7,13 @@ export default {
       routeParameters: {
         currentPageNumber: 0,
         sortDirection: 'asc',
-        sortColumn: 'name',
+        sortColumn: 'lab',
         currentLetter: null
       },
       letters: 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'.split(','),
       message: '',
       sleeplabsTotalNumber: 0,
-      sleeplabsPerPage: 20,
+      sleeplabsPerPage: 2,
       sleeplabs: [],
       tableHeaders: {
         'lab': {
@@ -38,48 +38,93 @@ export default {
     }
   },
   mixins: [handlerMixin],
+  watch: {
+    '$route.query.page': function () {
+      if (this.$route.query.page != undefined && this.$route.query.page <= this.totalPages) {
+        this.$set(this.routeParameters, 'currentPageNumber', this.$route.query.page)
+      }
+    },
+    '$route.query.sort': function () {
+      if (this.$route.query.sort in this.tableHeaders) {
+        this.$set(this.routeParameters, 'sortColumn', this.$route.query.sort)
+      } else {
+        this.$set(this.routeParameters, 'sortColumn', 'lab')
+      }
+    },
+    '$route.query.sortdir': function () {
+      if (this.$route.query.sortdir.toLowerCase() == 'desc') {
+        this.$set(this.routeParameters, 'sortDirection', this.$route.query.sortdir.toLowerCase())
+      } else {
+        this.$set(this.routeParameters, 'sortDirection', 'asc')
+      }
+    },
+    '$route.query.letter': function () {
+      if (this.letters.indexOf(this.$route.query.letter) > -1) {
+        this.$set(this.routeParameters, 'currentLetter', this.$route.query.letter)
+      } else {
+        this.$set(this.routeParameters, 'currentLetter', null)
+      }
+    },
+    '$route.query.delid': function () {
+      if (this.$route.query.delid > 0) {
+        // TODO: set fucntion on this event
+      }
+    },
+    'routeParameters': {
+      handler: function () {
+        this.getListOfSleeplabs()
+      },
+      deep: true
+    }
+  },
   computed: {
     totalPages () {
       return Math.ceil(this.sleeplabsTotalNumber / this.sleeplabsPerPage)
     }
   },
-  created () {
-
-  },
   mounted () {
-    this.getSleeplabs()
-      .then(function (response) {
-        var data = response.data.data;
-
-        if (data.total) {
-          data.result = data.result.forEach((value) => {
-            value['name'] = (value.salutation ? value.salutation + ' ' : '')
-              + (value.firstname ? value.firstname + ' ' : '')
-              + (value.middlename ? value.middlename + ' ' : '')
-              + (value.lastname || '')
-          })
-
-          this.sleeplabs = data.result
-        }
-      }, function (response) {
-        this.handleErrors('getSleeplabs', response)
-      })
+    this.getListOfSleeplabs()
   },
   methods: {
-    getCurrentDirection (sort) {
-      if (this.routeParameters.sortColumn == sort) {
-        return this.routeParameters.sortDirection.toLowerCase() === 'asc' ? 'desc' : 'asc'
-      } else {
-        return sort === 'name' ? 'asc': 'desc'
-      }
+    getListOfSleeplabs () {
+      this.getSleeplabs(
+        this.routeParameters.currentPageNumber,
+        this.sleeplabsPerPage,
+        this.routeParameters.sortColumn,
+        this.routeParameters.sortDirection,
+        this.routeParameters.currentLetter
+      ).then(function (response) {
+          var data = response.data.data;
+
+          if (data.total) {
+            data.result = data.result.map((value) => {
+              value['name'] = (value.salutation ? value.salutation + ' ' : '')
+                + (value.firstname ? value.firstname + ' ' : '')
+                + (value.middlename ? value.middlename + ' ' : '')
+                + (value.lastname || '')
+
+              value['show_patients'] = false
+
+              return value
+            })
+
+            this.sleeplabs = data.result
+            this.sleeplabsTotalNumber = data.total
+          }
+        }, function (response) {
+          this.handleErrors('getSleeplabs', response)
+        })
     },
-    getSleeplabs () {
+    getCurrentDirection (sort) {
+      return this.routeParameters.sortDirection.toLowerCase() === 'asc' ? 'desc' : 'asc'
+    },
+    getSleeplabs (pageNumber, rowsPerPage, sort, sortDir, letter) {
       var data = {
-        page: this.routeParameters.currentPageNumber,
-        rows_per_page: this.sleeplabsPerPage,
-        sort: this.routeParameters.sortColumn,
-        sort_dir: this.routeParameters.sortDirection,
-        letter: this.routeParameters.currentLetter
+        page: pageNumber,
+        rows_per_page: rowsPerPage,
+        sort: sort,
+        sort_dir: sortDir,
+        letter: letter
       }
 
       return this.$http.post(process.env.API_PATH + 'sleeplabs/list', data)

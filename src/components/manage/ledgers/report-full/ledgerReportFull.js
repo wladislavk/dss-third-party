5e698f0126fab156016755ec3983b576bd207106
Sergey <gerra.lampdev@gmail.com>
2017-04-07 12:54:39 +0300

    DSS-548 * Add `LedgerReportFull` component * Update a link to `LedgerReportFull` component on the dashboard

---
+++ src/components/manage/ledgers/report-full/ledgerReportFull.js
@@ -0,0 +1,99 @@
+var handlerMixin = require('../../../../modules/handler/HandlerMixin.js')
+
+export default {
+  name: 'ledger-report-full',
+  data () {
+    return {
+      patientId: 0,
+      routeParameters: {
+        currentPageNumber: 0,
+        sortColumn: 'service_date',
+        sortDirection: 'desc'
+      },
+      name: '',
+      message: '',
+      ledgerRowsTotalNumber: 0,
+      ledgerRowsPerPage: 20,
+      ledgerRows: [],
+      tableHeaders: {
+        'service_date': {
+          title: 'Svc Date',
+          with_link: true,
+          width: 10
+        },
+        'entry_date': {
+          title: 'Entry Date',
+          with_link: true,
+          width: 10
+        },
+        'patient': {
+          title: 'Patient',
+          with_link: true,
+          width: 10
+        },
+        'producer': {
+          title: 'Producer',
+          with_link: true,
+          width: 10
+        },
+        'description': {
+          title: 'Description',
+          with_link: true,
+          width: 10
+        },
+        'amount': {
+          title: 'Charges',
+          with_link: true,
+          width: 10
+        },
+        'paid_amount': {
+          title: 'Credits',
+          with_link: true,
+          width: 10
+        },
+        'adjustments': {
+          title: 'Adjustments',
+          width: 10
+        },
+        'status': {
+          title: 'Ins',
+          with_link: true,
+          width: 5
+        }
+      }
+    }
+  },
+  mixins: [handlerMixin],
+  computed: {
+    currentDate () {
+      return new Date()
+    },
+    totalCharges () {
+      return 0
+    },
+    totalCredits () {
+      return 0
+    },
+    totalAdjustments () {
+      return 0
+    }
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+    formatLedger (value) {
+      return accounting.formatMoney(value, '$')
+    },
+    getCurrentDirection (sort) {
+      if (this.routeParameters.sortColumn == sort) {
+        return this.routeParameters.sortDirection.toLowerCase() === 'asc' ? 'desc' : 'asc'
+      } else {
+        return 'asc'
+      }
+    }
+  }
+}

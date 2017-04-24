f58769874ba8c6a49793b4ef595af6b08f550ff7
Sergey <gerra.lampdev@gmail.com>
2017-04-24 08:51:31 +0300

    DSS-552 * Add `Manage Ledger` component

---
+++ vue-webpack/src/components/manage/ledgers/ledger.js
@@ -0,0 +1,22 @@
+var handlerMixin = require('../../../modules/handler/HandlerMixin.js')
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+  mixins: [handlerMixin],
+  watch: {
+
+  },
+  computed: {
+
+  },
+  created () {
+
+  },
+  methods: {
+    
+  }
+}

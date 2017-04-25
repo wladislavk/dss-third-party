1827d83b5583bae65d1836c0f76f16434a18b4b7
Sergey <gerra.lampdev@gmail.com>
2017-04-25 17:43:01 +0300

    DSS-552 * Update `Manage Ledger` component

--- vue-webpack/src/components/manage/ledgers/ledger.js
+++ vue-webpack/src/components/manage/ledgers/ledger.js
@@ -9,8 +9,8 @@ export default {
         openclaims: 0,
         inspay: 0,
         currentPageNumber: 0,
-        sortColumn: '',
-        sortDirection: 'asc'
+        sortColumn: 'service_date',
+        sortDirection: 'desc'
       },
       patient: {},
       ledgerRowsTotalNumber: 0,
@@ -75,6 +75,36 @@ export default {
   watch: {
     'routeParameters.inspay': function () {
       this.message = this.routeParameters.inspay === 1 ? 'Please select claim below to apply insurance payment.' : ''
+    },
+    'routeParameters.patientId': function () {
+      if (this.routeParameters.patientId > 0) {
+        this.updatePatientSummary(this.routeParameters.patientId)
+      }
+    },
+    '$route.query.page': function () {
+      if (this.$route.query.page != undefined && this.$route.query.page <= this.totalPages) {
+        this.$set(this.routeParameters, 'currentPageNumber', +this.$route.query.page)
+      }
+    },
+    '$route.query.sort': function () {
+      if (this.$route.query.sort in this.tableHeaders) {
+        this.$set(this.routeParameters, 'sortColumn', this.$route.query.sort)
+      } else {
+        this.$set(this.routeParameters, 'sortColumn', 'service_date')
+      }
+    },
+    '$route.query.sortdir': function () {
+      if (this.$route.query.sortdir && this.$route.query.sortdir.toLowerCase() == 'desc') {
+        this.$set(this.routeParameters, 'sortDirection', this.$route.query.sortdir.toLowerCase())
+      } else {
+        this.$set(this.routeParameters, 'sortDirection', 'asc')
+      }
+    },
+    'routeParameters': {
+      handler: function () {
+        this.getLedgerData()
+      },
+      deep: true
     }
   },
   computed: {
@@ -89,12 +119,42 @@ export default {
 
   },
   methods: {
+    getLedgerData () {
+      this.getLedgerRows(
+        this.routeParameters.currentPageNumber,
+        this.ledgerRowsPerPage,
+        this.routeParameters.sortColumn,
+        this.routeParameters.sortDirection
+      ).then(function (response) {
+        var data = response.data.data
+
+        this.ledgerRowsTotalNumber = data.total
+        this.ledgerRows = data.result
+      }, function (response) {
+        this.handleErrors('getLedgerRows', response)
+      })
+    },
     getCurrentDirection (sort) {
       if (this.routeParameters.sortColumn == sort) {
         return this.routeParameters.sortDirection.toLowerCase() === 'asc' ? 'desc' : 'asc'
       } else {
         return 'asc'
       }
+    },
+    updatePatientSummary (patientId) {
+      var data = { patient_id: patientId }
+
+      return this.$http.post(process.env.API_PATH + 'ledgers/update-patient-summary', data)
+    },
+    getLedgerRows (pageNumber, rowsPerPage, sortColumn, sortDir) {
+      var data = {
+        page: pageNumber,
+        rows_per_page: rowsPerPage,
+        sort: sortColumn,
+        sort_dir: sortDir
+      }
+
+      return this.$http.post(process.env.API_PATH + 'ledgers/list', data)
     }
   }
 }

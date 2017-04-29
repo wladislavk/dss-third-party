fae82cf82012581f22bcced59edcc66c03924290
Ast Derek <ast.derek@gmail.com>
2017-04-28 19:47:58 -0500

    DSS-549 Refactor import contacts logic

--- manage/upload_contacts.php
+++ manage/upload_contacts.php
@@ -1,13 +1,15 @@
-<?php namespace Ds3\Libraries\Legacy; ?><?php
-include "includes/top.htm";
-require_once('includes/constants.inc');
-require_once('includes/formatters.php');
-require_once('includes/checkemail.php');
-require_once('includes/general_functions.php');
-?>
-<div style="width:90%; margin-left:5%;">
 <?php
+namespace Ds3\Libraries\Legacy;
 
+require_once __DIR__ . '/includes/top.htm';
+require_once __DIR__ . '/includes/constants.inc';
+require_once __DIR__ . '/includes/formatters.php';
+require_once __DIR__ . '/includes/checkemail.php';
+require_once __DIR__ . '/includes/general_functions.php';
+
+/**
+ * @return array
+ */
 function getContactTypeList () {
     $db = new Db();
     $list = array();
@@ -19,216 +21,225 @@ function getContactTypeList () {
 
     return $list;
 }
+/**
+ * Encapsulate logic to read csv and insert contacts.
+ * Each insertion is a batch, to reduce DB load.
+ *
+ * @param string $filename
+ * @param int    $docId
+ * @param bool   $ignoreExtension
+ * @return array
+ */
+function insertContactList ($filename, $docId, $ignoreExtension=false) {
+    $return = [
+        'total' => 0,
+        'inserted' => 0,
+        'excluded' => 0,
+        'errors' => [],
+    ];
 
-if(isset($_POST['submitbut'])){
-    $csv = array();
-    // check there are no errors
-    if($_FILES['csv']['error'] == 0){
-        $name = $_FILES['csv']['name'];
-        $ext = strtolower(preg_replace('/^.*[.]([^.]+)$/', '$1', ($_FILES['csv']['name'])));
-        $type = $_FILES['csv']['type'];
-        $tmpName = $_FILES['csv']['tmp_name'];
-
-        // check the file is a csv
-        if($ext === 'csv'){
-            if(($handle = fopen($tmpName, 'r')) !== FALSE) {
-                // necessary if a large csv file
-                set_time_limit(0);
-
-                $row = 0;
-
-                $error_count = 0;
-                $error_array = array();
-
-                $contactTypeList = getContactTypeList();
-
-                while(($data = fgetcsv($handle, 1000, ',')) !== FALSE) {
-                    // number of fields in the csv
-                    $num = count($data);
-                    // get the values from the csv
-            		if($row == 0){
-                        for($i=0;$i<27;$i++){
-                            switch(strtolower(trim($data[$i]))){
-                                case 'lastname':
-                                	$fields[] = 'lastname';	
-                                	break;
-                                case 'firstname':
-                                    $fields[] = 'firstname';  
-                                    break;
-                                case 'middleinit':
-                                    $fields[] = 'middlename';  
-                                    break;
-                                case 'title':
-                                    $fields[] = 'salutation';  
-                                    break;
-                                case 'companyname':
-                                    $fields[] = 'company';       
-                                    break;
-                                case 'address1':
-                                    $fields[] = 'add1';       
-                                    break;
-                                case 'address2':
-                                    $fields[] = 'add2';       
-                                    break;
-                                case 'city':
-                                    $fields[] = 'city';       
-                                    break;
-                                case 'state':
-                                    $fields[] = 'state';       
-                                    break;
-                                case 'postalcode':
-                                    $fields[] = 'zip';       
-                                    break;
-                                case 'active':
-                                	if(!in_array('status', $fields)){
-                                        $fields[] = 'status';       
-                                	}else{
-                                        $fields[] = '';
-                                	}
-                                    break;
-                                case 'phone':
-                                    $fields[] = 'phone1';       
-                                    break;
-                                case 'phone2':
-                                    $fields[] = 'phone2';       
-                                    break;
-                                case 'fax':
-                                    $fields[] = 'fax';       
-                                    break;
-                                case 'email':
-                                    $fields[] = 'email';       
-                                    break;
-                                case 'npi':
-                                    $fields[] = 'national_provider_id';       
-                                    break;
-                                case 'otherid':
-                                    $fields[] = 'qualifierid';       
-                                    break;
-                                case 'otheridqual':
-                                    $fields[] = 'qualifier';       
-                                    break;
-                                case 'notes':
-                                    $fields[] = 'notes';       
-                                    break;
-                                case 'contacttype':
-                                    $fields[] = 'contacttypeid';       
-                                    break;
-                                case 'specialty':
-                                    $fields[] = 'specialty';       
-                                    break;
-                                default:
-                                	$fields[] = '';
-                                	break;
-                            }
-                        }
-             			$row++;
-            		}else{
-            			$notes = '';
-            			$s = "INSERT INTO dental_contact SET ";
-                		foreach($fields AS $id => $field){
-                            switch($field){
-                                case 'qualifierid':
-                                	if(trim($data[$id])!=''){
-                                	  $notes .= "OtherID - ".$data[$id]." ";
-                                	}
-                                	break;
-                                case 'qualifier':
-                                    if(trim($data[$id])!=''){
-                                      $notes .= "OtherIDQualifier - ".$data[$id]." ";
-                                    }
-                                    break;
-                                case 'specialty':
-                                    if(trim($data[$id])!=''){
-                                      $notes .= "Specialty - ".$data[$id]." ";
-                                    }
-                                    break;
-                                case 'notes':
-                                      $notes .= $data[$id]." ";
-                                    break;
-                                case 'status':
-                                	$s .= "status = '" . (strtolower(trim($data[$id])) == 'true' ? 3 : 4) . "', ";
-                                	break;
-                                case 'phone1':
-                                case 'phone2':
-                                case 'fax':
-                                    $s .= $field . " = '" .num($data[$id])."', ";
-                                    if($field=='fax'){
-                                        $s .= " preferredcontact = 'fax', ";
-                                    }
-                                	break;
-                                case 'contacttypeid':
-                                	switch(strtolower(trim($data[$id]))){
-                                		case 'insurance':
-                                			$c = "Insurance";
-                                			break;
-                                        case 'ent':
-                                            $c = "ENT Physician";
-                                            break;
-                                        case 'dental physician':
-                                		case 'orthodontist':
-                                            $c = "Dentist";
-                                            break;
-                                        case 'primary care physician':
-                                            $c = "Primary Care Physician";
-                                            break;
-                                        case 'sleep disorder specialist':
-                                            $c = "Sleep Physician";
-                                            break;
-                                		case 'pulmonologist':
-                                			$c = "Pulmonologist";
-                                			break;
-                                		default:
-                                			$c = "Other Physician";
-                                			break;
-                                	}
-                                	if(isset($contactTypeList[$c])){
-                                        $s .= $field . " = '" .intval($contactTypeList[$c])."', ";
-                                	}
-                                	break;
-                                default:
-                                	if($field!=''){
-                                			$s .= $field . " = '" .mysqli_real_escape_string($con, $data[$id])."', ";
-                                	}
-                                	break;
-                            }
-                		}
-                		$s .= " notes='".mysqli_real_escape_string($con,$notes)."', ";
-                		$s .= " docid='".mysqli_real_escape_string($con,$_SESSION['docid'])."'";
-                		//echo $s."<br />";
-                		$cid = $db->getInsertId($s);
-                   		$row++;
-            		}		
-                }
-                fclose($handle);
-        		$msg = "<h4>Inserted ".($row-1)." rows.</h4>";
-        		if($error_count){
-                    $msg .= "<p>".$error_count." errors. Imported emails already assigned to existing patients. The following email addresses will not be imported:</p><ul>";
-                    foreach($error_array as $e){
-                        $msg .= "<li>".$e."</li>";
-                    }
-                    $msg .= "</ul>";
-        		}
-        		?>
-        		<script type="text/javascript">
-        			window.location = "pending_contacts.php?msg=<?php echo $msg; ?>";
-        		</script>
-        		<?php
-            }
-        }else{ ?>
-            <h3>Unrecognized file type. Please upload a CSV file only.</h3>
-        <?php }
+    /**
+     * Allow to ignore extension, in case the filename is a temporary file
+     */
+    if (!$ignoreExtension && strtolower(substr($filename, -4)) !== '.csv') {
+        $return['errors'][] = 'The file extension is not CSV.';
+        return $return;
     }
+
+    $handle = fopen($filename, 'r');
+
+    if ($handle === false) {
+        $return['errors'][] = 'The file could not be read.';
+        return $return;
+    }
+
+    set_time_limit(0);
+
+    $db = new Db();
+    $docId = intval($docId);
+
+    $count = 0;
+    $batchSize = 50;
+    $fields = [];
+
+    do {
+        $batch = [];
+
+        do {
+            $row = fgetcsv($handle, 10000, ',');
+
+            if (!$row) {
+                break;
+            }
+
+            $count++;
+
+            if ($count === 1) {
+                $allowedFields = [
+                    'lastname',
+                    'firstname',
+                    'middlename' => 'middleinit',
+                    'salutation' => 'title',
+                    'company' => 'companyname',
+                    'add1' => 'address1',
+                    'add2' => 'address2',
+                    'city',
+                    'state',
+                    'zip' => 'postalcode',
+                    'status',
+                    'phone1' => 'phone',
+                    'phone2',
+                    'fax',
+                    'email',
+                    'national_provider_id' => 'npi',
+                    'qualifierid' => 'otherid',
+                    'qualifier' => 'otheridqual',
+                    'notes',
+                    'contacttypeid' => 'contacttype',
+                    'specialty',
+                ];
+
+                foreach ($row as $index=>$column) {
+                    $column = strtolower(trim($column));
+
+                    /**
+                     * If already stored, or if not a string, skip
+                     */
+                    if (in_array($column, $fields) || is_numeric($column) || !is_string($column)) {
+                        continue;
+                    }
+
+                    /**
+                     * Save the position of the column, to retrieve the proper data field
+                     */
+                    if (array_key_exists($column, $allowedFields)) {
+                        $fields[$index] = $column;
+                        continue;
+                    }
+
+                    if (in_array($column, $allowedFields)) {
+                        $found = array_search($column, $allowedFields);
+                        $fields[$index] = is_numeric($found) ? $column : $found;
+                        continue;
+                    }
+                }
+
+                /**
+                 * No fields = no valid csv
+                 */
+                if (!count($fields)) {
+                    $return['errors'][] = count($row) ? 'The header is invalid.' : 'The CSV file contains no header.';
+                    return $return;
+                }
+
+                foreach (['notes', 'docid', 'ip_address'] as $each) {
+                    if (!in_array($each, $fields)) {
+                        $fields[] = $each;
+                    }
+                }
+
+                continue;
+            }
+
+            $data = [];
+
+            foreach ($fields as $index=>$column) {
+                switch ($column) {
+                    case 'notes':
+                        $data[$column] = '';
+                        break;
+                    case 'docid':
+                        $data[$column] = $docId;
+                        break;
+                    case 'ip_address':
+                        $data[$column] = $_SERVER['REMOTE_ADDR'];
+                        break;
+                    default:
+                        $data[$column] = trim($row[$index]);
+                }
+            }
+
+            /**
+             * Exclude rows that have no names
+             */
+            if (empty($data['firstname']) && empty($data['lastname'])) {
+                $return['excluded']++;
+                continue;
+            }
+
+            $batch[] = $data;
+
+            if (count($batch) >= $batchSize) {
+                break;
+            }
+        } while ($row);
+
+        if (!$batch) {
+            break;
+        }
+
+        array_walk($batch, function (&$each) use ($db) {
+            $each = $db->escapeList($each);
+        });
+
+        $insert = [
+            'columns' => '(' . join(', ', $fields) . ', adddate)',
+            'values' => '(' . join($batch, ", NOW()), (") . ', NOW())',
+        ];
+
+        $return['inserted'] += $db->getAffectedRows("INSERT INTO dental_contact
+            {$insert['columns']} VALUES {$insert['values']}");
+    } while ($row);
+
+    fclose($handle);
+    $return['total'] = $count;
+
+    return $return;
 }
-?> 
 
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data" method="post">
+if (isset($_POST['submitbut'])) {
+    // check there are no errors
+    if ($_FILES['csv']['error'] == 0) {
+        if (strtolower(substr($_FILES['csv']['name'], -4)) === '.csv') {
+            $details = insertContactList($_FILES['csv']['tmp_name'], $_SESSION['docid'], true);
+            $errors = $details['errors'];
+        } else {
+            $errors = ['The file extension is not CSV.'];
+        }
+    } else {
+        $errors = ['There was a problem uploading the file.'];
+    }
 
-<input type="file" name="csv" />
-<br />
-<br />
-<input type="submit" name="submitbut" value="Upload" />
-</form>
+    $message = '';
+
+    if (!empty($details)) {
+        $message = ($details['inserted'] ?: 'No') . ' contacts saved.' .
+            ($details['excluded'] ? " {$details['excluded']} rows (excluded) missing both first name and last name." : '');
+
+        if (!$details['total']) {
+            $errors[] = 'The file was empty.';
+        }
+    }
+
+    if ($errors) {
+        $message .= ' Errors: ' . join(' ', $errors);
+    }
+
+    ?>
+    <script type="text/javascript">
+        window.location = "pending_contacts.php?msg=<?= urlencode($message) ?>";
+    </script>
+    <?php
+}
+?>
+<div style="width:90%; margin-left:5%;">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data" method="post">
+        <input type="file" name="csv" />
+        <br /><br />
+        <input type="submit" name="submitbut" value="Upload" />
+    </form>
 </div>
 <br /><br />
-<?php include "includes/bottom.htm";?>
+<?php
 
+require_once __DIR__ . '/includes/bottom.htm';

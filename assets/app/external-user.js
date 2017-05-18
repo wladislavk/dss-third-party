7b96c8aab346987ecaf20690be7a259682ea87ac
Ast Derek <ast.derek@gmail.com>
2017-05-18 04:05:05 -0500

    DSS-519 Save basic API options in user profile

---
+++ assets/app/external-user.js
@@ -0,0 +1,84 @@
+Vue.http.headers.common['Authorization'] = 'Bearer ' + document.getElementById('dom-api-token').value;
+
+var userId = $('[name=ed]').val();
+var apiPath = apiRoot + 'api/v1/external-user/' + userId;
+
+var user = new Vue({
+    el: '#add-user-form',
+    data: {
+        fields: {
+            id: 0,
+            user_id: userId,
+            api_key: '',
+            valid_from: '',
+            valid_to: ''
+        },
+        enabled: null
+    },
+    methods: {
+        saveUser: function (e) {
+            e.preventDefault();
+
+            if (typeof userabc_warn !== 'undefined' && !userabc_warn(this.$el)) {
+                return;
+            }
+
+            var method = this.fields.id ? 'put' : 'post';
+
+            this.$http[method](apiPath, this.fields, function() {
+                this.$el.submit();
+            }).error(function () {
+                this.$el.submit();
+            });
+        },
+        deleteUser: function (e) {
+            e.preventDefault();
+
+            if (!confirm('Do Your Really want to Delete?.')) {
+                return;
+            }
+
+            this.$http.delete(apiPath, function () {
+                this.enabled = false;
+
+                if (e.target && e.target.href) {
+                    window.location = e.target.href;
+                }
+            }).error(function () {
+                if (e.target && e.target.href) {
+                    window.location = e.target.href;
+                }
+            });
+        },
+        generateApiKey: function (fields) {
+            function guid () {
+                function s4 () {
+                    return Math.floor((1 + Math.random()) * 0x10000)
+                        .toString(16)
+                        .substring(1);
+                }
+
+                return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+                    s4() + '-' + s4() + s4() + s4();
+            }
+            fields.api_key = guid();
+        },
+        onReady: function() {
+            this.enabled = this.id && this.fields.user_id;
+
+            this.$http.get(apiPath, function (data) {
+                this.$set('fields', data.data);
+                this.enabled = this.id && this.fields.user_id;
+            });
+        }
+    },
+    ready: function() {
+        this.onReady();
+    }
+});
+
+$(function(){
+    $('.input-group.date').datepicker({
+        format:'yyyy-mm-dd'
+    });
+});

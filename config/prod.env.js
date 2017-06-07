21168ccfa48ef6b3c6122e678ee23fff3c5c3606
Sergey <gerra.lampdev@gmail.com>
2017-06-07 11:43:25 +0300

    DSS-569 * Fix `API_ROOT` in the config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API_ROOT: '"https://api.docker.localhost"',
+  API_ROOT: '"https://api.docker.localhost/"',
   API_PATH: '"https://api.docker.localhost/api/v1/"'
 }

b9adb1df617fd48da560d2f56bb01f27cf64e62e
vladislavk <wladislav.k@gmail.com>
2017-06-14 12:25:10 -0500

    Added bash script for hook

---
+++ commit-msg.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# 1. Create an empty commit-msg git hook file, and make it executable. From your project's root directory:
+#    install -b -m 755 /dev/null .git/hooks/commit-msg
+# 2. Save the following script to the newly-created .git/hooks/commit-msg file.
+
+COMMIT_MSG=$(cat $1)
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+JIRA_ID=$(echo "$CURRENT_BRANCH" | grep -Eo "[A-Z0-9]{1,10}-?[A-Z0-9]+-\d+")
+
+if [ ! -z "$JIRA_ID" ]; then
+    echo "$JIRA_ID $COMMIT_MSG" > $1
+    echo "JIRA ID '$JIRA_ID', matched in current branch name, prepended to commit message. (Use --no-verify to skip)"
+fi

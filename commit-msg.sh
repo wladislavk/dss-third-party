7217c04978ea350460154c74ebfe151e529a9b27
vladislavk <wladislav.k@gmail.com>
2017-06-14 13:28:29 -0500

    DSS-580 * Test commit

--- commit-msg.sh
+++ commit-msg.sh
@@ -6,9 +6,11 @@
 
 COMMIT_MSG=$(cat $1)
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-JIRA_ID=$(echo "$CURRENT_BRANCH" | grep -Eo "DSS\-[0-9]+?")
+JIRA_ID=$(echo "$CURRENT_BRANCH" | grep -Eo "^DSS\-[0-9]+?")
+COMMIT_MSG_MATCH=$(echo "$COMMIT_MSG" | grep -Eo "^$JIRA_ID")
 
-if [ ! -z "$JIRA_ID" ]; then
+# if branch name starts with the regexp and commit message does not start with the regexp
+if [ ! -z "$JIRA_ID" ] && [ -z "$COMMIT_MSG_MATCH" ]; then
     echo "$JIRA_ID * $COMMIT_MSG" > $1
     echo "JIRA ID '$JIRA_ID', matched in current branch name, prepended to commit message. (Use --no-verify to skip)"
 fi

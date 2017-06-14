165a0d96b03b11a68bcd6d0daa61ed95109706e9
vladislavk <wladislav.k@gmail.com>
2017-06-14 12:29:24 -0500

    Test commit

--- commit-msg.sh
+++ commit-msg.sh
@@ -12,3 +12,4 @@ if [ ! -z "$JIRA_ID" ]; then
     echo "$JIRA_ID $COMMIT_MSG" > $1
     echo "JIRA ID '$JIRA_ID', matched in current branch name, prepended to commit message. (Use --no-verify to skip)"
 fi
+

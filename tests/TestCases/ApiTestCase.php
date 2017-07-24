630bf171ecfccfdb3578af410679f7bf28757ceb
Vladislav Kryshtanovskiy <wladislav.k@gmail.com>
2017-07-24 14:14:47 +0700

    DSS-603 * Temporary commit

--- tests/TestCases/ApiTestCase.php
+++ tests/TestCases/ApiTestCase.php
@@ -1,10 +1,68 @@
 <?php
 namespace Tests\TestCases;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 
-class ApiTestCase extends BaseApiTestCase
+abstract class ApiTestCase extends BaseApiTestCase
 {
     use WithoutMiddleware, DatabaseTransactions;
+
+    const ROUTE_PREFIX = '/api/v1';
+
+    protected $table;
+
+    /** @var int */
+    protected $userId = 100;
+
+    /**
+     * @return string
+     */
+    abstract protected function getModel();
+
+    /**
+     * @return string
+     */
+    abstract protected function getRoute();
+
+    public function setUp()
+    {
+        parent::setUp();
+        $modelClass = $this->getModel();
+        /** @var Model $model */
+        $model = new $modelClass();
+        $this->table = $model->getTable();
+    }
+
+    public function testStore()
+    {
+        $data = factory($this->getModel())->make()->toArray();
+
+        $data['userid'] = $this->userId;
+
+        $this->post(self::ROUTE_PREFIX . $this->getRoute(), $data);
+        $this->assertResponseOk();
+        $this->seeInDatabase($this->table, ['userid' => $this->userId]);
+    }
+
+    public function testUpdate()
+    {
+        $testRecord = factory($this->getModel())->create();
+
+        $data['adminid'] = $this->userId;
+
+        $this->put(self::ROUTE_PREFIX . $this->getRoute() . '/' . $testRecord->id, $data);
+        $this->assertResponseOk();
+        $this->seeInDatabase($this->table, ['adminid' => $this->userId]);
+    }
+
+    public function testDestroy()
+    {
+        $testRecord = factory($this->getModel())->create();
+
+        $this->delete(self::ROUTE_PREFIX . $this->getRoute() . '/' . $testRecord->id);
+        $this->assertResponseOk();
+        $this->notSeeInDatabase($this->table, ['id' => $testRecord->id]);
+    }
 }

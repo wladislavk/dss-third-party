2bad0d7311738ce1ef4eacd8e4c443aaa811616b
vladislavk <wladislav.k@gmail.com>
2017-10-18 17:14:30 +0700

    DSS-625 * Backport from dss-642 to stop API tests from failing

--- tests/Api/EnrollmentApiTest.php
+++ tests/Api/EnrollmentApiTest.php
@@ -152,9 +152,9 @@ class EnrollmentApiTest extends BaseApiTestCase
             'names' => [
                 'Medicare Part B of Maryland',
                 'Medicare of Maryland',
+                'Medicare of Maryland Jurisdiction A',
             ],
             'created_at' => '2014-07-20T07:17:21Z',
-            'updated_at' => '2017-01-03T21:32:25Z',
             'supported_endpoints' => [
                 [
                     'endpoint' => 'coverage',
@@ -240,9 +240,9 @@ class EnrollmentApiTest extends BaseApiTestCase
             'names' => [
                 'Medicare Part B of Maryland',
                 'Medicare of Maryland',
+                'Medicare of Maryland Jurisdiction A',
             ],
             'created_at' => '2014-07-20T07:17:21Z',
-            'updated_at' => '2017-01-03T21:32:25Z',
             'supported_endpoints' => [
                 [
                     'endpoint' => 'coverage',

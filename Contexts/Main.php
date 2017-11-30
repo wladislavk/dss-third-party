03da9334a385265e2b69ac4619e813423ba97e39
Sergey <gerra.lampdev@gmail.com>
2017-11-30 15:56:24 +0200

    DSS-660 * Add a todo comment to the main context

--- Contexts/Main.php
+++ Contexts/Main.php
@@ -381,6 +381,7 @@ class Main extends BaseContext
     public function testSeeModal()
     {
         if (SUT_HOST === 'vue') {
+            // TODO: add logic for checking if div#modal-content has a child
             return;
         }
 

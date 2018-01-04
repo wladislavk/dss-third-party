a99065a9b56df0a854c2ca3022cdc1ea443bef3d
dima_morozov <dmytro.morozov.ld@gmail.com>
2018-01-04 15:33:00 +0200

    DSS-703 * Returned back HEADLESS urls

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,8 @@ const merge = require('webpack-merge')
 const processEnv = process.env
 
 module.exports = merge(processEnv, {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  HEADLESS_API_ROOT: '"http://api/"',
+  HEADLESS_API_PATH: '"http://api/api/v1/"',
+  HEADLESS_LEGACY_ROOT: '"http://loader/"'
 })

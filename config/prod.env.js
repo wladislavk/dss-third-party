5db1c6964f89cc8871c6da15ff01d44c17e69303
dima_morozov <dmytro.morozov.ld@gmail.com>
2018-01-04 13:41:20 +0200

    DSS-703 * Got enviroment variables from the 4th container

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,10 +1,6 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  API_ROOT: '"https://api.docker.localhost/"',
-  API_PATH: '"https://api.docker.localhost/api/v1/"',
-  LEGACY_ROOT: '"https://loader.docker.localhost/"',
-  HEADLESS_API_ROOT: '"http://api/"',
-  HEADLESS_API_PATH: '"http://api/api/v1/"',
-  HEADLESS_LEGACY_ROOT: '"http://loader/"',
-  IMAGE_PATH: '"https://loader.docker.localhost/shared/"'
-}
+const merge = require('webpack-merge')
+const processEnv = process.env
+
+module.exports = merge(processEnv, {
+  NODE_ENV: '"production"'
+})

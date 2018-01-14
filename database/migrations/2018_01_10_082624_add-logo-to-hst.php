ebb1bbd2d7a0be4c05e7bf68213405f5207ce5ae
Vladislav Kryshtanovskiy <wladislav.k@gmail.com>
2018-01-14 18:31:20 +0200

    DSS-718 * Disallowed migration in production

--- database/migrations/2018_01_10_082624_add-logo-to-hst.php
+++ database/migrations/2018_01_10_082624_add-logo-to-hst.php
@@ -12,6 +12,9 @@ class AddLogoToHst extends Migration
      */
     public function up()
     {
+        if (env('APP_ENV') == 'production') {
+            return;
+        }
         DB::update("UPDATE companies SET logo = 'company_logo_5.jpg' WHERE id = 5");
     }
 
@@ -22,6 +25,9 @@ class AddLogoToHst extends Migration
      */
     public function down()
     {
+        if (env('APP_ENV') == 'production') {
+            return;
+        }
         DB::update("UPDATE companies SET logo = '' WHERE id = 5");
     }
 }

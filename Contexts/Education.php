3f25cfaff3654c709a7904eb4ef6d331ece3cddf
Michał Krakiewicz <michal.krakiewicz@polcode.net>
2018-02-08 15:44:03 +0100

    Added tests for legacy Education

--- Contexts/Education.php
+++ Contexts/Education.php
@@ -6,6 +6,7 @@ use PHPUnit\Framework\Assert;
 
 class Education extends BaseContext
 {
+
     /**
      * @Then I see dental sleep procedures manual
      */
@@ -15,5 +16,49 @@ class Education extends BaseContext
         $heading = $this->findCss('h2');
         Assert::assertNotNull($heading);
         Assert::assertEquals('DENTAL SLEEP PROCEDURES MANUAL', $this->sanitizeText($heading->getText()));
+    }
+
+    /**
+     * @Then I see dental sleep medicine manual
+     */
+    public function testMedicineManual()
+    {
+        $this->wait(self::SHORT_WAIT_TIME);
+        $heading = $this->findCss('.WordSection1 b');
+        Assert::assertNotNull($heading);
+        Assert::assertEquals('DENTAL SLEEP MEDICINE', $this->sanitizeText($heading->getText()));
+    }
+
+    /**
+     * @Then I see quick facts reference
+     */
+    public function testQuickFactsReference()
+    {
+        $this->wait(self::SHORT_WAIT_TIME);
+        $heading = $this->findCss('.MsoNormal');
+        Assert::assertNotNull($heading);
+        Assert::assertEquals('QUICK FACTS & REFERENCE', $this->sanitizeText($heading->getText()));
+    }
+
+    /**
+     * @Then I see videos
+     */
+    public function testVideos()
+    {
+        $this->wait(self::SHORT_WAIT_TIME);
+        $heading = $this->findCss('h2:nth-child(2)');
+        Assert::assertNotNull($heading);
+        Assert::assertEquals('Fundamentals of Dental Sleep Medicine', $this->sanitizeText($heading->getText()));
+    }
+
+    /**
+     * @Then I see certificates
+     */
+    public function testCertificates()
+    {
+        $this->wait(self::SHORT_WAIT_TIME);
+        $heading = $this->findCss('#contentMain');
+        Assert::assertNotNull($heading);
+        Assert::assertEquals('EdX Certificates', $this->sanitizeText($heading->getText()));
     }
 }
--- Features/Legacy/Education.feature
+++ Features/Legacy/Education.feature
@@ -7,3 +7,37 @@ Feature: Education manuals
     Then I see "Dental Sleep Solutions Procedures Manual" link
     When I click "Dental Sleep Solutions Procedures Manual" link
     Then I see dental sleep procedures manual
+
+  Scenario: Check medicine manual
+    Given I am logged in as "doc1f"
+    When I go to "start" page
+    And I run mouse over "Education" menu point
+    Then I see "Dental Sleep Medicine Manual" link
+    When I click "Dental Sleep Medicine Manual" link
+    Then I see dental sleep medicine manual
+
+  Scenario: Check quick facts reference
+    Given I am logged in as "doc1f"
+    When I go to "start" page
+    And I run mouse over "Education" menu point
+    Then I see "Quick Facts Reference" link
+    When I click "Quick Facts Reference" link
+    Then I see quick facts reference
+
+  Scenario: Check watch videos
+    Given I am logged in as "doc1f"
+    When I go to "start" page
+    And I run mouse over "Education" menu point
+    Then I see "Watch Videos" link
+    When I click "Watch Videos" link
+    Then I see videos
+
+  Scenario: Check certificates
+    Given I am logged in as "doc1f"
+    When I go to "start" page
+    And I run mouse over "Education" menu point
+    Then I see "Certificates" link
+    When I click "Certificates" link
+    Then I see certificates
+
+

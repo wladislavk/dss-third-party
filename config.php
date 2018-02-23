38f7828a05240909ca75831e70bf16073729855d
vladislavk <wladislav.k@gmail.com>
2018-02-23 11:40:53 +0700

    Deleted unneeded files

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-//define('DUAL_APP', false);
-define('DUAL_APP', true);
+define('DUAL_APP', false);
+//define('DUAL_APP', true);
 define('VUE_URL', 'https://vue.docker.localhost/manage/');
 define('HEADLESS_VUE_URL', 'http://vue/manage/');
 define('API_URL', 'http://api/api/v1/');

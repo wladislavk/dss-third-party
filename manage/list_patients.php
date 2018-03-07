85c19cde14f8608f0aec85254bae15686b88daf4
vladislavk <wladislav.k@gmail.com>
2018-03-07 21:02:49 +0700

    Temporary commit

--- manage/list_patients.php
+++ manage/list_patients.php
@@ -8,7 +8,7 @@ include_once 'includes/formatters.php';
 $partial = '';
 if (isset($_POST['partial_name'])) {
     $partial = $_POST['partial_name'];
-    $partial = preg_replace("[^ A-Za-z'\-]", "", $partial);
+    $partial = preg_replace("/[^ A-Za-z'\-]/", "", $partial);
     $partial = s_for($partial);
 }
 

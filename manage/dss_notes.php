a45c2494a7621b2869f3dd0326d2896fdef13151
vladislavk <wladislav.k@gmail.com>
2018-03-08 17:55:59 +0700

    Fixed notes

--- manage/dss_notes.php
+++ manage/dss_notes.php
@@ -1,4 +1,4 @@
-<?php namespace Ds3\Libraries\Legacy; ?><?php 
+<?php namespace Ds3\Libraries\Legacy; ?><?php
 if (isset($_POST['newnotesubmit'])) {
     $query = "UPDATE dental_patients SET patient_notes='".mysqli_real_escape_string($con, $_POST['notecontent'])."' WHERE patientid='".(!empty($_GET['pid']) ? $_GET['pid'] : '')."';";
     
@@ -12,9 +12,11 @@ if (isset($_POST['newnotesubmit'])) {
     $query = "SELECT patient_notes FROM dental_patients WHERE patientid='".(!empty($_GET['pid']) ? $_GET['pid'] : '')."';";
     $array = $db->getResults($query);
 
-    foreach ($array as $notes) { ?>
-        <textarea name="notecontent" style="width:98%;" class="form-control" rows="3"><?= $notes['patient_notes'] ?></textarea>
-        <?php
+    if ($array) {
+        foreach ($array as $notes) { ?>
+            <textarea name="notecontent" style="width:98%;" class="form-control" rows="3"><?= $notes['patient_notes'] ?></textarea>
+            <?php
+        }
     } ?>
     <br />
     <input type="submit" name="newnotesubmit" class="btn btn-info" value="Update Notes">

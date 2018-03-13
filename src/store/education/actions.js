a7eefaf87a2e00c0ece8fd2421a207a99874151a
Michał Krakiewicz <michal.krakiewicz@polcode.net>
2018-03-13 20:07:25 +0100

    DSS-662 * Post review changes

--- src/store/education/actions.js
+++ src/store/education/actions.js
@@ -6,16 +6,22 @@ export default {
   [symbols.actions.getEdxCertificatesData] ({commit, dispatch}) {
     http.get(endpoints.education.edxCertificates).then((response) => {
       const data = response.data.data
-      const edxData = {
-        id: data.id,
-        url: data.url,
-        courseName: data.course_name,
-        courseSection: data.course_section,
-        courseSubsection: data.course_subsection,
-        numberCe: data.number_ce,
-        addDate: data.adddate,
-        ipAddress: data.ip_address
-      }
+      const edxData = []
+      console.log('data', data)
+      response.data.data.forEach((data) => {
+        const certificate = {
+          id: data.id,
+          url: data.url,
+          courseName: data.course_name,
+          courseSection: data.course_section,
+          courseSubsection: data.course_subsection,
+          numberCe: data.number_ce,
+          addDate: data.adddate,
+          ipAddress: data.ip_address
+        }
+        edxData.push(certificate)
+      })
+
       commit(symbols.mutations.edxCertificatesData, edxData)
     }).catch((response) => {
       dispatch(symbols.actions.handleErrors, {title: 'getEdxCertificatesData', response: response})

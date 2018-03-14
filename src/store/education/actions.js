3f26f376a05cba5540d863de9e6e3c81dce8de59
Michal Krakiewicz <michal.krakiewicz@polcode.net>
2018-03-14 15:40:50 +0100

    DSS-662 * Post review changes

--- src/store/education/actions.js
+++ src/store/education/actions.js
@@ -10,14 +10,12 @@ export default {
       console.log('data', data)
       response.data.data.forEach((data) => {
         const certificate = {
-          id: data.id,
+          id: parseInt(data.id),
           url: data.url,
           courseName: data.course_name,
           courseSection: data.course_section,
           courseSubsection: data.course_subsection,
-          numberCe: data.number_ce,
-          addDate: data.adddate,
-          ipAddress: data.ip_address
+          numberCe: parseInt(data.number_ce)
         }
         edxData.push(certificate)
       })

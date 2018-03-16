59f8a022c4bd6f59f3cae5cb8d7493c99ef7b7ef
vladislavk <wladislav.k@gmail.com>
2018-03-16 10:20:51 +0700

    DSS-662 * Moved videos to constant

---
+++ src/constants/education.js
@@ -0,0 +1,56 @@
+export const VIDEOS = [
+  {
+    heading: 'Fundamentals of Dental Sleep Medicine',
+    text: 'When we treat patients suffering from sleep apnea we cross over into the realm of medicine, and we need to learn the language in order to communicate with our physician colleagues. Learn the basics here, from sleep to testing to treatment.',
+    time: 35,
+    video: 'https://player.vimeo.com/video/65686921'
+  },
+  {
+    heading: 'Screening for Sleep Disordered Breathing',
+    text: 'Patients have been walking through our doors for years NOT knowing that they are suffering from a deadly disease. Learn effective ways to screen patients for sleep and breathing disorders, things you can say to get the patient to acknowledge that he may have a problem, and how to lead them down the path toward testing and treatment.',
+    time: 50,
+    video: 'https://player.vimeo.com/video/65784954'
+  },
+  {
+    heading: 'Consultation',
+    text: 'We\'ve been consulting with patients for years about how and why to fix teeth; treating sleep apnea is different and requires that you do some homework. Learn a process to efficiently evaluate patients for candidacy for text: \'Dental device therapy and how to steer patients toward appropriate treatment.\'',
+    time: 58,
+    video: 'https://player.vimeo.com/video/65683866'
+  },
+  {
+    heading: 'Treatment Options for Sleep Disordered Breathing',
+    text: 'What are my options for treating sleep apnea, Doctor? Learn the options, the ins and outs, the ups and downs, the advantages and disadvantages of a wide scope of treatment options.',
+    time: 37,
+    video: 'https://player.vimeo.com/video/65688339'
+  },
+  {
+    heading: 'Comprehensive Exam and Impressions',
+    text: 'Learn about "the other things" that you should look for, evaluate, and document, with regards to upper airway and text: \'Dental device treatment, as well as tips and tricks for getting accurate impressions for your devices.\'',
+    time: 37,
+    video: 'https://player.vimeo.com/video/65686917'
+  },
+  {
+    heading: 'Device Delivery',
+    text: 'Though each text: \'Dental device requires certain things, most devices share many commonalities. Learn proper protocols and procedures to produce consistent results; know what to say BEFORE the patient goes home with your device to help dramatically increase your treatment success.',
+    time: 20,
+    video: 'https://player.vimeo.com/video/65686916'
+  },
+  {
+    heading: 'Managing Side Effects',
+    text: 'Dental device therapy will cause side effects. Get over it! Learn specific tricks on how to deal with specific problems. You should NOT practice dental sleep medicine without knowing this material inside and out.',
+    time: 30,
+    video: 'https://player.vimeo.com/video/65688341'
+  },
+  {
+    heading: 'Follow Up and Home Sleep Testing',
+    text: 'Dental device treatment requires continual adjustment and feedback. Deliver the device. Now what? Learn what to document, how to evaluate effectiveness, and when to proceed to the next step of home sleep testing.',
+    time: 35,
+    video: 'https://player.vimeo.com/video/65686919'
+  },
+  {
+    heading: 'Treatment Complete and Annual Recall',
+    text: 'Learn how to decide when you have titrated your device far enough, when you have reached "maximum medical improvement", what constitutes successful treatment, and what to review at each annual recall appointment.',
+    time: 7,
+    video: 'https://player.vimeo.com/video/65784955'
+  }
+]

ecc443d79d1b67c41a31820bcc952278e333964c
Ast Derek <ast.derek@gmail.com>
2018-04-10 03:53:22 -0500

    DSS-786 Remove assertions from @when

--- acceptance/Contexts/ProfileStripe.php
+++ acceptance/Contexts/ProfileStripe.php
@@ -11,19 +11,26 @@ class ProfileStripe extends BaseContext
      * @When I fill in the following credit card details:
      *
      * @param TableNode $table
+     * @throws BehatException
      */
     public function fillCreditCardDetails(TableNode $table)
     {
         $fields = $table->getHash();
         $container = $this->findCss('div#card_form');
-        Assert::assertNotNull($container);
+        if (is_null($container)) {
+            throw new BehatException('Credit card details container not found');
+        }
 
         foreach ($fields as $field) {
             $label = $container->find('xpath', "//label[normalize-space()='{$field['label']}']");
-            Assert::assertNotNull($label);
+            if (is_null($label)) {
+                throw new BehatException("Label {$field['label']} not found");
+            }
             $parent = $label->getParent();
             $input = $parent->find('xpath', '//input[@type="text"]');
-            Assert::assertNotNull($input);
+            if (is_null($input)) {
+                throw new BehatException("Input for label {$field['label']} not found");
+            }
             // Field masks cause volatility if fields are not clicked first
             $input->click();
             $input->setValue($field['value']);

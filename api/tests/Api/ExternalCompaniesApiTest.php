d4aa72d246968bcabdc9a3dee8a168e5e1126b9b
Vladislav Kryshtanovskiy <wladislav.k@gmail.com>
2018-04-29 19:54:52 +0300

    Fixed external company test

--- api/tests/Api/ExternalCompaniesApiTest.php
+++ api/tests/Api/ExternalCompaniesApiTest.php
@@ -23,4 +23,41 @@ class ExternalCompaniesApiTest extends ModelAwareApiTestCase
             'api_key' => $this->faker->sha1,
         ];
     }
+
+    public function testShow()
+    {
+        $testRecord = $this->modelFactory()[0];
+
+        $primaryKey = $this->getModelKey();
+        $endpoint = $this->getRoutePrefix() . $this->getRoute() . '/' . $testRecord->$primaryKey;
+        $this->get($endpoint);
+        $this->assertResponseOk();
+        $data = $this->getResponseData();
+        $this->assertEquals($testRecord->$primaryKey, $data[$primaryKey]);
+    }
+
+    public function testUpdate()
+    {
+        $testRecord = $this->modelFactory()[0];
+
+        $primaryKey = $this->getModelKey();
+        $endpoint = $this->getRoutePrefix() . $this->getRoute() . '/' . $testRecord->$primaryKey;
+
+        $updateData = $this->getUpdateData();
+        $this->put($endpoint, $updateData);
+        $this->assertResponseOk();
+        $this->seeInDatabase($this->model->getTable(), $updateData);
+    }
+
+    public function testDestroy()
+    {
+        $testRecord = $this->modelFactory()[0];
+
+        $primaryKey = $this->getModelKey();
+        $endpoint = $this->getRoutePrefix() . $this->getRoute() . '/' . $testRecord->$primaryKey;
+
+        $this->delete($endpoint);
+        $this->assertResponseOk();
+        $this->notSeeInDatabase($this->model->getTable(), [$primaryKey => $testRecord->$primaryKey]);
+    }
 }

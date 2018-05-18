13ede4e80f288050cfda94114e5d4128356f54e7
dima_morozov <dmytro.morozov.ld@gmail.com>
2018-05-18 14:16:20 +0300

    DSS-786 * Stripe card and signature component moved to update_stripe_info.php

---
+++ legacy/manage/update_stripe_info.php
@@ -0,0 +1,27 @@
+<?php
+namespace Ds3\Libraries\Legacy;
+set_time_limit(0);
+require_once __DIR__ . '/includes/top.htm';
+?>
+
+    <link rel="stylesheet" href="css/manage_profile.css" type="text/css" />
+
+    <span class="admin_head">
+  Update Stripe Information
+</span>
+    <br />
+    <br />
+    &nbsp;
+    <a href="manage_profile.php"> Back to Manage Profile</a>
+    <br />
+
+
+    <div class="fullwidth">
+        <hr />
+        <?php require_once __DIR__ . '/stripe_card_info.php'; ?>
+    </div>
+
+    <?php
+require_once __DIR__ . '/signature_test.php';
+require_once __DIR__ . '/includes/bottom.htm';
+?>

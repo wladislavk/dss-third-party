7521beb779eaa0df8d548ec24294ebd6342efbcc
Ast Derek <ast.derek@gmail.com>
2018-05-18 23:05:23 -0500

    DSS-98 Remove auth IDs from requests

--- api/app/Http/Requests/TongueClinicalExam.php
+++ api/app/Http/Requests/TongueClinicalExam.php
@@ -6,15 +6,12 @@ class TongueClinicalExam extends Request
 {
     protected $rules = [
         'formid'               => 'required|integer',
-        'patientid'            => 'required|integer',
         'blood_pressure'       => ['regex:/^\d+(\/\d+)?$/'],
         'pulse'                => 'string',
         'neck_measurement'     => 'regex:/^([0-9]*[.])?[0-9]+$/',
         'bmi'                  => 'regex:/^[0-9]+\.[0-9]+$/',
         'additional_paragraph' => 'string',
         'tongue'               => 'regex:/^~([0-9]~)+$/',
-        'userid'               => 'integer',
-        'docid'                => 'integer',
         'status'               => 'integer',
     ];
 }

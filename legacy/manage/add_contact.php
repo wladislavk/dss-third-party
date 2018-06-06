311ad75dcfd220efdefd2612a7c4e04a11270461
vladislavk <wladislav.k@gmail.com>
2018-06-06 19:31:16 +0700

    DSS-837 * Removed unneeded third party

--- legacy/manage/add_contact.php
+++ legacy/manage/add_contact.php
@@ -1,10 +1,8 @@
 <?php namespace Ds3\Libraries\Legacy; ?><?php 
 include_once('admin/includes/main_include.php');
 include("includes/sescheck.php");
-//include "includes/general_functions.php";
 include_once "admin/includes/general.htm";
 include_once "includes/constants.inc";
-//include "includes/top.htm";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -47,7 +45,6 @@ if(!empty($_POST["contactsub"]) && $_POST["contactsub"] == 1){
 			$dct_r = $db->getRow($dct_sql);
 	        if($dct_r['physician']==1){	
 				//DO NOT CREATE LETTER 1 (FROM DSS) FOR USER TYPE SOFTWARE
-				//error_log($_SESSION['user_type'] ." ". DSS_USER_TYPE_SOFTWARE);
 				if($_SESSION['user_type'] != DSS_USER_TYPE_SOFTWARE){
 					create_welcome_letter('1', $rid, $_SESSION['docid']);
 				}	
@@ -109,18 +106,6 @@ if(!empty($_POST["contactsub"]) && $_POST["contactsub"] == 1){
 		trigger_error("Die called", E_USER_ERROR);
 	}
 }
-
-/*
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<link href="css/admin.css?v=20160404" rel="stylesheet" type="text/css" />
-<script language="javascript" type="text/javascript" src="script/validation.js"></script>
-
-<link rel="stylesheet" href="css/form.css" type="text/css" />
-</head>
-<body width="98%"> */ 
 
 $psql = "select * from dental_contacttype WHERE physician=1";
 $pq = $db->getResults($psql);
@@ -544,17 +529,6 @@ if($themyarray["contactid"] != ''){ ?>
 </form>
 
 </div>
-<!--<div style="margin:0 auto;background:url(images/dss_05.png) no-repeat top left;width:980px; height:28px;"> </div>
-  </td>
-</tr>-->
-<!-- Stick Footer Section Here -->
-<!--</table>-->
-<!--<div id="popupContact" style="width:750px;">
-    <a id="popupContactClose"><button>X</button></a>
-    <iframe id="aj_pop" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0"></iframe>
-</div>
-<div id="backgroundPopup"></div>
--->
 
 <script type="text/javascript" src="script/contact.js"></script>
 <script type="text/javascript" src="js/add_contact.js?v=20160328"></script>

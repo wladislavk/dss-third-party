3b57319f26d17c213536e14fa4c31578ea128ab9
vladislavk <wladislav.k@gmail.com>
2018-06-23 12:43:08 +0700

    DSS-860 * Removed more unused code in back office

--- legacy/manage/admin/invoice_monthly.php
+++ legacy/manage/admin/invoice_monthly.php
@@ -5,6 +5,8 @@ require_once __DIR__ . '/includes/stripe-functions.php';
 include_once 'includes/main_include.php';
 include_once '../includes/constants.inc';
 
+$db = new Db();
+
 $no_card = [];
 $sql = "SELECT du.*, 
     c.name AS company_name, 
@@ -45,146 +47,148 @@ while($r = mysqli_fetch_assoc($q)){
     }
 
     $doc = mysqli_fetch_assoc($doc_q);
-	if($r['last_monthly_fee_date']){
-	    $date = $r['last_monthly_fee_date'];
-	    $newdate = strtotime ( '+1 month' , strtotime ( $date ) ) ;
-	    $monthly_date = date ( 'Y-m-d' , $newdate );
-	}elseif($r['registration_date']){
-	    $date = $r['registration_date'];
-	    $newdate = strtotime ( '+1 month' , strtotime ( $date ) ) ;
-	    $monthly_date = date ( 'Y-m-d' , $newdate );
-	}elseif(!empty($user['trial_period'])  && !empty($user['adddate'])){
-          $date = $user['adddate'];
-          $newdate = strtotime ( '+'.($user['trial_period']+1).' day' , strtotime ( $date ) ) ;
-          $monthly_date = date ( 'm/d/Y' , $newdate );
-        }elseif($r['adddate']){
-	  $date = $r['adddate'];
-          $newdate = strtotime ( '+1 month' , strtotime ( $date ) ) ;
-          $monthly_date = date ( 'Y-m-d' , $newdate );
-	}else{
-	  $monthly_date = date('Y-m-d');
-	}
+    if($r['last_monthly_fee_date']){
+        $date = $r['last_monthly_fee_date'];
+        $newdate = strtotime ( '+1 month' , strtotime ( $date ) ) ;
+        $monthly_date = date ( 'Y-m-d' , $newdate );
+    }elseif($r['registration_date']){
+        $date = $r['registration_date'];
+        $newdate = strtotime ( '+1 month' , strtotime ( $date ) ) ;
+        $monthly_date = date ( 'Y-m-d' , $newdate );
+    }elseif(!empty($user['trial_period'])  && !empty($user['adddate'])){
+        $date = $user['adddate'];
+        $newdate = strtotime ( '+'.($user['trial_period']+1).' day' , strtotime ( $date ) ) ;
+        $monthly_date = date ( 'm/d/Y' , $newdate );
+    }elseif($r['adddate']){
+        $date = $r['adddate'];
+        $newdate = strtotime ( '+1 month' , strtotime ( $date ) ) ;
+        $monthly_date = date ( 'Y-m-d' , $newdate );
+    }else{
+        $monthly_date = date('Y-m-d');
+    }
 
-	$in_sql = "INSERT INTO dental_percase_invoice (adminid, docid, adddate, ip_address, monthly_fee_date, monthly_fee_amount) " .
-                " VALUES (".$_SESSION['adminuserid'].", ".$r['userid'].", NOW(), '".$_SERVER['REMOTE_ADDR']."', '".$db->escape(date('Y-m-d', strtotime($monthly_date)))."', '".$db->escape($doc['monthly_fee'])."')";
-	mysqli_query($con,$in_sql);
-	$invoiceid = mysqli_insert_id($con);
+    $in_sql = "INSERT INTO dental_percase_invoice (adminid, docid, adddate, ip_address, monthly_fee_date, monthly_fee_amount) VALUES (".$_SESSION['adminuserid'].", ".$r['userid'].", NOW(), '".$_SERVER['REMOTE_ADDR']."', '".$db->escape(date('Y-m-d', strtotime($monthly_date)))."', '".$db->escape($doc['monthly_fee'])."')";
+    mysqli_query($con,$in_sql);
+    $invoiceid = mysqli_insert_id($con);
 
-	if(isset($_GET['bill']) && $_GET['bill']=="1"){
-		if($r['cc_id']!=''){
-		  bill_card($r['cc_id'] ,$doc['monthly_fee'], $r['userid'], $invoiceid);	
-		}else{
-                    $charge_sql = "INSERT INTO dental_charge SET
-                        amount='".$db->escape(str_replace(',','',$doc['monthly_fee']))."',
-                        userid='".$db->escape((!empty($user['userid']) ? $user['userid'] : ''))."',
-                        adminid='".$db->escape($_SESSION['adminuserid'])."',
-                        charge_date=NOW(),
-			invoice_id='".$db->escape($invoiceid)."',
-                        status='2',
-                        adddate=NOW(),
-                        ip_address='".$db->escape($_SERVER['REMOTE_ADDR'])."'";
-                        mysqli_query($con,$charge_sql);
-		     $i_sql = "UPDATE dental_percase_invoice set status=2 WHERE id='".$invoiceid."'";
-			mysqli_query($con,$i_sql);
- 		  array_push($no_card, $r['first_name']." ".$r['last_name']);
-		}
-	}
-$_GET['invoice_id'] = $invoiceid;
-$redirect = false;
-include 'percase_invoice_pdf.php';
-  }
+    if(isset($_GET['bill']) && $_GET['bill']=="1"){
+        if($r['cc_id']!=''){
+            bill_card($r['cc_id'] ,$doc['monthly_fee'], $r['userid'], $invoiceid);
+        }else{
+            $charge_sql = "INSERT INTO dental_charge SET
+                amount='".$db->escape(str_replace(',','',$doc['monthly_fee']))."',
+                userid='".$db->escape((!empty($user['userid']) ? $user['userid'] : ''))."',
+                adminid='".$db->escape($_SESSION['adminuserid'])."',
+                charge_date=NOW(),
+                invoice_id='".$db->escape($invoiceid)."',
+                status='2',
+                adddate=NOW(),
+                ip_address='".$db->escape($_SERVER['REMOTE_ADDR'])."'";
+            mysqli_query($con,$charge_sql);
+
+            $i_sql = "UPDATE dental_percase_invoice set status=2 WHERE id='".$invoiceid."'";
+            mysqli_query($con,$i_sql);
+
+            array_push($no_card, $r['first_name']." ".$r['last_name']);
+        }
+    }
+    $_GET['invoice_id'] = $invoiceid;
+    $redirect = false;
+    include 'percase_invoice_pdf.php';
+}
 $msg = mysqli_num_rows($q) . " invoices created.";
-	if (count($no_card)==1) { ?>
-        <script type="text/javascript">
-            alert('<?php echo  implode($no_card); ?> does not have a credit card on record.');
-        </script>
-        <?php
-	} elseif(count($no_card)>0) {
-	    ?>
-        <script type="text/javascript">
-            alert('<?php echo  implode($no_card, ', '); ?> do not have credit cards on record.');
-        </script>
-        <?php
-	} ?>
+if (count($no_card)==1) { ?>
+    <script type="text/javascript">
+        alert('<?php echo  implode($no_card); ?> does not have a credit card on record.');
+    </script>
+    <?php
+} elseif(count($no_card)>0) {
+    ?>
+    <script type="text/javascript">
+        alert('<?php echo  implode($no_card, ', '); ?> do not have credit cards on record.');
+    </script>
+    <?php
+} ?>
 <script type="text/javascript">
-  window.location = "manage_monthly_invoice.php?msg=<?php echo  $msg; ?>";
+    window.location = "manage_monthly_invoice.php?msg=<?php echo  $msg; ?>";
 </script>
 <?php
 function bill_card($customerID, $amount, $userid, $invoiceid)
 {
-  if($amount==0){
-    ?>
-    <script type="text/javascript">
-      alert('Cannot post $0.00 charge to Stripe.');
-    </script>
-    <?php
-  }else{
-$key_r = getStripeRelatedUserData($userid);
-setupStripeConnection($key_r['stripe_secret_key']);
-$status = 1;
+    $con = $GLOBALS['con'];
+    $db = new Db();
 
-try{
-    $charge = \Stripe\Charge::create(array(
-      "amount" => ($amount*100), # $15.00 this time
-      "currency" => "usd",
-      "customer" => $customerID)
-    );
-} catch(\Stripe\Error\Card $e) {
-  $invoice_sql = "UPDATE dental_percase_invoice SET
-                        status=2
-                        WHERE id='".$db->escape($invoiceid)."'";
-  mysqli_query($con,$invoice_sql);
-  $status = 2;
-} catch (\Stripe\Error\InvalidRequest $e) {
-  $invoice_sql = "UPDATE dental_percase_invoice SET
-                        status=2
-                        WHERE id='".$db->escape($invoiceid)."'";
-  mysqli_query($con,$invoice_sql);
-  $status = 2;
-} catch (\Stripe\Error\Authentication $e) {
-  $invoice_sql = "UPDATE dental_percase_invoice SET
-                        status=2
-                        WHERE id='".$db->escape($invoiceid)."'";
-  mysqli_query($con,$invoice_sql);
-  $status = 2;
-} catch (\Stripe\Error\ApiConnection $e) {
-  $invoice_sql = "UPDATE dental_percase_invoice SET
-                        status=2
-                        WHERE id='".$db->escape($invoiceid)."'";
-  mysqli_query($con,$invoice_sql);
-  $status = 2;
-} catch (\Exception $e) {
-  $invoice_sql = "UPDATE dental_percase_invoice SET
-                        status=2
-                        WHERE id='".$db->escape($invoiceid)."'";
-  mysqli_query($con,$invoice_sql);
-  $status = 2;
-}
+    if($amount==0){
+        ?>
+        <script type="text/javascript">
+            alert('Cannot post $0.00 charge to Stripe.');
+        </script>
+        <?php
+    }else{
+        $key_r = getStripeRelatedUserData($userid);
+        setupStripeConnection($key_r['stripe_secret_key']);
+        $status = 1;
 
-  $stripe_charge = $charge->id;
-  $stripe_customer = $charge->customer;
-  $stripe_card_fingerprint = $charge->source->fingerprint;
-  $charge_sql = "INSERT INTO dental_charge SET
-                        amount='".$db->escape(str_replace(',','',$amount))."',
-                        userid='".$db->escape($userid)."',
-                        adminid='".$db->escape($_SESSION['adminuserid'])."',
-                        charge_date=NOW(),
-                        stripe_customer='".$db->escape($stripe_customer)."',
-                        stripe_charge='".$db->escape($stripe_charge)."',
-                        stripe_card_fingerprint='".$db->escape($stripe_card_fingerprint)."',
-			invoice_id='".$db->escape($invoiceid)."',
-			status='".$status."',
-                        adddate=NOW(),
-                        ip_address='".$db->escape($_SERVER['REMOTE_ADDR'])."'";
+        try{
+            $charge = \Stripe\Charge::create([
+                "amount" => ($amount*100), # $15.00 this time
+                "currency" => "usd",
+                "customer" => $customerID,
+            ]);
+        } catch(\Stripe\Error\Card $e) {
+            $invoice_sql = "UPDATE dental_percase_invoice SET
+                status=2
+                WHERE id='".$db->escape($invoiceid)."'";
+            mysqli_query($con,$invoice_sql);
+            $status = 2;
+        } catch (\Stripe\Error\InvalidRequest $e) {
+            $invoice_sql = "UPDATE dental_percase_invoice SET
+                status=2
+                WHERE id='".$db->escape($invoiceid)."'";
+            mysqli_query($con,$invoice_sql);
+            $status = 2;
+        } catch (\Stripe\Error\Authentication $e) {
+            $invoice_sql = "UPDATE dental_percase_invoice SET
+                status=2
+                WHERE id='".$db->escape($invoiceid)."'";
+            mysqli_query($con,$invoice_sql);
+            $status = 2;
+        } catch (\Stripe\Error\ApiConnection $e) {
+            $invoice_sql = "UPDATE dental_percase_invoice SET
+                status=2
+                WHERE id='".$db->escape($invoiceid)."'";
+            mysqli_query($con,$invoice_sql);
+            $status = 2;
+        } catch (\Exception $e) {
+            $invoice_sql = "UPDATE dental_percase_invoice SET
+                status=2
+                WHERE id='".$db->escape($invoiceid)."'";
+            mysqli_query($con,$invoice_sql);
+            $status = 2;
+        }
+
+        $stripe_charge = $charge->id;
+        $stripe_customer = $charge->customer;
+        $stripe_card_fingerprint = $charge->source->fingerprint;
+        $charge_sql = "INSERT INTO dental_charge SET
+            amount='".$db->escape(str_replace(',','',$amount))."',
+            userid='".$db->escape($userid)."',
+            adminid='".$db->escape($_SESSION['adminuserid'])."',
+            charge_date=NOW(),
+            stripe_customer='".$db->escape($stripe_customer)."',
+            stripe_charge='".$db->escape($stripe_charge)."',
+            stripe_card_fingerprint='".$db->escape($stripe_card_fingerprint)."',
+            invoice_id='".$db->escape($invoiceid)."',
+            status='".$status."',
+            adddate=NOW(),
+            ip_address='".$db->escape($_SERVER['REMOTE_ADDR'])."'";
         mysqli_query($con,$charge_sql);
-  if($status==1){
-    $invoice_sql = "UPDATE dental_percase_invoice SET
-			status=1
-			WHERE id='".$db->escape($invoiceid)."'";
-    mysqli_query($con,$invoice_sql);
-  }
-
-}
-  return true;
-
+        if($status==1){
+            $invoice_sql = "UPDATE dental_percase_invoice SET
+                status=1
+                WHERE id='".$db->escape($invoiceid)."'";
+            mysqli_query($con,$invoice_sql);
+        }
+    }
+    return true;
 }
--- legacy/manage/admin/letterhead.php
+++ legacy/manage/admin/letterhead.php
@@ -1,79 +1,92 @@
-<?php namespace Ds3\Libraries\Legacy; ?><?php
+<?php
+namespace Ds3\Libraries\Legacy;
+
 include "includes/top.htm";
 require_once('../includes/constants.inc');
 require_once('../includes/general_functions.php');
 
-function save_image($field = null, $fieldname = null) {
-	$folder = '../../../../shared/q_file';
-	if ((array_search($_FILES[$fieldname]["type"], $dss_image_file_types) !== false) ) {
-		$sql = "select ".st($field)." from dental_users where userid = '".s_for($_POST['uid'])."';";
-		$result = mysqli_query($con,$sql);
-		$my = mysqli_fetch_array($result);
-		$imgname = $my[$field]; 
+function save_image($field, $fieldname)
+{
+    $con = $GLOBALS['con'];
 
-		if($_FILES[$fieldname]["name"] != '') {
-			$fname = $_FILES[$fieldname]["name"];
-			$lastdot = strrpos($fname,".");
-			$name = substr($fname,0,$lastdot);
-			$extension = substr($fname,$lastdot+1);
-			$banner1 = $name.'_'.date('dmy_Hi');
-			$banner1 = str_replace(" ","_",$banner1);
-			$banner1 = str_replace(".","_",$banner1);
-			$banner1 .= ".".$extension;
-			$uploaded = uploadImage($_FILES[$fieldname], $folder.$banner1);
-			if($imgname != '') {
-				@unlink($folder.$imgname);
-			}
-		} else {
-			$banner1 = $imgname;
-		}
-		
-		if($uploaded) {
-			$ed_sql = " update dental_users set 
-			".st($field)." = '".s_for($banner1)."'
-			where userid = '".s_for($_POST['uid'])."'";
-			
-			mysqli_query($con,$ed_sql);				
+    $folder = '../../../../shared/q_file';
+    if ((array_search($_FILES[$fieldname]["type"], $dss_image_file_types) !== false) ) {
+        $sql = "select ".st($field)." from dental_users where userid = '".s_for($_POST['uid'])."';";
+        $result = mysqli_query($con,$sql);
+        $my = mysqli_fetch_array($result);
+        $imgname = $my[$field];
 
-			return true;
-		} else {
-			return "File is too large for $field<br />";
-		}
-	} else {
-		return "Invalid file type for $field<br />";
-	}
+        if($_FILES[$fieldname]["name"] != '') {
+            $fname = $_FILES[$fieldname]["name"];
+            $lastdot = strrpos($fname,".");
+            $name = substr($fname,0,$lastdot);
+            $extension = substr($fname,$lastdot+1);
+            $banner1 = $name.'_'.date('dmy_Hi');
+            $banner1 = str_replace(" ","_",$banner1);
+            $banner1 = str_replace(".","_",$banner1);
+            $banner1 .= ".".$extension;
+            $uploaded = uploadImage($_FILES[$fieldname], $folder.$banner1);
+            if($imgname != '') {
+                @unlink($folder.$imgname);
+            }
+        } else {
+            $banner1 = $imgname;
+        }
+
+        if($uploaded) {
+            $ed_sql = "update dental_users set 
+                ".st($field)." = '".s_for($banner1)."'
+                where userid = '".s_for($_POST['uid'])."'";
+
+            mysqli_query($con,$ed_sql);
+
+            return true;
+        } else {
+            return "File is too large for $field<br />";
+        }
+    } else {
+        return "Invalid file type for $field<br />";
+    }
 }
 
 // Handle POST
 
 if(!empty($_POST["submit_letterhead"])) {
-	$result1 = $result2 = $result3 = $result4 = null; 
+    $result1 = $result2 = $result3 = $result4 = null;
 
-	if ($_FILES['emailheaderimg']["name"]) $result1 = save_image('email_header', 'emailheaderimg');
-	if ($_FILES['emailfooterimg']["name"]) $result2 = save_image('email_footer', 'emailfooterimg');
-	if ($_FILES['faxheaderimg']["name"]) $result3 = save_image('fax_header', 'faxheaderimg');
-	if ($_FILES['faxfooterimg']["name"]) $result4 = save_image('fax_footer', 'faxfooterimg');
+    if ($_FILES['emailheaderimg']["name"]) {
+        $result1 = save_image('email_header', 'emailheaderimg');
+    }
+    if ($_FILES['emailfooterimg']["name"]) {
+        $result2 = save_image('email_footer', 'emailfooterimg');
+    }
+    if ($_FILES['faxheaderimg']["name"]) {
+        $result3 = save_image('fax_header', 'faxheaderimg');
+    }
+    if ($_FILES['faxfooterimg']["name"]) {
+        $result4 = save_image('fax_footer', 'faxfooterimg');
+    }
 
-  if ($result1 !== true) { 
-		$msg = $result1;
-	} else {
-		$msg = "Email Header uploaded sucessfully<br />";
-	}
-  if ($result2 !== true) {
-		$msg .= $result2;
-	} else {
-		$msg .= "Email Footer uploaded successfully<br />";
-	}
-  if ($result3 !== true) {
-		$msg .= $result3;
-	} else {
- 		$msg .= "Fax Header uploaded successfully<br />";
-	}
-  if ($result4 !== true) {
-		$msg .= $result4;
-	} else {
-		$msg .= "Fax Footer uploaded successully<br />";
-	}
+    if ($result1 !== true) {
+        $msg = $result1;
+    } else {
+        $msg = "Email Header uploaded sucessfully<br />";
+    }
+    if ($result2 !== true) {
+        $msg .= $result2;
+    } else {
+        $msg .= "Email Footer uploaded successfully<br />";
+    }
+    if ($result3 !== true) {
+        $msg .= $result3;
+    } else {
+        $msg .= "Fax Header uploaded successfully<br />";
+    }
+    if ($result4 !== true) {
+        $msg .= $result4;
+    } else {
+        $msg .= "Fax Footer uploaded successully<br />";
+    }
 }
 
 // Query Database for User Information
@@ -94,88 +107,87 @@ $fax_header_imgname = $my['fax_header'];
 ?>
 
 <script type="text/javascript">
-	$(document).ready(function() {
-		$('.toggle_but').click(function() {
-			var r = confirm("Do you want to replace the image on file? This cannot be undone.");
-			if (r == true) {
-				var prefix = $(this).attr('id');
-				$('#'+prefix+'view').css("display", "none");
-				$('#'+prefix).css("display", "none");
-				$('#'+prefix+'img').css("display", "inline");
-			}
-		});
-	});
+    $(document).ready(function() {
+        $('.toggle_but').click(function() {
+            var r = confirm("Do you want to replace the image on file? This cannot be undone.");
+            if (r == true) {
+                var prefix = $(this).attr('id');
+                $('#'+prefix+'view').css("display", "none");
+                $('#'+prefix).css("display", "none");
+                $('#'+prefix+'img').css("display", "inline");
+            }
+        });
+    });
 </script>
 
 <div class="page-header">
-	<h1>Update Letterhead for <?= $docname ?></h1>
+    <h1>Update Letterhead for <?= $docname ?></h1>
 </div>
 
 <?php if (!empty($msg)) { ?>
-<div class="alert alert-success text-center">
-    <?= $msg ?>
-</div>
+    <div class="alert alert-success text-center">
+        <?= $msg ?>
+    </div>
 <?php } ?>
 
 <form name="letterhead" action="<?=$_SERVER['PHP_SELF'];?>?uid=<?=$_GET['uid'];?>" method="post" enctype="multipart/form-data" class="form-horizontal">
-	<input type="hidden" name="uid" value="<?=$_GET['uid'];?>">
-	
-	<table class="table table-bordered table-hover">
-		<thead>
-			<tr>
-				<th></th>
-				<th>Email</th>
-				<th>Fax</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<th>Header</th>
-				<td>
-					<?php if (empty($email_header_imgname)): ?>
-						<input id="emailheaderimg" name="emailheaderimg" type="file" class="form-control">
-					<?php else: ?>
-						<input id="emailheaderimg" style="display:none;" name="emailheaderimg" type="file" class="form-control">
-						<input type="button" id="email_header_view" value="View" title="View" onClick="window.open('display_file.php?f=<?= $email_header_imgname ?>','windowname1','width=860, height=790,scrollbars=yes');return false;" class="btn btn-default">
-						<input type="button" id="emailheader" value="Edit" title="Edit"  class="btn btn-info toggle_but">
-					<?php endif; ?>
-				</td>
-				<td>
-					<?php if (empty($fax_header_imgname)): ?>
-						<input id="faxheaderimg" name="faxheaderimg" type="file" size="4" />
-					<?php else: ?>
-						<input id="faxheaderimg" style="display:none;" name="faxheaderimg" type="file" class="form-control">
-						<input type="button" id="fax_header_view" value="View" title="View" onClick="window.open('display_file.php?f=<?= $fax_header_imgname ?>','windowname1','width=860, height=790,scrollbars=yes');return false;" class="btn btn-default">
-						<input type="button" id="faxheader" value="Edit" title="Edit"  class="btn btn-info toggle_but">
-					<?php endif; ?>
-				</td>
-			</tr>
-			<tr>
-				<th>Footer</th>
-				<td>
-					<?php if (empty($email_footer_imgname)): ?>
-						<input id="emailfooterimg" name="emailfooterimg" type="file" class="form-control">
-					<?php else: ?>
-						<input id="emailfooterimg" style="display:none;" name="emailfooterimg" type="file" class="form-control">
-						<input type="button" id="email_footer_view" value="View" title="View" onClick="window.open('display_file.php?f=<?= $email_footer_imgname ?>','windowname1','width=860, height=790,scrollbars=yes');return false;" class="btn btn-default">
-						<input type="button" id="emailfooter" value="Edit" title="Edit"  class="btn btn-info toggle_but">
-					<?php endif; ?>
-				</td>
-				<td>
-					<?php if (empty($email_footer_imgname)): ?>
-						<input id="emailfooterimg" name="emailfooterimg" type="file" class="form-control">
-					<?php else: ?>
-						<input id="emailfooterimg" style="display:none;" name="emailfooterimg" type="file" class="form-control">
-						<input type="button" id="email_footer_view" value="View" title="View" onClick="window.open('display_file.php?f<?= $email_footer_imgname ?>','windowname1','width=860, height=790,scrollbars=yes');return false;" class="btn btn-default">
-						<input type="button" id="emailfooter" value="Edit" title="Edit"  class="btn btn-info toggle_but">
-					<?php endif; ?>
-				</td>
-			</tr>
-		</tbody>
-	</table>
-	<p class="text-center">
-		<input name="submit_letterhead" type="submit" value="Submit" class="btn btn-success">
-	</p>
+    <input type="hidden" name="uid" value="<?=$_GET['uid'];?>">
+    <table class="table table-bordered table-hover">
+        <thead>
+            <tr>
+                <th></th>
+                <th>Email</th>
+                <th>Fax</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>Header</th>
+                <td>
+                    <?php if (empty($email_header_imgname)): ?>
+                        <input id="emailheaderimg" name="emailheaderimg" type="file" class="form-control">
+                    <?php else: ?>
+                        <input id="emailheaderimg" style="display:none;" name="emailheaderimg" type="file" class="form-control">
+                        <input type="button" id="email_header_view" value="View" title="View" onClick="window.open('display_file.php?f=<?= $email_header_imgname ?>','windowname1','width=860, height=790,scrollbars=yes');return false;" class="btn btn-default">
+                        <input type="button" id="emailheader" value="Edit" title="Edit"  class="btn btn-info toggle_but">
+                    <?php endif; ?>
+                </td>
+                <td>
+                    <?php if (empty($fax_header_imgname)): ?>
+                        <input id="faxheaderimg" name="faxheaderimg" type="file" size="4" />
+                    <?php else: ?>
+                        <input id="faxheaderimg" style="display:none;" name="faxheaderimg" type="file" class="form-control">
+                        <input type="button" id="fax_header_view" value="View" title="View" onClick="window.open('display_file.php?f=<?= $fax_header_imgname ?>','windowname1','width=860, height=790,scrollbars=yes');return false;" class="btn btn-default">
+                        <input type="button" id="faxheader" value="Edit" title="Edit"  class="btn btn-info toggle_but">
+                    <?php endif; ?>
+                </td>
+            </tr>
+            <tr>
+                <th>Footer</th>
+                <td>
+                    <?php if (empty($email_footer_imgname)): ?>
+                        <input id="emailfooterimg" name="emailfooterimg" type="file" class="form-control">
+                    <?php else: ?>
+                        <input id="emailfooterimg" style="display:none;" name="emailfooterimg" type="file" class="form-control">
+                        <input type="button" id="email_footer_view" value="View" title="View" onClick="window.open('display_file.php?f=<?= $email_footer_imgname ?>','windowname1','width=860, height=790,scrollbars=yes');return false;" class="btn btn-default">
+                        <input type="button" id="emailfooter" value="Edit" title="Edit"  class="btn btn-info toggle_but">
+                    <?php endif; ?>
+                </td>
+                <td>
+                    <?php if (empty($email_footer_imgname)): ?>
+                        <input id="emailfooterimg" name="emailfooterimg" type="file" class="form-control">
+                    <?php else: ?>
+                        <input id="emailfooterimg" style="display:none;" name="emailfooterimg" type="file" class="form-control">
+                        <input type="button" id="email_footer_view" value="View" title="View" onClick="window.open('display_file.php?f<?= $email_footer_imgname ?>','windowname1','width=860, height=790,scrollbars=yes');return false;" class="btn btn-default">
+                        <input type="button" id="emailfooter" value="Edit" title="Edit"  class="btn btn-info toggle_but">
+                    <?php endif; ?>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <p class="text-center">
+        <input name="submit_letterhead" type="submit" value="Submit" class="btn btn-success">
+    </p>
 </form>
 
 <?php include "includes/bottom.htm"; ?>

d3e1158da3b4a3a4e1b0b1cabb17bbb8e0604823
Vladislav Kryshtanovskiy <wladislav.k@gmail.com>
2018-07-20 23:16:45 +0700

    DSS-874 * Moved file uploader

---
+++ src/FileUploader/php.php
@@ -0,0 +1,12 @@
+<?php
+namespace Ds3ThirdParty\FileUploader;
+
+// list of valid extensions, ex. array("jpeg", "xml", "bmp")
+$allowedExtensions = array();
+// max file size in bytes
+$sizeLimit = 10 * 1024 * 1024;
+
+$uploader = new qqFileUploader($allowedExtensions, $sizeLimit);
+$result = $uploader->handleUpload('uploads/');
+// to pass data through iframe you will need to encode all html tags
+echo htmlspecialchars(json_encode($result), ENT_NOQUOTES);

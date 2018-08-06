9b21860cfe7b27ba1d2fd71baf9520dcf4495f10
Ast Derek <ast.derek@gmail.com>
2018-08-06 17:48:33 -0500

    DSS-911 Correct sql delimiters

--- api/database/migrations/2018_08_04_000000_alter_dental_api_permissions_nullable_patient_id.php
+++ api/database/migrations/2018_08_04_000000_alter_dental_api_permissions_nullable_patient_id.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Migrations\Migration;
 class AlterDentalApiPermissionsNullablePatientId extends Migration
 {
     public function up () {
-        DB::statement(DB::raw("ALTER TABLE dental_api_permissions MODIFY patient_id INT(11) DEFAULT NULL"));
-        DB::statement(DB::raw("UPDATE dental_api_permissions SET patient_id = NULL WHERE patient_id = 0"));
+        DB::statement(DB::raw("ALTER TABLE `dental_api_permissions` MODIFY `patient_id` INT(11) DEFAULT NULL"));
+        DB::statement(DB::raw("UPDATE `dental_api_permissions` SET `patient_id` = NULL WHERE `patient_id` = 0"));
     }
 
     public function down () {
-        DB::statement(DB::raw("UPDATE dental_api_permissions SET patient_id = 0 WHERE patient_id IS NULL"));
-        DB::statement(DB::raw("ALTER TABLE dental_api_permissions MODIFY patient_id INT(11) NOT NULL DEFAULT 0"));
+        DB::statement(DB::raw("UPDATE `dental_api_permissions` SET `patient_id` = 0 WHERE `patient_id` IS NULL"));
+        DB::statement(DB::raw("ALTER TABLE `dental_api_permissions` MODIFY `patient_id` INT(11) NOT NULL DEFAULT 0"));
     }
 }

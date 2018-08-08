705afe156cd9f15eb9e1d9e511953cd8667baa2f
Ast Derek <ast.derek@gmail.com>
2018-08-08 17:35:24 -0500

    DSS-911 Avoid future merge conflicts

--- api/database/migrations/2018_08_04_000000_alter_dental_api_permissions_nullable_patient_id.php
+++ api/database/migrations/2018_08_04_000000_alter_dental_api_permissions_nullable_patient_id.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 class AlterDentalApiPermissionsNullablePatientId extends Migration
 {
     public function up () {
-        DB::statement(DB::raw("ALTER TABLE `dental_api_permissions` MODIFY `patient_id` INT(11) DEFAULT NULL"));
+        DB::statement(DB::raw("ALTER TABLE `dental_api_permissions` MODIFY `patient_id` INT(10) DEFAULT NULL"));
         DB::statement(DB::raw("UPDATE `dental_api_permissions` SET `patient_id` = NULL WHERE `patient_id` = 0"));
     }
 

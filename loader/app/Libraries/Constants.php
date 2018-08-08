07d812b1709742b09aed2c41d06df0606d9209cd
Ast Derek <ast.derek@gmail.com>
2018-08-08 16:16:21 -0500

    DSS-929 Apply changes from PR review feedback

--- loader/app/Libraries/Constants.php
+++ loader/app/Libraries/Constants.php
@@ -328,8 +328,6 @@ class Constants
         self::DSS_USER_STATUS_SUSPENDED  => 'Suspended'
     );
 
-    const DSS_PATIENT_STATUS_ACTIVE = 1;
-
     const DSS_OFFICE_TYPE_BACK   = 0;
     const DSS_OFFICE_TYPE_FRONT  = 1;
 
